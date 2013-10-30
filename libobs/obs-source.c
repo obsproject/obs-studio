3adfaf7f9fc02da252b8238748038dffec969d17
jp9000 <obs.jim@gmail.com>
2013-10-29 23:54:43 -0700

    not sure how a backslash slipped into an include, but it's gone now

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -15,7 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
-#include "media-io\format-conversion.h"
+#include "media-io/format-conversion.h"
 #include "util/platform.h"
 
 #include "obs.h"
