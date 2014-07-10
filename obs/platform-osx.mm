5bbd918af348fea69fab073c3e0b35d09b3b0a0f
Palana <palana@stunned.de>
2014-07-10 22:00:18 +0200

    Update default video save path for OSX

--- obs/platform-osx.mm
+++ obs/platform-osx.mm
@@ -79,5 +79,15 @@ bool InitApplicationBundle()
 
 string GetDefaultVideoSavePath()
 {
-	return string(getenv("HOME"));
+	NSFileManager *fm = [NSFileManager defaultManager];
+	NSURL *url = [fm URLForDirectory:NSMoviesDirectory
+				inDomain:NSUserDomainMask
+		       appropriateForURL:nil
+				  create:true
+				   error:nil];
+	
+	if (!url)
+		return getenv("HOME");
+
+	return url.fileSystemRepresentation;
 }
