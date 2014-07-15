38c2fc87aa67f746dc0463518b131b2025e6f50c
BtbN <btbn@btbn.de>
2014-07-15 15:02:03 +0200

    Move all data into the subdir it belongs to
    Completely removes the build dir in favor of cmake based build layouting

--- build/copy_debug64.bat
+++
@@ -1,5 +0,0 @@
-copy ..\vs\2010\x64\Debug\libobs.dll bin\64bit
-copy ..\vs\2010\x64\Debug\libobs-opengl.dll bin\64bit
-copy ..\vs\2010\x64\Debug\libobs-d3d11.dll bin\64bit
-copy ..\vs\2010\x64\Debug\wintest.exe bin\64bit
-copy ..\vs\2010\x64\Debug\test-input.dll plugins\64bit
