07d099bd1e3ac6e3c88eb7e042c2125aaf6a76b7
jp9000 <obs.jim@gmail.com>
2014-08-25 09:10:29 -0700

    text-freetype2: Localize text file path filter

--- plugins/text-freetype2/text-freetype2.c
+++ plugins/text-freetype2/text-freetype2.c
@@ -112,7 +112,7 @@ static obs_properties_t ft2_source_properties(void)
 
 	obs_properties_add_path(props,
 		"text_file", obs_module_text("TextFile"),
-		OBS_PATH_FILE, "Text Files (*.txt);;", NULL);
+		OBS_PATH_FILE, obs_module_text("TextFileFilter"), NULL);
 
 	obs_properties_add_color(props, "color1",
 		obs_module_text("Color1"));
