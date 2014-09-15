978ff008fe89c9fd04c3bbb432af1be1b4d0c627
BtbN <btbn@btbn.de>
2014-09-15 19:58:31 +0200

    Fix major misunderstanding of cached variables in all find modules

--- cmake/Modules/FindLibavformat.cmake
+++ cmake/Modules/FindLibavformat.cmake
@@ -3,53 +3,49 @@
 #  LIBAVFORMAT_FOUND
 #  LIBAVFORMAT_INCLUDE_DIRS
 #  LIBAVFORMAT_LIBRARIES
-#
 
-if(LIBAVFORMAT_INCLUDE_DIRS AND LIBAVFORMAT_LIBRARIES)
-	set(LIBAVFORMAT_FOUND TRUE)
-else()
-	find_package(PkgConfig QUIET)
-	if (PKG_CONFIG_FOUND)
-		pkg_check_modules(_AVFORMAT QUIET libavformat)
-	endif()
-
-	if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-		set(_lib_suffix 64)
-	else()
-		set(_lib_suffix 32)
-	endif()
-
-	set(FFMPEG_PATH_ARCH FFmpegPath${_lib_suffix})
-
-	find_path(FFMPEG_INCLUDE_DIR
-		NAMES libavformat/avformat.h
-		HINTS
-			${_AVFORMAT_INCLUDE_DIRS}
-			"${CMAKE_SOURCE_DIR}/additional_install_files/include"
-			"$ENV{obsAdditionalInstallFiles}/include"
-			ENV FFmpegPath
-			ENV ${FFMPEG_PATH_ARCH}
-		PATHS
-			/usr/include /usr/local/include /opt/local/include /sw/include
-		PATH_SUFFIXES ffmpeg libav)
-
-	find_library(AVFORMAT_LIB
-		NAMES ${_AVFORMAT_LIBRARIES} avformat-ffmpeg avformat
-		HINTS
-			${_AVFORMAT_LIBRARY_DIRS}
-			"${FFMPEG_INCLUDE_DIR}/../lib"
-			"${FFMPEG_INCLUDE_DIR}/../lib${_lib_suffix}"
-			"${FFMPEG_INCLUDE_DIR}/../libs${_lib_suffix}"
-			"${FFMPEG_INCLUDE_DIR}/lib"
-			"${FFMPEG_INCLUDE_DIR}/lib${_lib_suffix}"
-		PATHS
-			/usr/lib /usr/local/lib /opt/local/lib /sw/lib)
-
-	set(LIBAVFORMAT_INCLUDE_DIRS ${FFMPEG_INCLUDE_DIR} CACHE PATH "Libavformat include dir")
-	set(LIBAVFORMAT_LIBRARIES ${AVFORMAT_LIB} CACHE STRING "Libavformat libraries")
-
-	include(FindPackageHandleStandardArgs)
-	find_package_handle_standard_args(Libavformat DEFAULT_MSG AVFORMAT_LIB FFMPEG_INCLUDE_DIR)
-	mark_as_advanced(FFMPEG_INCLUDE_DIR AVFORMAT_LIB)
+find_package(PkgConfig QUIET)
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(_AVFORMAT QUIET libavformat)
 endif()
 
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(_lib_suffix 64)
+else()
+	set(_lib_suffix 32)
+endif()
+
+set(FFMPEG_PATH_ARCH FFmpegPath${_lib_suffix})
+
+find_path(FFMPEG_INCLUDE_DIR
+	NAMES libavformat/avformat.h
+	HINTS
+		${_AVFORMAT_INCLUDE_DIRS}
+		"${CMAKE_SOURCE_DIR}/additional_install_files/include"
+		"$ENV{obsAdditionalInstallFiles}/include"
+		ENV FFmpegPath
+		ENV ${FFMPEG_PATH_ARCH}
+	PATHS
+		/usr/include /usr/local/include /opt/local/include /sw/include
+	PATH_SUFFIXES ffmpeg libav)
+
+find_library(AVFORMAT_LIB
+	NAMES ${_AVFORMAT_LIBRARIES} avformat-ffmpeg avformat
+	HINTS
+		${_AVFORMAT_LIBRARY_DIRS}
+		"${FFMPEG_INCLUDE_DIR}/../lib"
+		"${FFMPEG_INCLUDE_DIR}/../lib${_lib_suffix}"
+		"${FFMPEG_INCLUDE_DIR}/../libs${_lib_suffix}"
+		"${FFMPEG_INCLUDE_DIR}/lib"
+		"${FFMPEG_INCLUDE_DIR}/lib${_lib_suffix}"
+	PATHS
+		/usr/lib /usr/local/lib /opt/local/lib /sw/lib)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libavformat DEFAULT_MSG AVFORMAT_LIB FFMPEG_INCLUDE_DIR)
+mark_as_advanced(FFMPEG_INCLUDE_DIR AVFORMAT_LIB)
+
+if(LIBAVFORMAT_FOUND)
+	set(LIBAVFORMAT_INCLUDE_DIRS ${FFMPEG_INCLUDE_DIR})
+	set(LIBAVFORMAT_LIBRARIES ${AVFORMAT_LIB})
+endif()
--- cmake/Modules/FindLibv4l2.cmake
+++ cmake/Modules/FindLibv4l2.cmake
@@ -3,37 +3,31 @@
 #  LIBV4L2_FOUND
 #  LIBV4L2_INCLUDE_DIRS
 #  LIBV4L2_LIBRARIES
-#
 
-if(LIBV4L2_INCLUDE_DIRS AND LIBV4L2_LIBRARIES)
-	set(LIBV4L2_FOUND TRUE)
-else()
-	find_package(PkgConfig QUIET)
-	if (PKG_CONFIG_FOUND)
-		pkg_check_modules(_V4L2 QUIET v4l-utils)
-	endif()
+find_package(PkgConfig QUIET)
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(_V4L2 QUIET v4l-utils)
+endif()
 
-	find_path(V4L2_INCLUDE_DIR
-		NAMES libv4l2.h
-		HINTS
-			${_V4L2_INCLUDE_DIRS}
-		PATHS
-			/usr/include /usr/local/include /opt/local/include)
+find_path(V4L2_INCLUDE_DIR
+	NAMES libv4l2.h
+	HINTS
+		${_V4L2_INCLUDE_DIRS}
+	PATHS
+		/usr/include /usr/local/include /opt/local/include)
 
-	find_library(V4L2_LIB
-		NAMES v4l2
-		HINTS
-			${_V4L2_LIBRARY_DIRS}
-		PATHS
-			/usr/lib /usr/local/lib /opt/local/lib)
+find_library(V4L2_LIB
+	NAMES v4l2
+	HINTS
+		${_V4L2_LIBRARY_DIRS}
+	PATHS
+		/usr/lib /usr/local/lib /opt/local/lib)
 
-	set(LIBV4L2_INCLUDE_DIRS ${V4L2_INCLUDE_DIR}
-		CACHE PATH "v4l2 include dir")
-	set(LIBV4L2_LIBRARIES "${V4L2_LIB}"
-		CACHE STRING "v4l2 libraries")
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibV4L2 DEFAULT_MSG V4L2_LIB V4L2_INCLUDE_DIR)
+mark_as_advanced(V4L2_INCLUDE_DIR V4L2_LIB)
 
-	include(FindPackageHandleStandardArgs)
-	find_package_handle_standard_args(LibV4L2 DEFAULT_MSG V4L2_LIB
-		V4L2_INCLUDE_DIR)
-	mark_as_advanced(V4L2_INCLUDE_DIR V4L2_LIB)
+if(LIBV4L2_FOUND)
+	set(LIBV4L2_INCLUDE_DIRS ${V4L2_INCLUDE_DIR})
+	set(LIBV4L2_LIBRARIES ${V4L2_LIB})
 endif()
