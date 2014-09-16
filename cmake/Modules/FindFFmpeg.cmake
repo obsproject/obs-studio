ba9561370030a37e2e1232c6354177d7425b82b4
Matt Coffin <mcoffin13@gmail.com>
2014-09-16 14:12:38 -0600

    Finish FFmpeg find module conversion

--- cmake/Modules/FindFFmpeg.cmake
+++ cmake/Modules/FindFFmpeg.cmake
@@ -14,33 +14,57 @@ endif(NOT FFmpeg_FIND_COMPONENTS)
 
 # Generate component include files and requirements
 foreach(comp ${FFmpeg_FIND_COMPONENTS})
-	list(APPEND required "FFmpeg_${comp}_LIBRARY")
-	list(APPEND FFmpeg_INCLUDE_FILES "lib${comp}/${comp}.h")
+	if(FFmpeg_FIND_REQUIRED_${comp})
+		list(APPEND required "FFmpeg_${comp}_FOUND")
+	endif()
 endforeach(comp)
 
-# Find root include directory
-find_path(FFmpeg_INCLUDE_DIR
-	NAMES ${FFmpeg_INCLUDE_FILES}
-	DOC "FFmpeg include directory"
-)
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(_lib_suffix 64)
+else()
+	set(_lib_suffix 32)
+endif()
 
+set(FFMPEG_PATH_ARCH FFmpegPath${_lib_suffix})
 # Find libraries
+find_package(PkgConfig QUIET)
 foreach(comp ${FFmpeg_FIND_COMPONENTS})
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(_${comp} QUIET lib${comp})
+	endif()
+	find_path(FFmpeg_${comp}_INCLUDE_DIR
+		NAMES "lib${comp}/${comp}.h"
+		HINTS
+			${_${comp}_INCLUDE_DIRS}
+			ENV FFmpegPath
+			ENV ${FFMPEG_PATH_ARCH}
+		PATHS
+			/usr/include /usr/local/include /opt/local/include /sw/include
+		PATH_SUFFIXES ffmpeg libav
+		DOC "FFmpeg include directory")
 	find_library(FFmpeg_${comp}_LIBRARY
-		NAMES ${comp}
+		NAMES ${comp} ${comp}-ffmpeg ${_${comp}_LIBRARIES}
+		HINTS
+			${_${comp}_LIBRARY_DIRS}
+			"${FFmpeg_${comp}_INCLUDE_DIR}/../lib"
+			"${FFmpeg_${comp}_INCLUDE_DIR}/../lib${_lib_suffix}"
+			"${FFmpeg_${comp}_INCLUDE_DIR}/../libs${_lib_suffix}"
+			"${FFmpeg_${comp}_INCLUDE_DIR}/lib"
+			"${FFmpeg_${comp}_INCLUDE_DIR}/lib${_lib_suffix}"
+		PATHS
+			/usr/lib /usr/local/lib /opt/local/lib /sw/lib
 		PATH_SUFFIXES ${comp} lib${comp}
 		DOC "FFmpeg ${comp} library")
 	find_package_handle_standard_args(FFmpeg_${comp}
 		FOUND_VAR FFmpeg_${comp}_FOUND
-		REQUIRED_VARS FFmpeg_${comp}_LIBRARY)
-	list(APPEND FFmpeg_LIBRARIES ${FFmpeg_${comp}_LIBRARY})
+		REQUIRED_VARS FFmpeg_${comp}_LIBRARY FFmpeg_${comp}_INCLUDE_DIR)
+	if(${FFmpeg_${comp}_FOUND})
+		list(APPEND FFmpeg_INCLUDE_DIRS ${FFmpeg_${comp}_INCLUDE_DIR})
+		list(APPEND FFmpeg_LIBRARIES ${FFmpeg_${comp}_LIBRARY})
+	endif()
 endforeach(comp)
-
-# Set exported variables
-set(FFmpeg_INCLUDE_DIRS ${FFmpeg_INCLUDE_DIR})
 
 # Run checks via find_package_handle_standard_args
 find_package_handle_standard_args(FFmpeg
 	FOUND_VAR FFmpeg_FOUND
-	REQUIRED_VARS ${required} FFmpeg_INCLUDE_DIRS
-)
+	REQUIRED_VARS ${required} FFmpeg_INCLUDE_DIRS FFmpeg_LIBRARIES)
