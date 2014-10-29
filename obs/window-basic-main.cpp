6dcdaab2be6c7d2fa0ce137c5a6fde01bd427dfb
Palana <palana@stunned.de>
2014-10-29 15:53:59 +0100

    Register OBSSource as Qt metatype

--- obs/window-basic-main.cpp
+++ obs/window-basic-main.cpp
@@ -57,6 +57,7 @@ using namespace std;
 
 Q_DECLARE_METATYPE(OBSScene);
 Q_DECLARE_METATYPE(OBSSceneItem);
+Q_DECLARE_METATYPE(OBSSource);
 Q_DECLARE_METATYPE(obs_order_movement);
 
 static void AddExtraModulePaths()
