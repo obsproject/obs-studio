a49d731df87554784e9cac107ca2cd0b2dd1445b
jp9000 <obs.jim@gmail.com>
2014-11-10 01:21:50 -0800

    win-capture: Add obfuscation functions
    This adds obfuscation functions primarily for use with GetProcAddress.
    This takes an obfuscated string and uses a simple integer key to
    de-obfuscate it to the intended function name string, which is then
    loaded dynamically using GetProcAddress.
    
    This is typically only used with functions such as OpenProcess,
    SetWindowsHookEx, and the like, which can often be misinterpreted the
    wrong way by security programs if those strings are found within the
    strings segment of a scanned executable.

---
+++ plugins/win-capture/obfuscate.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* this is a workaround to A/Vs going crazy whenever certain functions (such as
+ * OpenProcess) are used */ 
+extern void *get_obfuscated_func(HMODULE module, const char *str, uint64_t val);
+
+#ifdef __cplusplus
+}
+#endif
