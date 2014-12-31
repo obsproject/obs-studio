5d0551eb27ac6d01f4b3bf7f3e014371c0b46f6f
jp9000 <obs.jim@gmail.com>
2014-12-31 00:29:14 -0800

    libobs/media-io: Add #pragma once to video-frame.h

--- libobs/media-io/video-frame.h
+++ libobs/media-io/video-frame.h
@@ -15,6 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
+#pragma once
+
 #include "../util/bmem.h"
 #include "video-io.h"
 
