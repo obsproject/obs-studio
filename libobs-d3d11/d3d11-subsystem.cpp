d1a1a847222444008e30d5acc74de73fccb5e795
martell <martellmalone@gmail.com>
2015-02-07 00:27:23 +0000

    obs-d3d11: Use cinttypes header (mingw)
    There appears to be a bug with mingw where including the regular C
    inttypes.h won't work; instead you have to use the 'cinttypes' C++
    version.

--- libobs-d3d11/d3d11-subsystem.cpp
+++ libobs-d3d11/d3d11-subsystem.cpp
@@ -15,7 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
-#include <inttypes.h>
+#include <cinttypes>
 #include <util/base.h>
 #include <util/platform.h>
 #include <graphics/matrix3.h>
