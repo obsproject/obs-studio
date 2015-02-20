70f8369ae3a43810ac11701e35626bd3de5e65bb
Skyler Lipthay <skyler.lipthay@gmail.com>
2015-02-19 17:15:59 -0800

    decklink: Add Linux DeckLink project
    The code specific to Linux helps convert `const char *` instances to
    `std::string`s.

--- plugins/decklink/platform.hpp
+++ plugins/decklink/platform.hpp
@@ -7,7 +7,8 @@
 #include <CoreFoundation/CoreFoundation.h>
 typedef CFStringRef decklink_string_t;
 #elif defined(__linux__)
-// TODO: Linux support
+#include "linux/decklink-sdk/DeckLinkAPI.h"
+typedef const char *decklink_string_t;
 #endif
 
 #include <util/windows/HRError.hpp>
