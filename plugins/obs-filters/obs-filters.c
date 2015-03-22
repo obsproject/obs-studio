bee5b3a2d2e1e08e8f78707b34455dc707c2e00c
jp9000 <obs.jim@gmail.com>
2015-03-21 18:34:29 -0700

    obs-filters: Add color correction filter
    Adds a color correction filter if one wishes to just apply
    gamma/constrast/brightness/opacity/color to a source.

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -6,12 +6,14 @@ OBS_MODULE_USE_DEFAULT_LOCALE("obs-filters", "en-US")
 
 extern struct obs_source_info mask_filter;
 extern struct obs_source_info crop_filter;
+extern struct obs_source_info color_filter;
 extern struct obs_source_info async_delay_filter;
 
 bool obs_module_load(void)
 {
 	obs_register_source(&mask_filter);
 	obs_register_source(&crop_filter);
+	obs_register_source(&color_filter);
 	obs_register_source(&async_delay_filter);
 	return true;
 }
