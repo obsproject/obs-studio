b33d95999fe6af181a4462ea345211c875260228
jp9000 <obs.jim@gmail.com>
2015-03-23 19:11:20 -0700

    libobs: Mark audio filters as async
    Async filters and effect filters are really two different classes of
    filters, so it's important that audio filters be marked as async.

--- libobs/obs-module.c
+++ libobs/obs-module.c
@@ -473,6 +473,12 @@ void obs_register_source_s(const struct obs_source_info *info, size_t size)
 
 	memcpy(&data, info, size);
 
+	/* mark audio-only filters as an async filter categorically */
+	if (data.type == OBS_SOURCE_TYPE_FILTER) {
+		if ((data.output_flags & OBS_SOURCE_VIDEO) == 0)
+			data.output_flags |= OBS_SOURCE_ASYNC;
+	}
+
 	darray_push_back(sizeof(struct obs_source_info), array, &data);
 }
 
