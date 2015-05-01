9c483aae104df5fc15d344f3e53cbb2ed68beddb
Kris Moore <kris@pcbsd.org>
2015-05-01 22:05:00 +0200

    libobs: Add support for FreeBSD to gs_window
    Enable the X11 specific members in the gs_window struct when on FreeBSD.

--- libobs/graphics/graphics.h
+++ libobs/graphics/graphics.h
@@ -426,7 +426,7 @@ struct gs_window {
 	void                    *hwnd;
 #elif defined(__APPLE__)
 	__unsafe_unretained id  view;
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__FreeBSD__)
 	/* I'm not sure how portable defining id to uint32_t is. */
 	uint32_t id;
 	void* display;
