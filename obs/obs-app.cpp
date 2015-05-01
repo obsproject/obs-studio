29c7113dd71ff510ee9d568d48db9b37e74c5309
Kris Moore <kris@pcbsd.org>
2015-05-01 22:05:00 +0200

    UI: Add FreeBSD as a platform for version string
    Add an additional ifdef to correctly identify FreeBSD as platform when
    generating the version string.

--- obs/obs-app.cpp
+++ obs/obs-app.cpp
@@ -355,6 +355,8 @@ string OBSApp::GetVersionString() const
 	ver << "windows)";
 #elif __APPLE__
 	ver << "mac)";
+#elif __FreeBSD__
+	ver << "freebsd)";
 #else /* assume linux for the time being */
 	ver << "linux)";
 #endif
