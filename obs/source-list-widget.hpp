b53d6e9e58dc404266fbe68b302daa301bd4a87d
Palana <palana@stunned.de>
2015-06-27 03:54:20 +0200

    UI: Add drag&drop reordering for the sources list

--- obs/source-list-widget.hpp
+++ obs/source-list-widget.hpp
@@ -6,12 +6,17 @@ class QMouseEvent;
 
 class SourceListWidget : public QListWidget {
 	Q_OBJECT
+
+	bool ignoreReorder = false;
 public:
 	inline SourceListWidget(QWidget *parent = nullptr)
 		: QListWidget(parent)
 	{
 	}
 
+	bool IgnoreReorder() const { return ignoreReorder; }
+
 protected:
 	virtual void mouseDoubleClickEvent(QMouseEvent *event) override;
+	virtual void dropEvent(QDropEvent *event) override;
 };
