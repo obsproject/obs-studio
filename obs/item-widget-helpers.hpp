db9e966e6c3a3142c747d7458538145c148d63b1
jp9000 <obs.jim@gmail.com>
2015-06-27 18:44:36 -0700

    UI: Add item widget helper functions
    These functions are designed to manually delete item widgets within
    other widgets to prevent ->deleteLater from being called on them.  This
    prevents the item widgets from being stuck in the event queue, and
    prevents references to things like sources from being stuck in the event
    queue along with them if they're used in the item widget's class or
    functions.

---
+++ obs/item-widget-helpers.hpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+    Copyright (C) 2015 by Hugh Bailey <obs.jim@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#pragma once
+
+/* This file exists to prevent ->deleteLater from being called on custom-made
+ * item widgets in widgets such as list widgets.  We do this to prevent things
+ * such as references to sources/etc from getting stuck in the Qt event queue
+ * with no way of controlling when they'll be released. */
+
+class QListWidget;
+class QListWidgetItem;
+
+QListWidgetItem *TakeListItem(QListWidget *widget, int row);
+void DeleteListItem(QListWidget *widget, QListWidgetItem *item);
+void ClearListItems(QListWidget *widget);
