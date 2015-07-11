1682d77df34a5c98ab24f41a273b29dd52e4f59e
Jess Mayo <jm-floss@chaoshouse.net>
2015-07-11 13:53:07 +0900

    obs-outputs: Fix rtmp authentication
    Reinstate flag checks in RTMP_Close that were erroneously removed.
    
    Clear out the Link state before we establish a new connection. There is
    too much state carried around during authentication that has no good
    place to clear it in librtmp, which assumes a clean structure when the
    connection is initially established.

--- plugins/obs-outputs/librtmp/rtmp.c
+++ plugins/obs-outputs/librtmp/rtmp.c
@@ -4221,14 +4221,17 @@ RTMP_Close(RTMP *r)
     }
 
 #if defined(CRYPTO) || defined(USE_ONLY_MD5)
-    for (int idx = 0; idx < r->Link.nStreams; idx++)
+    if (!(r->Link.protocol & RTMP_FEATURE_WRITE) || (r->Link.pFlags & RTMP_PUB_CLEAN))
     {
-        free(r->Link.streams[idx].playpath.av_val);
-        r->Link.streams[idx].playpath.av_val = NULL;
-    }
+        for (int idx = 0; idx < r->Link.nStreams; idx++)
+        {
+            free(r->Link.streams[idx].playpath.av_val);
+            r->Link.streams[idx].playpath.av_val = NULL;
+        }
 
-    r->Link.curStreamIdx = 0;
-    r->Link.nStreams = 0;
+        r->Link.curStreamIdx = 0;
+        r->Link.nStreams = 0;
+    }
 
     if ((r->Link.protocol & RTMP_FEATURE_WRITE) &&
             (r->Link.pFlags & RTMP_PUB_CLEAN) &&
