ffb3ca45958695f68875b4da6f627c49fc3b4496
Anthony Catel <paraboul@gmail.com>
2015-07-21 17:58:37 +0200

    libobs: Use one copy for RGBA output when possible
    A minor optimization: in copy_rgbx_frame (used when libobs is set to
    output RGBA frames instead of YUV frames), if the line sizes for the
    source and destination match, just use a single memcpy call for all of
    the data instead of multiple memcpy calls.

--- libobs/obs-video.c
+++ libobs/obs-video.c
@@ -473,10 +473,15 @@ static inline void copy_rgbx_frame(
 	uint8_t *in_ptr = input->data[0];
 	uint8_t *out_ptr = output->data[0];
 
-	for (size_t y = 0; y < info->height; y++) {
-		memcpy(out_ptr, in_ptr, info->width * 4);
-		in_ptr += input->linesize[0];
-		out_ptr += output->linesize[0];
+	/* if the line sizes match, do a single copy */
+	if (input->linesize[0] == output->linesize[0]) {
+		memcpy(out_ptr, in_ptr, input->linesize[0] * info->height);
+	} else {
+		for (size_t y = 0; y < info->height; y++) {
+			memcpy(out_ptr, in_ptr, info->width * 4);
+			in_ptr += input->linesize[0];
+			out_ptr += output->linesize[0];
+		}
 	}
 }
 
