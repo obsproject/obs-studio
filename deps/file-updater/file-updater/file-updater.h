d3eaeda27cb4ffafad65227b73b1b645502c50a8
jp9000 <obs.jim@gmail.com>
2015-08-19 15:48:04 -0700

    deps/file-updater: Add file updater util. lib
    This allows plugins to update and cache data files from a remote source.
    
    Here are the steps that occur when the API initiates an update check:
    
    1.) It checks to see if the local files are greater than the cached
        files.  If the local version is newer (for whatever reason), it
        replaces the cached version(s) with the local version.
    
    2.) A packages.json file is downloaded from the specified URL.  That
        packages.json file contains a version number and a list of files to
        be updated.
    
    3.) If the downloaded package version is greater than the cached
        version, executes step 4-5 on each file.
    
    4.) Checks the version for the file to update in packages.json, and if
        the version is greater than the cached version, proceeds to step 5,
        otherwise repeat step 4-5 for other files.
    
    5.) Calls the callback given to the update function (if any) with the
        file information (file name, buffer, etc), and if the callback
        returns true, allows the cached file to be updated and replaced,
        otherwise goes back to step 4-6 for the rest of the files.
    
    NOTE: Files are never modified directly.  All file saving/modification
    is performed in a temporary directory, and then files are moved to their
    destination.  This should eliminate any possibility of file corruption
    (or at least dramatically reduce the possibility).

---
+++ deps/file-updater/file-updater/file-updater.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct update_info;
+typedef struct update_info update_info_t;
+
+struct file_download_data {
+	const char *name;
+	int version;
+
+	DARRAY(uint8_t) buffer;
+};
+
+typedef bool (*confirm_file_callback_t)(void *param,
+		struct file_download_data *file);
+
+update_info_t *update_info_create(
+		const char *log_prefix,
+		const char *user_agent,
+		const char *update_url,
+		const char *local_dir,
+		const char *cache_dir,
+		confirm_file_callback_t confirm_callback,
+		void *param);
+void update_info_destroy(update_info_t *info);
