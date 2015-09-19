6d9bb63face09993675ac3e8762860c0acf756b6
jp9000 <obs.jim@gmail.com>
2015-09-19 12:28:33 -0700

    UI: Default to colorspace 601 instead of 709
    Seems that most decoders that don't read the h264 header tend to expect
    601 instead of 709.

--- obs/window-basic-main.cpp
+++ obs/window-basic-main.cpp
@@ -726,7 +726,7 @@ bool OBSBasic::InitBasicConfigDefaults()
 	config_set_default_uint  (basicConfig, "Video", "FPSDen", 1);
 	config_set_default_string(basicConfig, "Video", "ScaleType", "bicubic");
 	config_set_default_string(basicConfig, "Video", "ColorFormat", "NV12");
-	config_set_default_string(basicConfig, "Video", "ColorSpace", "709");
+	config_set_default_string(basicConfig, "Video", "ColorSpace", "601");
 	config_set_default_string(basicConfig, "Video", "ColorRange",
 			"Partial");
 
