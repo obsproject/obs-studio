obsproject/obs-studio

commit 6407707a0410fcd1d3e8b38534a8e2d7bf1ca1a2

UI: Ensure the output res. is modified along with base

obs/window-basic-settings.cpp
@@ -925,6 +925,8 @@ void OBSBasicSettings::ResetDownscales(uint32_t cx, uint32_t cy)
 		ui->outputResolution->lineEdit()->setText(bestScale.c_str());
 
 	ui->outputResolution->blockSignals(false);
+	ui->outputResolution->setProperty("changed", QVariant(true));
+	videoChanged = true;
 
 	if (advRescale.isEmpty())
 		advRescale = res.c_str();

commit fddb7d283df226416417c3d82cbf28638bd66f22

UI: Fix settings window data always being "changed"
Fixes a bug introduced in 6407707a04 where if you open the settings
window the settings would always think there were new changes.  It was
marking a control as changed when it shouldn't be

obs/window-basic-settings.cpp
@@ -919,14 +919,18 @@ void OBSBasicSettings::ResetDownscales(uint32_t cx, uint32_t cy)
 	float baseAspect   = float(cx) / float(cy);
 	float outputAspect = float(out_cx) / float(out_cy);
 
-	if (close_float(baseAspect, outputAspect, 0.01f))
+	bool closeAspect = close_float(baseAspect, outputAspect, 0.01f);
+	if (closeAspect)
 		ui->outputResolution->lineEdit()->setText(oldOutputRes);
 	else
 		ui->outputResolution->lineEdit()->setText(bestScale.c_str());
 
 	ui->outputResolution->blockSignals(false);
-	ui->outputResolution->setProperty("changed", QVariant(true));
-	videoChanged = true;
+
+	if (!closeAspect) {
+		ui->outputResolution->setProperty("changed", QVariant(true));
+		videoChanged = true;
+	}
 
 	if (advRescale.isEmpty())
 		advRescale = res.c_str();