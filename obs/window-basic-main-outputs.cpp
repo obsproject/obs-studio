560b69ecb35916c5d8f727655822f9daa8154437
jp9000 <obs.jim@gmail.com>
2016-02-09 19:24:44 -0800

    UI: Use uncompressed audio for lossless
    Originally there was an issue using FLAC for audio in AVI files, AVI
    files wouldn't play back with some players.  Because FLAC doesn't work,
    and lossless should be lossless, instead just used uncompressed WAV
    data for the lossless audio, which is always supported.

--- obs/window-basic-main-outputs.cpp
+++ obs/window-basic-main-outputs.cpp
@@ -155,8 +155,7 @@ void SimpleOutput::LoadRecordingPreset_Lossless()
 	obs_data_t *settings = obs_data_create();
 	obs_data_set_string(settings, "format_name", "avi");
 	obs_data_set_string(settings, "video_encoder", "utvideo");
-	obs_data_set_int(settings, "audio_bitrate", 512);
-	obs_data_set_string(settings, "audio_encoder", "ac3");
+	obs_data_set_string(settings, "audio_encoder", "pcm_s16le");
 
 	obs_output_update(fileOutput, settings);
 	obs_data_release(settings);
