2bdc1e9d7cd7f935fd34bbda225fd5d0192b1b69
Charles Ray Shisler III <charlesrayshisleriii@gmail.com>
2016-02-21 18:25:36 -0800

    linux-capture: Fix texture lingering after window closes
    After source window closes, texture is still visible in preview until
    the source window opens again.

--- plugins/linux-capture/xcompcap-main.cpp
+++ plugins/linux-capture/xcompcap-main.cpp
@@ -520,6 +520,10 @@ void XCompcapMain::tick(float seconds)
 void XCompcapMain::render(gs_effect_t *effect)
 {
 	PLock lock(&p->lock, true);
+
+	if (!p->win)
+		return;
+
 	effect = obs_get_base_effect(OBS_EFFECT_OPAQUE);
 
 	if (!lock.isLocked() || !p->tex)
