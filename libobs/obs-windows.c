2f6a10c93f49386b17ae1208d893cc94a1c46fd2
jp9000 <obs.jim@gmail.com>
2016-05-23 06:55:46 -0700

    libobs: Fix log message about max memory on 32bit
    Sometimes windows will report 2 gigs instead of 4 gigs max.

--- libobs/obs-windows.c
+++ libobs/obs-windows.c
@@ -163,7 +163,7 @@ static void log_available_memory(void)
 #ifdef _WIN64
 	const char *note = "";
 #else
-	const char *note = " (NOTE: 4 gigs max is normal for 32bit programs)";
+	const char *note = " (NOTE: 2 or 4 gigs max is normal for 32bit programs)";
 #endif
 
 	blog(LOG_INFO, "Physical Memory: %luMB Total, %luMB Free%s",
