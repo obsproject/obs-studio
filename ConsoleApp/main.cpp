ae26e34cf0ed2de80710a509b31f96655851bd64
João Portela <jportela@abyssal.eu>
2016-07-25 17:35:50 +0100

    Initial demo working. Still has some teardown issues but it's recording correctly.

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 #include<memory>
 
 #include "obs.h"
@@ -8,26 +9,6 @@
 struct AddSourceData {
 	obs_source_t *source;
 	bool visible;
-};
-
-struct obs_data {
-	volatile long        ref;
-	char                 *json;
-	struct obs_data_item *first_item;
-};
-
-struct obs_data_item {
-	volatile long        ref;
-	struct obs_data      *parent;
-	struct obs_data_item *next;
-	enum obs_data_type   type;
-	size_t               name_len;
-	size_t               data_len;
-	size_t               data_size;
-	size_t               default_len;
-	size_t               default_size;
-	size_t               autoselect_size;
-	size_t               capacity;
 };
 
 void print_obs_enum_input_types() {
@@ -41,6 +22,27 @@ void print_obs_enum_input_types() {
 		const char *name = obs_source_get_display_name(type);
 
 		std::cout << type << "\t" << name << std::endl;
+		foundValues = true;
+	}
+
+	if (!foundValues) {
+		std::cout << "Not Found!" << std::endl;
+	}
+	std::cout << "#############" << std::endl;
+}
+
+void print_obs_enum_encoder_types() {
+	const char *type;
+	bool foundValues = false;
+	size_t idx = 0;
+
+	std::cout << "Encoder types:" << std::endl;
+
+	while (obs_enum_encoder_types(idx++, &type)) {
+		const char *name = obs_encoder_get_display_name(type);
+		const char *codec = obs_get_encoder_codec(type);
+
+		std::cout << type << "\t" << name << "\t" << codec << std::endl;
 		foundValues = true;
 	}
 
@@ -81,21 +83,120 @@ bool printme(void *nulldata, obs_source_t *source)
 	return true;
 }
 
-obs_output_t* load_output() {
-	obs_output_t* fileOutput = obs_output_create("ffmpeg_output",
-		"simple_ffmpeg_output", nullptr, nullptr);
-	if (!fileOutput)
-		throw "Failed to create recording FFmpeg output "
-		"(simple output)";
-	//obs_output_release(fileOutput);
+void reset_video() {
+	struct obs_video_info ovi;
 
-	obs_data_t *settings = obs_data_create();
-	obs_data_set_string(settings, "format_name", "avi");
-	obs_data_set_string(settings, "video_encoder", "utvideo");
-	obs_data_set_string(settings, "audio_encoder", "pcm_s16le");
+	ovi.fps_num = 60;
+	ovi.fps_den = 1;
 
-	obs_output_update(fileOutput, settings);
-	// obs_data_release(settings);
+	ovi.graphics_module = "libobs-d3d11.dll"; // DL_D3D11
+	ovi.base_width = 1920;
+	ovi.base_height = 1080;
+	ovi.output_width = 1920;
+	ovi.output_height = 1080;
+	ovi.output_format = VIDEO_FORMAT_NV12;
+	ovi.colorspace = VIDEO_CS_601;
+	ovi.range = VIDEO_RANGE_PARTIAL;
+	ovi.adapter = 0;
+	ovi.gpu_conversion = true;
+	ovi.scale_type = OBS_SCALE_BICUBIC;
+
+	int ret = obs_reset_video(&ovi);
+	if (ret != OBS_VIDEO_SUCCESS) {
+		std::cout << "reset_video failed!" << std::endl;
+	}
+}
+
+void reset_audio() {
+	struct obs_audio_info ai;
+	ai.samples_per_sec = 44100;
+	ai.speakers = SPEAKERS_STEREO;
+
+	bool success = obs_reset_audio(&ai);
+	if (!success)
+		std::cout << "Audio reset failed!" << std::endl;
+}
+
+void setup_input() {
+	obs_source_t *source = obs_source_create("monitor_capture", "my capture 1", nullptr, nullptr);
+
+	{
+		obs_data_t *source_settings = obs_data_create();
+		obs_data_set_int(source_settings, "monitor", 0);
+		obs_data_set_bool(source_settings, "capture_cursor", true);
+
+		obs_source_update(source, source_settings);
+
+		obs_data_release(source_settings);
+	}
+
+	//if (source) {
+	//	obs_scene_t* scene1 = obs_scene_create("");
+	//	AddSourceData data;
+	//	data.source = source;
+	//	data.visible = true;
+
+	//	obs_sceneitem_t* sceneitem = obs_scene_add(scene1, data.source);
+	//	obs_sceneitem_set_visible(sceneitem, data.visible);
+
+	//	obs_data_t* settings = obs_source_get_settings(source);
+
+	//	//obs_data_t *settings = obs_data_create();
+	//}
+
+	// set this source as output.
+	obs_set_output_source(0, source);
+}
+
+obs_output_t* setup_output() {
+	obs_encoder_t* video_encoder = obs_video_encoder_create("obs_x264", "video_encoder", nullptr, nullptr);
+	obs_encoder_set_video(video_encoder, obs_get_video());
+	{
+		obs_data_t *encoder_settings = obs_data_create();
+		obs_data_set_string(encoder_settings, "rate_control", "CBR");
+		obs_data_set_int(encoder_settings, "bitrate", 2500);
+
+		obs_encoder_update(video_encoder, encoder_settings);
+
+		obs_data_release(encoder_settings);
+	}
+
+	obs_encoder_t* audio_encoder = obs_audio_encoder_create("mf_aac", "audio_encoder", nullptr, 0, nullptr);
+	obs_encoder_set_audio(audio_encoder, obs_get_audio());
+	{
+		obs_data_t *encoder_settings = obs_data_create();
+		obs_data_set_string(encoder_settings, "rate_control", "CBR");
+		obs_data_set_int(encoder_settings, "samplerate", 44100);
+		obs_data_set_int(encoder_settings, "bitrate", 160);
+		obs_data_set_default_bool(encoder_settings, "allow he-aac", true);
+
+		obs_encoder_update(audio_encoder, encoder_settings);
+
+		obs_data_release(encoder_settings);
+	}
+
+	obs_output_t* file_output = obs_output_create("ffmpeg_muxer", "simple_file_output", nullptr, nullptr);
+	{
+		obs_data_t* output_settings = obs_data_create();
+		obs_data_set_string(output_settings, "path", "E:\\libobs_vid\\video.mp4");
+		obs_data_set_string(output_settings, "muxer_settings", NULL);
+
+		obs_output_update(file_output, output_settings);
+
+		obs_data_release(output_settings);
+	}
+
+	obs_output_set_video_encoder(file_output, video_encoder);
+	obs_output_set_audio_encoder(file_output, audio_encoder, 0);
+
+	if (obs_output_start(file_output)) {
+		std::cout << "Recording stuff!!!" << std::endl;
+	}
+	else {
+		std::cout << "Fail!" << std::endl;
+	}
+
+	return file_output;
 }
 
 int main() {
@@ -107,39 +208,24 @@ int main() {
 		<< (obs_initialized() ? "successfully " : "not ")
 		<< "initialized." << std::endl;
 
-	/*const char *sceneCollection = config_get_string(App()->GlobalConfig(),
-		"Basic", "SceneCollectionFile");*/
-
+	reset_video();
+	reset_audio();
 	obs_load_all_modules();
 
 	print_obs_enum_input_types();
-
+	print_obs_enum_encoder_types();
 	print_obs_enum_output_types();
 
-	obs_scene_t* scene1 = obs_scene_create("Scene 1");
+	setup_input();
 
-	obs_source_t *source = obs_source_create("monitor_capture", "capture 1", nullptr, nullptr);
+	obs_output_t* fileOutput = setup_output();
 
-	if (source) {
-		AddSourceData data;
-		data.source = source;
-		data.visible = true;
+	std::string str;
+	std::getline(std::cin, str);
 
-		obs_sceneitem_t* sceneitem = obs_scene_add(scene1, data.source);
-		obs_sceneitem_set_visible(sceneitem, data.visible);
+	obs_output_stop(fileOutput);
 
-		obs_data_t* settings = obs_source_get_settings(source);
-
-		//obs_data_t *settings = obs_data_create();
-
-
-		std::cout << ":)" << std::endl;
-	}
-
-	obs_output_t *output = load_output();
-	obs_output_start(output);
-
-	std::cout << "-----" << std::endl;
+	std::cout << "-------" << std::endl;
 
 	return 0;
 }
