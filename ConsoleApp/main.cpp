f21480b1866be5e16c2756a2b9b64c8ada84e137
João Portela <jportela@abyssal.eu>
2016-07-26 12:39:24 +0100

    Improved command line interface.

--- ConsoleApp/main.cpp
+++ ConsoleApp/main.cpp
@@ -1,6 +1,13 @@
+/******************************************************************************
+	OBS Studio Console
+	Copyright(C) 2016  João Portela
+	******************************************************************************/
+
 #include<iostream>
 #include<string>
 #include<memory>
+
+#include<boost/program_options.hpp>
 
 #include "obs.h"
 #include "obs.hpp"
@@ -10,6 +17,15 @@ struct AddSourceData {
 	obs_source_t *source;
 	bool visible;
 };
+
+// default
+int monitor_to_record = 0;
+std::string encoder_selected = "obs_x264";
+std::string output_filepath = "default.mp4";
+
+std::string output_filepath2 = "default2.mp4";
+int video_bitrate = 2500;
+
 
 void print_obs_enum_input_types() {
 	const char *type;
@@ -122,8 +138,8 @@ void setup_input() {
 
 	{
 		obs_data_t *source_settings = obs_data_create();
-		obs_data_set_int(source_settings, "monitor", 0);
-		obs_data_set_bool(source_settings, "capture_cursor", true);
+		obs_data_set_int(source_settings, "monitor", monitor_to_record);
+		obs_data_set_bool(source_settings, "capture_cursor", false);
 
 		obs_source_update(source, source_settings);
 
@@ -149,12 +165,12 @@ void setup_input() {
 }
 
 obs_output_t* setup_output() {
-	obs_encoder_t* video_encoder = obs_video_encoder_create("obs_x264", "video_encoder", nullptr, nullptr);
+	obs_encoder_t* video_encoder = obs_video_encoder_create(encoder_selected.c_str(), "video_encoder", nullptr, nullptr);
 	obs_encoder_set_video(video_encoder, obs_get_video());
 	{
 		obs_data_t *encoder_settings = obs_data_create();
 		obs_data_set_string(encoder_settings, "rate_control", "CBR");
-		obs_data_set_int(encoder_settings, "bitrate", 2500);
+		obs_data_set_int(encoder_settings, "bitrate", video_bitrate);
 
 		obs_encoder_update(video_encoder, encoder_settings);
 
@@ -178,7 +194,7 @@ obs_output_t* setup_output() {
 	obs_output_t* file_output = obs_output_create("ffmpeg_muxer", "simple_file_output", nullptr, nullptr);
 	{
 		obs_data_t* output_settings = obs_data_create();
-		obs_data_set_string(output_settings, "path", "E:\\libobs_vid\\video.mp4");
+		obs_data_set_string(output_settings, "path", output_filepath.c_str());
 		obs_data_set_string(output_settings, "muxer_settings", NULL);
 
 		obs_output_update(file_output, output_settings);
@@ -186,10 +202,23 @@ obs_output_t* setup_output() {
 		obs_data_release(output_settings);
 	}
 
-	obs_output_set_video_encoder(file_output, video_encoder);
-	obs_output_set_audio_encoder(file_output, audio_encoder, 0);
+	obs_output_t* file_output2 = obs_output_create("ffmpeg_muxer", "simple_file_output2", nullptr, nullptr);
+	{
+		obs_data_t* output_settings2 = obs_data_create();
+		obs_data_set_string(output_settings2, "path", output_filepath2.c_str());
+		obs_data_set_string(output_settings2, "muxer_settings", NULL);
 
-	if (obs_output_start(file_output)) {
+		obs_output_update(file_output2, output_settings2);
+
+		obs_data_release(output_settings2);
+	}
+
+	obs_output_set_video_encoder(file_output, video_encoder);
+	obs_output_set_video_encoder(file_output2, video_encoder);
+	obs_output_set_audio_encoder(file_output, audio_encoder, 0);
+	obs_output_set_audio_encoder(file_output2, audio_encoder, 0);
+
+	if (obs_output_start(file_output) && obs_output_start(file_output2)) {
 		std::cout << "Recording stuff!!!" << std::endl;
 	}
 	else {
@@ -199,7 +228,93 @@ obs_output_t* setup_output() {
 	return file_output;
 }
 
-int main() {
+int parse_args(int argc, char **argv) {
+	namespace po = boost::program_options;
+
+	// Declare the supported options.
+	po::options_description desc("\n****************************************************************\nAllowed options");
+	desc.add_options()
+		("help,h", "produce help message")
+		("monitor,m", po::value<int>(), "set monitor to be recorded")
+		("encoder,e", po::value<std::string>(), "set encoder")
+		("vbitrate,v", po::value<int>(), "set video bitrate. suggested values: 1200 for low, 2500 for medium, 5000 for high")
+		("output,o", po::value<std::string>(), "set file destination")
+		("secondary_output,s", po::value<std::string>(), "set file destination")
+		;
+
+	try {
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			std::cout << desc << "\n";
+			//print_obs_enum_input_types();
+			print_obs_enum_encoder_types();
+			//print_obs_enum_output_types();
+			return 1;
+		}
+
+		if (vm.count("monitor")) {
+			std::cout << "Monitor was set to "
+				<< vm["monitor"].as<int>() << ".\n";
+			monitor_to_record = vm["monitor"].as<int>();
+		}
+		else {
+			std::cout << "Monitor not set.\n";
+			return 2;
+		}
+
+		if (vm.count("encoder")) {
+			std::cout << "Encoder was set to "
+				<< vm["encoder"].as<std::string>() << ".\n";
+			encoder_selected = vm["encoder"].as<std::string>();
+		}
+		else {
+			std::cout << "Encoder not set.\n";
+			return 3;
+		}
+
+		if (vm.count("vbitrate")) {
+			std::cout << "Video bitrate was set to "
+				<< vm["vbitrate"].as<int>() << ".\n";
+			video_bitrate = vm["vbitrate"].as<int>();
+		}
+		else {
+			std::cout << "Encoder not set.\n";
+			return 4;
+		}
+
+		if (vm.count("output")) {
+			std::cout << "Output was set to "
+				<< vm["output"].as<std::string>() << ".\n";
+			output_filepath = vm["output"].as<std::string>();
+		}
+		else {
+			std::cout << "Output not set.\n";
+			return 5;
+		}
+		if (vm.count("secondary_output")) {
+			std::cout << "Secondary output was set to "
+				<< vm["secondary_output"].as<std::string>() << ".\n";
+			output_filepath2 = vm["secondary_output"].as<std::string>();
+		}
+		else {
+			std::cout << "Secondary output not set.\n";
+		}
+
+	}
+	catch (po::error& e) {
+		std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+		std::cerr << desc << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+
 	// manages object context so that we don't have to call
 	// obs_startup and obs_shutdown.
 	OBSContext ObsScope("en-US", nullptr, nullptr);
@@ -212,10 +327,17 @@ int main() {
 	reset_audio();
 	obs_load_all_modules();
 
+
+	//After Initializing OBS so that the help can print available encoders, etc.
+	int ret = parse_args(argc, argv);
+	if (ret != 0)
+		return ret;
+
+	/*
 	print_obs_enum_input_types();
 	print_obs_enum_encoder_types();
 	print_obs_enum_output_types();
-
+*/
 	setup_input();
 
 	obs_output_t* fileOutput = setup_output();
