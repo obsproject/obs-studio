498051b8f0a5e10248768d8185f0a14f9809d95e
jp9000 <obs.jim@gmail.com>
2016-07-27 10:02:12 -0700

    obs-filters: Fix macro check
    This should be checking true or false -- not whether it's defined or
    not.

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -15,7 +15,7 @@ extern struct obs_source_info color_key_filter;
 extern struct obs_source_info sharpness_filter;
 extern struct obs_source_info chroma_key_filter;
 extern struct obs_source_info async_delay_filter;
-#ifdef SPEEXDSP_ENABLED
+#if SPEEXDSP_ENABLED
 extern struct obs_source_info noise_suppress_filter;
 #endif
 extern struct obs_source_info noise_gate_filter;
@@ -32,7 +32,7 @@ bool obs_module_load(void)
 	obs_register_source(&sharpness_filter);
 	obs_register_source(&chroma_key_filter);
 	obs_register_source(&async_delay_filter);
-#ifdef SPEEXDSP_ENABLED
+#if SPEEXDSP_ENABLED
 	obs_register_source(&noise_suppress_filter);
 #endif
 	obs_register_source(&noise_gate_filter);
