cbc1f6399004297c4c316a62ac1ef250caab6fe5
jp9000 <obs.jim@gmail.com>
2016-08-06 19:42:02 -0700

    vlc-video: Allow all VLC-supported file types
    Instead of just having a limited number of video file types that can be
    used, allow all files types that VLC supports.

--- plugins/vlc-video/vlc-video-plugin.h
+++ plugins/vlc-video/vlc-video-plugin.h
@@ -154,3 +154,76 @@ extern LIBVLC_MEDIA_LIST_PLAYER_SET_MEDIA_PLAYER libvlc_media_list_player_set_me
 extern LIBVLC_MEDIA_LIST_PLAYER_SET_MEDIA_LIST libvlc_media_list_player_set_media_list_;
 extern LIBVLC_MEDIA_LIST_PLAYER_EVENT_MANAGER libvlc_media_list_player_event_manager_;
 extern LIBVLC_MEDIA_LIST_PLAYER_SET_PLAYBACK_MODE libvlc_media_list_player_set_playback_mode_;
+
+#define EXTENSIONS_AUDIO \
+	"*.3ga;" \
+	"*.669;" \
+	"*.a52;" \
+	"*.aac;" \
+	"*.ac3;" \
+	"*.adt;" \
+	"*.adts;" \
+	"*.aif;"\
+	"*.aifc;"\
+	"*.aiff;"\
+	"*.amb;" \
+	"*.amr;" \
+	"*.aob;" \
+	"*.ape;" \
+	"*.au;" \
+	"*.awb;" \
+	"*.caf;" \
+	"*.dts;" \
+	"*.flac;"\
+	"*.it;"  \
+	"*.kar;" \
+	"*.m4a;" \
+	"*.m4b;" \
+	"*.m4p;" \
+	"*.m5p;" \
+	"*.mid;" \
+	"*.mka;" \
+	"*.mlp;" \
+	"*.mod;" \
+	"*.mpa;" \
+	"*.mp1;" \
+	"*.mp2;" \
+	"*.mp3;" \
+	"*.mpc;" \
+	"*.mpga;" \
+	"*.mus;" \
+	"*.oga;" \
+	"*.ogg;" \
+	"*.oma;" \
+	"*.opus;" \
+	"*.qcp;" \
+	"*.ra;" \
+	"*.rmi;" \
+	"*.s3m;" \
+	"*.sid;" \
+	"*.spx;" \
+	"*.tak;" \
+	"*.thd;" \
+	"*.tta;" \
+	"*.voc;" \
+	"*.vqf;" \
+	"*.w64;" \
+	"*.wav;" \
+	"*.wma;" \
+	"*.wv;"  \
+	"*.xa;"  \
+	"*.xm"
+
+#define EXTENSIONS_VIDEO "*.3g2;*.3gp;*.3gp2;*.3gpp;*.amv;*.asf;*.avi;" \
+	"*.bik;*.bin;*.crf;*.divx;*.drc;*.dv;*.evo;*.f4v;*.flv;*.gvi;*.gxf;" \
+	"*.iso;*.m1v;*.m2v;*.m2t;*.m2ts;*.m4v;*.mkv;*.mov;*.mp2;*.mp2v;*.mp4;" \
+	"*.mp4v;*.mpe;*.mpeg;*.mpeg1;*.mpeg2;*.mpeg4;*.mpg;*.mpv2;*.mts;" \
+	"*.mtv;*.mxf;*.mxg;*.nsv;*.nuv;*.ogg;*.ogm;*.ogv;*.ogx;*.ps;*.rec;" \
+	"*.rm;*.rmvb;*.rpl;*.thp;*.tod;*.ts;*.tts;*.txd;*.vob;*.vro;*.webm;" \
+	"*.wm;*.wmv;*.wtv;*.xesc"
+
+#define EXTENSIONS_PLAYLIST "*.asx;*.b4s;*.cue;*.ifo;*.m3u;*.m3u8;*.pls;" \
+	"*.ram;*.rar;*.sdp;*.vlc;*.xspf;*.wax;*.wvx;*.zip;*.conf"
+
+#define EXTENSIONS_MEDIA EXTENSIONS_VIDEO ";" EXTENSIONS_AUDIO ";" \
+	EXTENSIONS_PLAYLIST
