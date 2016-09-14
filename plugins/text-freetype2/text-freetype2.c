5af1a2b5bbf9efddf1b27636bd74127860257658
jp9000 <obs.jim@gmail.com>
2016-09-14 16:28:55 -0700

    text-freetype2: Deprecate on windows

--- plugins/text-freetype2/text-freetype2.c
+++ plugins/text-freetype2/text-freetype2.c
@@ -35,6 +35,9 @@ static struct obs_source_info freetype2_source_info = {
 	.id = "text_ft2_source",
 	.type = OBS_SOURCE_TYPE_INPUT,
 	.output_flags = OBS_SOURCE_VIDEO |
+#ifdef _WIN32
+	                OBS_SOURCE_DEPRECATED |
+#endif
 	                OBS_SOURCE_CUSTOM_DRAW,
 	.get_name = ft2_source_get_name,
 	.create = ft2_source_create,
