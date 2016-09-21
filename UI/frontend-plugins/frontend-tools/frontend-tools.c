bde23ee1a6e8ef92c2ee5ed3818e439e75232846
cg2121 <claytong1214@gmail.com>
2016-09-21 03:51:18 -0500

    frontend-tools: Add output timers
    This adds streaming and recording timers to the frontend tools.

--- UI/frontend-plugins/frontend-tools/frontend-tools.c
+++ UI/frontend-plugins/frontend-tools/frontend-tools.c
@@ -3,16 +3,57 @@
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("frontend-tools", "en-US")
 
+#ifdef _WIN32
 void InitSceneSwitcher();
 void FreeSceneSwitcher();
+void InitOutputTimer();
+void FreeOutputTimer();
 
 bool obs_module_load(void)
 {
 	InitSceneSwitcher();
+	InitOutputTimer();
 	return true;
 }
 
 void obs_module_unload(void)
 {
-	FreeSceneSwitcher();
+	FreeSceneSwitcher;
+	FreeOutputTimer();
 }
+
+#elif _APPLE_
+void InitSceneSwitcher();
+void FreeSceneSwitcher();
+void InitOutputTimer();
+void FreeOutputTimer();
+
+bool obs_module_load(void)
+{
+	InitSceneSwitcher();
+	InitOutputTimer();
+	return true;
+}
+
+void obs_module_unload(void)
+{
+	FreeSceneSwitcher;
+	FreeOutputTimer();
+}
+
+#else
+void InitOutputTimer();
+void FreeOutputTimer();
+
+bool obs_module_load(void)
+{
+	InitOutputTimer();
+	return true;
+}
+
+void obs_module_unload(void)
+{
+	FreeOutputTimer();
+}
+
+#endif
