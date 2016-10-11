947d04012ffc65e6b843b9568fd44a8c2461cc0f
Colin Edwards <colin@recursivepenguin.com>
2016-10-10 22:49:45 -0500

    Build browser plugin on travis for OSX

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake ..
+cmake -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_3.2704.1434.gec3e9ed_macosx64 ..
--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -3,3 +3,15 @@ brew update
 #Base OBS Deps
 brew install ffmpeg x264 qt5 python
 
+# CEF Stuff
+cd ../
+curl -kLO http://opensource.spotify.com/cefbuilds/cef_binary_3.2704.1434.gec3e9ed_macosx64.tar.bz2
+tar -xf ./cef_binary_3.2704.1434.gec3e9ed_macosx64.tar.bz2
+cd ./cef_binary_3.2704.1434.gec3e9ed_macosx64
+mkdir build
+cd ./build
+cmake -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_EXE_LINKER_FLAGS="-std=c++11 -stdlib=libc++" ..
+make -j4
+mkdir libcef_dll
+mv ./libcef_dll_wrapper/libcef_dll_wrapper.a ./libcef_dll/libcef_dll_wrapper.a
+cd ../../
