a0b4439893964a23f91b57891295597436b8b4ba
Colin Edwards <colin@recursivepenguin.com>
2016-10-11 20:47:22 -0500

    CI: Package OSX build into an actual app

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -2,6 +2,7 @@ export GIT_HASH=$(git rev-parse --short HEAD)
 export FILE_DATE=$(date +%Y-%m-%d.%H:%M:%S)
 export FILENAME=$FILE_DATE-$GIT_HASH-osx.zip
 mkdir nightly
-cd ./build/rundir/RelWithDebInfo
-zip -r -X $FILENAME .
-mv ./$FILENAME ../../../nightly
+cd ./build
+sudo python3 ../CI/install/osx/build_app.py
+zip -r -X $FILENAME OBS.app
+mv ./$FILENAME ../nightly
