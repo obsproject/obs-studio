64559a0cb8c9f4991c3a06786da9e89e9ba1cd45
Serge Paquet <spaquet@cortex.bz>
2016-11-24 07:11:08 -0800

    cmake: Fix permissions with OSX fixup_bundle.sh
    Fixes an issue where the files copied by fixup_bundle.sh will not work
    with install_name_tool without proper permissions.
    
    Closes jp9000/obs-studio#618

--- cmake/osxbundle/fixup_bundle.sh
+++ cmake/osxbundle/fixup_bundle.sh
@@ -36,6 +36,7 @@ for lib in $FOUNDLIBS; do
 
 	INTOOL_CALL+=(-change "$lib" "$LDEST/$libname")
 	cp "$lib" "$DEST/$libname"
+	chmod 644 "$DEST/$libname"
 
 	echo "Fixing up dependency: $libname"
 done
