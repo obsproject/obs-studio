47fe89b96435116fca6dd009d2d0c3cecdab1d35
jp9000 <obs.jim@gmail.com>
2016-12-07 05:18:32 -0800

    UI: Disable simple output rec. settings when active

--- UI/window-basic-settings.cpp
+++ UI/window-basic-settings.cpp
@@ -1554,6 +1554,7 @@ void OBSBasicSettings::LoadOutputSettings()
 	if (video_output_active(obs_get_video())) {
 		ui->outputMode->setEnabled(false);
 		ui->outputModeLabel->setEnabled(false);
+		ui->simpleRecordingGroupBox->setEnabled(false);
 		ui->advOutTopContainer->setEnabled(false);
 		ui->advOutRecTopContainer->setEnabled(false);
 		ui->advOutRecTypeContainer->setEnabled(false);
