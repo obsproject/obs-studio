afcdb72762ea5e2ba6e68432e7f47cd212ff6ff4
Cephas Reis <c3r1c3@nevermindonline.com>
2016-12-22 23:28:10 -0600

    obs-filters: Add "Color" Option to "Color Correction" Filter

--- plugins/obs-filters/color-correction-filter.c
+++ plugins/obs-filters/color-correction-filter.c
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SETTING_SATURATION             "saturation"
 #define SETTING_HUESHIFT               "hue_shift"
 #define SETTING_OPACITY                "opacity"
+#define SETTING_COLOR                  "color"
 
 #define TEXT_GAMMA                     obs_module_text("Gamma")
 #define TEXT_CONTRAST                  obs_module_text("Contrast")
@@ -32,6 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TEXT_SATURATION                obs_module_text("Saturation")
 #define TEXT_HUESHIFT                  obs_module_text("HueShift")
 #define TEXT_OPACITY                   obs_module_text("Opacity")
+#define TEXT_COLOR                     obs_module_text("Color")
 
 struct color_correction_filter_data {
 	obs_source_t                   *context;
@@ -47,12 +49,14 @@ struct color_correction_filter_data {
 	float                           saturation;
 	float                           hue_shift;
 	float                           opacity;
+	struct vec4                     color;
 
 	/* Pre-Computes */
 	struct matrix4                  con_matrix;
 	struct matrix4                  bright_matrix;
 	struct matrix4                  sat_matrix;
 	struct matrix4                  hue_op_matrix;
+	struct matrix4                  color_matrix;
 	struct matrix4                  final_matrix;
 
 	struct vec3                     rot_quaternion;
@@ -69,9 +73,9 @@ struct color_correction_filter_data {
 const static float root3 = 0.57735f;
 
 /*
-* As the functions' namesake, this provides the user facing name
-* of your Filter.
-*/
+ * As the functions' namesake, this provides the internal name of your Filter,
+ * which is then translated/referenced in the "data/locale" files.
+ */
 static const char *color_correction_filter_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
@@ -79,11 +83,11 @@ static const char *color_correction_filter_name(void *unused)
 }
 
 /*
-* This function is called (see bottom of this file for more details)
-* whenever the OBS filter interface changes. So when the user is messing
-* with a slider this function is called to update the internal settings
-* in OBS, and hence the settings being passed to the CPU/GPU.
-*/
+ * This function is called (see bottom of this file for more details)
+ * whenever the OBS filter interface changes. So when the user is messing
+ * with a slider this function is called to update the internal settings
+ * in OBS, and hence the settings being passed to the CPU/GPU.
+ */
 static void color_correction_filter_update(void *data, obs_data_t *settings)
 {
 	struct color_correction_filter_data *filter = data;
@@ -147,7 +151,8 @@ static void color_correction_filter_update(void *data, obs_data_t *settings)
 			SETTING_OPACITY) * 0.01f;
 
 	/* Hue is the radian of 0 to 360 degrees. */
-	float half_angle = 0.5f * (float)(filter->hue_shift / (180.0f / M_PI));
+	float half_angle = 0.5f * (float)(filter->hue_shift /
+			(180.0f / M_PI));
 
 	/* Pseudo-Quaternion To Matrix. */
 	float rot_quad1 = root3 * (float)sin(half_angle);
@@ -188,22 +193,48 @@ static void color_correction_filter_update(void *data, obs_data_t *settings)
 		0.0f, 0.0f, 0.0f, filter->opacity
 	};
 
+	/* Now get the overlay color data. */
+	uint32_t color = (uint32_t)obs_data_get_int(settings,
+			SETTING_COLOR);
+	vec4_from_rgba(&filter->color, color);
+
+	/*
+	* Now let's build our Color 'overlay' matrix.
+	* Earlier (in the function color_correction_filter_create) we set
+	* this matrix to the identity matrix, so now we only need
+	* to set the 6 variables that have changed.
+	*/
+	filter->color_matrix.x.x = filter->color.x;
+	filter->color_matrix.y.y = filter->color.y;
+	filter->color_matrix.z.z = filter->color.z;
+
+	filter->color_matrix.t.x = filter->color.w *
+			filter->color.x;
+	filter->color_matrix.t.y = filter->color.w *
+			filter->color.y;
+	filter->color_matrix.t.z = filter->color.w *
+			filter->color.z;
+
+
 	/* First we apply the Contrast & Brightness matrix. */
 	matrix4_mul(&filter->final_matrix, &filter->bright_matrix,
 			&filter->con_matrix);
 	/* Now we apply the Saturation matrix. */
 	matrix4_mul(&filter->final_matrix, &filter->final_matrix,
 			&filter->sat_matrix);
-	/* Lastly we apply the Hue+Opacity matrix. */
+	/* Next we apply the Hue+Opacity matrix. */
 	matrix4_mul(&filter->final_matrix, &filter->final_matrix,
 			&filter->hue_op_matrix);
+	/* Lastly we apply the Color Wash matrix. */
+	matrix4_mul(&filter->final_matrix, &filter->final_matrix,
+			&filter->color_matrix);
 }
 
 /*
-* Since this is C we have to be careful when destroying/removing items from
-* OBS. Jim has added several useful functions to help keep memory leaks to
-* a minimum, and handle the destruction and construction of these filters.
-*/
+ * Since this is C we have to be careful when destroying/removing items from
+ * OBS. Jim has added several useful functions to help keep memory leaks to
+ * a minimum, and handle the destruction and construction of these filters.
+ */
 static void color_correction_filter_destroy(void *data)
 {
 	struct color_correction_filter_data *filter = data;
@@ -218,17 +249,17 @@ static void color_correction_filter_destroy(void *data)
 }
 
 /*
-* When you apply a filter OBS creates it, and adds it to the source. OBS also
-* starts rendering it immediately. This function doesn't just 'create' the
-* filter, it also calls the render function (farther below) that contains the
-* actual rendering code.
-*/
+ * When you apply a filter OBS creates it, and adds it to the source. OBS also
+ * starts rendering it immediately. This function doesn't just 'create' the
+ * filter, it also calls the render function (farther below) that contains the
+ * actual rendering code.
+ */
 static void *color_correction_filter_create(obs_data_t *settings,
 	obs_source_t *context)
 {
 	/*
 	* Because of limitations of pre-c99 compilers, you can't create an
-	* array that doesn't have a know size at compile time. The below
+	* array that doesn't have a known size at compile time. The below
 	* function calculates the size needed and allocates memory to
 	* handle the source.
 	*/
@@ -238,7 +269,7 @@ static void *color_correction_filter_create(obs_data_t *settings,
 	/*
 	 * By default the effect file is stored in the ./data directory that
 	 * your filter resides in.
-	*/
+	 */
 	char *effect_path = obs_module_file("color_correction_filter.effect");
 
 	filter->context = context;
@@ -246,6 +277,7 @@ static void *color_correction_filter_create(obs_data_t *settings,
 	/* Set/clear/assign for all necessary vectors. */
 	vec3_set(&filter->half_unit, 0.5f, 0.5f, 0.5f);
 	matrix4_identity(&filter->bright_matrix);
+	matrix4_identity(&filter->color_matrix);
 
 	/* Here we enter the GPU drawing/shader portion of our code. */
 	obs_enter_graphics();
@@ -276,10 +308,10 @@ static void *color_correction_filter_create(obs_data_t *settings,
 	}
 
 	/*
-	* It's important to call the update function here. if we don't
-	* we could end up with the user controlled sliders and values
-	* updating, but the visuals not updating to match.
-	*/
+	 * It's important to call the update function here. If we don't
+	 * we could end up with the user controlled sliders and values
+	 * updating, but the visuals not updating to match.
+	 */
 	color_correction_filter_update(filter, settings);
 	return filter;
 }
@@ -295,7 +327,8 @@ static void color_correction_filter_render(void *data, gs_effect_t *effect)
 
 	/* Now pass the interface variables to the .effect file. */
 	gs_effect_set_vec3(filter->gamma_param, &filter->gamma);
-	gs_effect_set_matrix4(filter->final_matrix_param, &filter->final_matrix);
+	gs_effect_set_matrix4(filter->final_matrix_param,
+			&filter->final_matrix);
 
 	obs_source_process_filter_end(filter->context, filter->effect, 0, 0);
 
@@ -303,11 +336,11 @@ static void color_correction_filter_render(void *data, gs_effect_t *effect)
 }
 
 /*
-* This function sets the interface. the types (add_*_Slider), the type of
-* data collected (int), the internal name, user-facing name, minimum,
-* maximum and step values. While a custom interface can be built, for a
-* simple filter like this it's better to use the supplied functions.
-*/
+ * This function sets the interface. the types (add_*_Slider), the type of
+ * data collected (int), the internal name, user-facing name, minimum,
+ * maximum and step values. While a custom interface can be built, for a
+ * simple filter like this it's better to use the supplied functions.
+ */
 static obs_properties_t *color_correction_filter_properties(void *data)
 {
 	obs_properties_t *props = obs_properties_create();
@@ -326,17 +359,19 @@ static obs_properties_t *color_correction_filter_properties(void *data)
 	obs_properties_add_int_slider(props, SETTING_OPACITY,
 			TEXT_OPACITY, 0, 100, 1);
 
+	obs_properties_add_color(props, SETTING_COLOR, TEXT_COLOR);
+
 	UNUSED_PARAMETER(data);
 	return props;
 }
 
 /*
  * As the functions' namesake, this provides the default settings for any
-* options you wish to provide a default for. Try to select defaults that
-* make sense to the end user, or that don't effect the data.
-* *NOTE* this function is completely optional, as is providing a default
-* for any particular setting.
-*/
+ * options you wish to provide a default for. Try to select defaults that
+ * make sense to the end user, or that don't effect the data.
+ * *NOTE* this function is completely optional, as is providing a default
+ * for any particular setting.
+ */
 static void color_correction_filter_defaults(obs_data_t *settings)
 {
 	obs_data_set_default_double(settings, SETTING_GAMMA, 0.0);
@@ -346,27 +381,28 @@ static void color_correction_filter_defaults(obs_data_t *settings)
 			SETTING_SATURATION, 0.0);
 	obs_data_set_default_double(settings, SETTING_HUESHIFT, 0.0);
 	obs_data_set_default_double(settings, SETTING_OPACITY, 100.0);
+	obs_data_set_default_int(settings, SETTING_COLOR, 0xFFFFFF);
 }
 
 /*
-* So how does OBS keep track of all these plug-ins/filters? How does OBS know
-* which function to call when it needs to update a setting? Or a source? Or
-* what type of source this is?
-*
-* OBS does it through the obs_source_info_struct. Notice how variables are
-* assigned the name of a function? Notice how the function name has the
-* variable name in it? While not mandatory, it helps a ton for you (and those
-* reading your code) to follow this convention.
-*/
+ * So how does OBS keep track of all these plug-ins/filters? How does OBS know
+ * which function to call when it needs to update a setting? Or a source? Or
+ * what type of source this is?
+ *
+ * OBS does it through the obs_source_info_struct. Notice how variables are
+ * assigned the name of a function? Notice how the function name has the
+ * variable name in it? While not mandatory, it helps a ton for you (and those
+ * reading your code) to follow this convention.
+ */
 struct obs_source_info color_filter = {
-	.id = "color_filter",
-	.type = OBS_SOURCE_TYPE_FILTER,
-	.output_flags = OBS_SOURCE_VIDEO,
-	.get_name = color_correction_filter_name,
-	.create = color_correction_filter_create,
-	.destroy = color_correction_filter_destroy,
-	.video_render = color_correction_filter_render,
-	.update = color_correction_filter_update,
-	.get_properties = color_correction_filter_properties,
-	.get_defaults = color_correction_filter_defaults
+	.id =                          "color_filter",
+	.type =                         OBS_SOURCE_TYPE_FILTER,
+	.output_flags =                 OBS_SOURCE_VIDEO,
+	.get_name =                     color_correction_filter_name,
+	.create =                       color_correction_filter_create,
+	.destroy =                      color_correction_filter_destroy,
+	.video_render =                 color_correction_filter_render,
+	.update =                       color_correction_filter_update,
+	.get_properties =               color_correction_filter_properties,
+	.get_defaults =                 color_correction_filter_defaults
 };
