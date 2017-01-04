953b8d04d304b41ce9f0bd87e144ccd56ed0f40b
mape <mape@ma.pe>
2017-01-04 17:37:38 +0100

    obs-filters: Add "Color Grading" filter This allows for color grading based on a color look-up table. Commonly used to color correct and/or grade video that has been shot in Log.

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -12,6 +12,7 @@ extern struct obs_source_info color_filter;
 extern struct obs_source_info scale_filter;
 extern struct obs_source_info scroll_filter;
 extern struct obs_source_info color_key_filter;
+extern struct obs_source_info color_grade_filter;
 extern struct obs_source_info sharpness_filter;
 extern struct obs_source_info chroma_key_filter;
 extern struct obs_source_info async_delay_filter;
@@ -29,6 +30,7 @@ bool obs_module_load(void)
 	obs_register_source(&scale_filter);
 	obs_register_source(&scroll_filter);
 	obs_register_source(&color_key_filter);
+	obs_register_source(&color_grade_filter);
 	obs_register_source(&sharpness_filter);
 	obs_register_source(&chroma_key_filter);
 	obs_register_source(&async_delay_filter);
