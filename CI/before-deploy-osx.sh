836031a7ba8ea64d85f594ca80d6cc711f2c2032
Colin Edwards <colin@recursivepenguin.com>
2017-01-12 20:52:30 -0600

    CI: Ability to make packages on travis

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -1,8 +1,15 @@
+curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/packagesbuild
+chmod +x ./packagesbuild
+curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/packagesutil
+chmod +x ./packagesutil
+
 export GIT_HASH=$(git rev-parse --short HEAD)
 export FILE_DATE=$(date +%Y-%m-%d.%H:%M:%S)
-export FILENAME=$FILE_DATE-$GIT_HASH-osx.zip
+export FILENAME=$FILE_DATE-$GIT_HASH-osx.pkg
 mkdir nightly
 cd ./build
 sudo python ../CI/install/osx/build_app.py
-sudo zip -r -X $FILENAME OBS.app
+packagesbuild ../CI/install/osx/CMakeLists.pkgproj
+
+sudo cp OBS.pkg ./$FILENAME
 sudo mv ./$FILENAME ../nightly
