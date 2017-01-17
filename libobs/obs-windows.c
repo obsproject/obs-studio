6b52f0fe32c948e697312bcde4b0bf642f64372f
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2017-01-17 10:11:27 -0200

    Return the correct amount of memory on x86 applications.

--- libobs/obs-windows.c
+++ libobs/obs-windows.c
@@ -157,19 +157,14 @@ static void log_processor_cores(void)
 
 static void log_available_memory(void)
 {
-	MEMORYSTATUS ms;
-	GlobalMemoryStatus(&ms);
+	MEMORYSTATUSEX ms;
+	ms.dwLength = sizeof(ms);
 
-#ifdef _WIN64
-	const char *note = "";
-#else
-	const char *note = " (NOTE: 2 or 4 gigs max is normal for 32bit programs)";
-#endif
+	GlobalMemoryStatusEx(&ms);
 
-	blog(LOG_INFO, "Physical Memory: %luMB Total, %luMB Free%s",
-			(DWORD)(ms.dwTotalPhys / 1048576),
-			(DWORD)(ms.dwAvailPhys / 1048576),
-			note);
+	blog(LOG_INFO, "Physical Memory: %luMB Total, %luMB Free",
+			(DWORD)(ms.ullTotalPhys / 1048576),
+			(DWORD)(ms.ullAvailPhys / 1048576));
 }
 
 static void log_windows_version(void)
