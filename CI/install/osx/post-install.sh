ff3c82a507644f257df581388310e77fda776643
David Cooper <davidc@eventbrite.com>
2017-02-04 18:51:56 -0800

    Fix OSX Post Install Script

--- CI/install/osx/post-install.sh
+++ CI/install/osx/post-install.sh
@@ -2,3 +2,4 @@
 
 # Fix permissions on CEF
 chmod 744 "/Library/Application Support/obs-studio/plugins/obs-browser/bin/CEF.app/Contents/Info.plist"
+chmod 744 "/Library/Application Support/obs-studio/plugins/obs-browser/bin/CEF.app/Contents/Frameworks/CEF Helper.app/Contents/Info.plist"
