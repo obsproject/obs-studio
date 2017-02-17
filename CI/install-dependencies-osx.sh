e99c06ad6add948c7cf3abb8e4fc9383bcaa6ec7
juvester <juvester@cs.helsinki.fi>
2017-02-17 04:28:22 +0200

    CI: Retry failed downloads
    If all retries fail, stop the build.

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -1,7 +1,10 @@
+# Exit if something fails
+set -e
+
 # Leave obs-studio folder
 cd ../
 
-curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg
+curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg -f --retry 5 -C -
 sudo installer -pkg ./Packages.pkg -target /
 
 brew update
@@ -9,14 +12,14 @@ brew update
 #Base OBS Deps
 brew install qt5
 
-curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz
+curl -L -O https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz -f --retry 5 -C -
 tar -xf ./osx-deps.tar.gz -C /tmp
 
-curl -L -o vlc-master.zip https://github.com/videolan/vlc/archive/master.zip
+curl -L -o vlc-master.zip https://github.com/videolan/vlc/archive/master.zip -f --retry 5 -C -
 unzip -q ./vlc-master.zip
 
 # CEF Stuff
-curl -kLO http://opensource.spotify.com/cefbuilds/cef_binary_3.2883.1540.gedbfb20_macosx64.tar.bz2
+curl -kLO http://opensource.spotify.com/cefbuilds/cef_binary_3.2883.1540.gedbfb20_macosx64.tar.bz2 -f --retry 5 -C -
 tar -xf ./cef_binary_3.2883.1540.gedbfb20_macosx64.tar.bz2
 cd ./cef_binary_3.2883.1540.gedbfb20_macosx64
 mkdir build
