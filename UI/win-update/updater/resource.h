86862b672ed8accdd6758536812d07efdc8b8981
jp9000 <obs.jim@gmail.com>
2017-02-20 04:50:17 -0800

    updater: Add windows updater module

---
+++ UI/win-update/updater/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by updater.rc
+//
+#define IDD_UPDATEDIALOG                101
+#define IDI_ICON1                       103
+#define IDC_PROGRESS                    1001
+#define IDC_STATUS                      1002
+#define IDCBUTTON                       1004
+#define IDC_BUTTON                      1004
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
