48abb05de471ac30dbca20efee71421c4446775e
Colin Edwards <colin@recursivepenguin.com>
2017-02-26 16:16:04 -0600

    CI: Use combined cert for signing OSX

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -23,13 +23,11 @@ mv ./obs-browser.so ./rundir/RelWithDebInfo/obs-plugins/
 packagesbuild ../CI/install/osx/CMakeLists.pkgproj
 
 # Signing stuff
-openssl aes-256-cbc -K $encrypted_dd3c7f5e9db9_key -iv $encrypted_dd3c7f5e9db9_iv -in ../CI/osxcert/cert.p12.enc -out cert.p12 -d
-openssl aes-256-cbc -K $encrypted_dd3c7f5e9db9_key -iv $encrypted_dd3c7f5e9db9_iv -in ../CI/osxcert/pk.p12.enc -out pk.p12 -d
+openssl aes-256-cbc -K $encrypted_dd3c7f5e9db9_key -iv $encrypted_dd3c7f5e9db9_iv -in ../CI/osxcert/Certificates.p12.enc -out Certificates.p12 -d
 security create-keychain -p mysecretpassword build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p mysecretpassword build.keychain
-security import cert.p12 -k build.keychain -T /usr/bin/codesign
-security import pk.p12 -k build.keychain -T /usr/bin/codesign
+security import Certificates.p12 -k build.keychain -T /usr/bin/codesign
 productsign --sign 'Developer ID Installer: Hugh Bailey (2MMRE5MTB8)' ./OBS.pkg ./$FILENAME
 
 # Move to the folder that travis uses to upload artifacts from
