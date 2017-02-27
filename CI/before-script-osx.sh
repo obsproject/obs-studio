5f128d9e54b9cf34742c254f55e3ceebc500a7b1
juvester <juvester@cs.helsinki.fi>
2017-02-27 12:10:39 +0200

    CI: Use ccache to speed up the build
    Speeds up recompilation by caching previous compilations and detecting
    when the same compilation is being done again. [skip appveyor]

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -1,3 +1,6 @@
+# Make sure ccache is found
+export PATH=/usr/local/opt/ccache/libexec:$PATH
+
 mkdir build
 cd build
 cmake -DENABLE_SPARKLE_UPDATER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
