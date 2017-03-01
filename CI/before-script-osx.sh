5843100b9e24e4ff566c2050b9ce9727e8434d23
Colin Edwards <colin@recursivepenguin.com>
2017-03-01 12:22:41 -0600

    CI: OSX - Use bash variable for CEF version

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_3.2987.1588.g1952835_macosx64 ..
+cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_$CEF_BUILD_VERSION_macosx64 ..
