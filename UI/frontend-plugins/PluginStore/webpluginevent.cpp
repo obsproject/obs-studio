3abb0555d9aa67b5224ee225ae4a7ca7a4ba81c1
maxiang <87089123@163.com>
2017-05-05 17:23:10 +0800

    add js call,and test html

---
+++ UI/frontend-plugins/PluginStore/webpluginevent.cpp
@@ -0,0 +1,33 @@
+#include <QMessageBox>
+#include "webpluginevent.h"
+
+WebPluginEvent::WebPluginEvent(QObject *parent) : QObject(parent)
+{
+
+}
+WebPluginEvent::~WebPluginEvent()
+{
+	
+}
+QStringList	WebPluginEvent::GetLocalPluginList()
+{
+	return	{ "1", "2", "3" };
+}
+QString		WebPluginEvent::GetLocalPluginVersion(QString strName)
+{
+	return	QString("1.1.1.1");
+}
+void		WebPluginEvent::DownLoadPluginUrl(QString strName,QString strUrl)
+{
+	emit DownLoadState(strName, 0, 0);
+}
+bool		WebPluginEvent::InstallPluginName(QString strName)
+{
+	
+	return true;
+}
+bool		WebPluginEvent::UnInstallPluginName(QString strName)
+{
+	return true;
+}
+
---
+++ UI/frontend-plugins/PluginStore/webpluginevent.h
@@ -0,0 +1,22 @@
+#ifndef WEBPLUGINEVENT_H
+#define WEBPLUGINEVENT_H
+
+#include <QObject>
+class WebPluginEvent : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WebPluginEvent(QObject *parent = 0);
+	~WebPluginEvent();
+signals:
+	void DownLoadState(QString strPluginName, qint64 iDownLoadSize,qint64 iTotalSize);
+public	slots:
+	QStringList	GetLocalPluginList();
+	QString		GetLocalPluginVersion(QString strName);
+	void		DownLoadPluginUrl(QString strName,QString strUrl);
+	bool		InstallPluginName(QString strName);
+	bool		UnInstallPluginName(QString strName);
+private:
+};
+
+#endif // WEBPLUGINEVENT_H
