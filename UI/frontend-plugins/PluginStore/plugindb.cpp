e38249d32abde399d861925b46c1666235686da3
PowerMan <shidachuang@gmail.com>
2017-05-23 16:20:27 +0800

    add sqlite create

--- UI/frontend-plugins/PluginStore/plugindb.cpp
+++ UI/frontend-plugins/PluginStore/plugindb.cpp
@@ -1,11 +1,45 @@
 #include "pluginDB.h"
 
-
 PluginDB::PluginDB()
 {
+
 }
 
 
 PluginDB::~PluginDB()
 {
+}
+
+bool PluginDB::InitPluginDB()
+{
+    bool bRet = false;
+    if (QSqlDatabase::isDriverAvailable(__DEF_SQL_DRIVER_NAME__))
+    {
+        if (m_database.isOpen())
+        {
+            CloseDB();
+        }
+        m_database.setDatabaseName(__DEF_SQL_DATABASE_NAME__);
+        m_database.setUserName("root");
+        m_database.setPassword("123456");
+
+        bRet = OpenDB();
+    }    
+    return bRet;
+}
+
+
+bool PluginDB::OpenDB()
+{
+    bool bRet = false;
+    if (m_database.isOpen())
+    {
+        CloseDB();
+    }
+    return m_database.open();
+}
+
+void PluginDB::CloseDB()
+{
+    m_database.close();
 }
--- UI/frontend-plugins/PluginStore/plugindb.h
+++ UI/frontend-plugins/PluginStore/plugindb.h
@@ -1,10 +1,21 @@
 #ifndef PLUGINDB_H
 #define PLUGINDB_H
+#include <QtSql/QSqlDatabase>
+
+#define __DEF_SQL_DRIVER_NAME__     "QSQLITE"
+#define __DEF_SQL_DATABASE_NAME__   "c:\\plugin_database.db"
 
 class PluginDB
 {
 public:
     PluginDB();
     ~PluginDB();
+
+    bool InitPluginDB();
+    bool OpenDB();
+    void CloseDB();
+
+private:
+    QSqlDatabase m_database;
 };
 #endif
