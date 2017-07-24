4ca64ff4248ad68a59b5ae9a89930d71f38a0aae
Erik Mayrhofer <obyoxar@gmail.com>
2017-07-24 22:21:52 +0200

    obs-filters: Add License Notice

--- plugins/obs-filters/transform-filter.c
+++ plugins/obs-filters/transform-filter.c
@@ -1,3 +1,21 @@
+/*****************************************************************************
+Copyright (C) 2017 by Erik Mayrhofer <obyoxar@gmail.com>
+
+This file is part of OBS.
+
+OBS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*****************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,7 +68,7 @@ struct transform_filter_data {
 static const char *transform_filter_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
-	return obs_module_text("TransformFiltering");
+	return obs_module_text("Transformation");
 }
 
 static void transform_filter_update(void *data, obs_data_t *settings)
