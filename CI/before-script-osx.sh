54a68ec535a41740f43f313666a2faf44f5d48a9
Ryan Foster <RytoEX@gmail.com>
2017-10-14 23:07:53 -0400

    CI: Update Travis script to target OSX 10.10+
    Since OSX 10.10 is now the officially supported minimum OS version on
    Mac, update the CI script.

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,4 +3,4 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 
 mkdir build
 cd build
-cmake -DENABLE_SPARKLE_UPDATER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
+cmake -DENABLE_SPARKLE_UPDATER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
