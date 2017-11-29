7fe76904044b445af9bc0ba44efc369c59220e20
Ryan Foster <RytoEX@gmail.com>
2017-11-29 03:08:49 -0500

    UI: Add ability to copy scenes to other scene collections
    This commit adds the ability for users to copy an entire scene and all
    of its child sources to another scene collection. This is for parity
    with OBS Classic.

---
+++ UI/window-scenecollectiondialog.hpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+    Copyright (C) 2013 by Hugh Bailey <obs.jim@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#pragma once
+
+#include <QDialog>
+#include <string>
+#include <memory>
+
+#include "ui_SceneCollectionDialog.h"
+
+class SceneCollectionDialog : public QDialog {
+	Q_OBJECT
+
+private:
+	std::unique_ptr<Ui::SceneCollectionDialog> ui;
+
+public:
+	SceneCollectionDialog(QWidget *parent);
+
+	static bool AskForName(QWidget *parent, const QString &title,
+			const QString &text, std::string &str,
+			std::vector<std::string> &strings,
+			const QString &placeHolder = QString(""));
+};
