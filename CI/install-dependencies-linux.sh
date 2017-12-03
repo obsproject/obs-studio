2f788716625e7a5f076cc32c912e8b42b4804f5e
juvester <juvester@users.noreply.github.com>
2017-12-03 23:10:46 +0200

    Revert "CI: Linux - Install FFmpeg from source"
    This reverts commit de32d89fc6ac1cc85346732f722fe74ac1cb1eca.
    
    Fixes an issue where Linux CI builds fail because FFmpeg fails to
    compile. Using FFmpeg git master in CI builds was obviously not the
    smartest idea because changes in FFmpeg can break things unexpectedly.
    Sorry about that.
    
    The reason for the original commit was that OBS failed to compile
    with FFmpeg 2.4. That has been fixed since.

--- CI/install-dependencies-linux.sh
+++ CI/install-dependencies-linux.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -ex
 
+sudo add-apt-repository ppa:kirillshkrogalev/ffmpeg-next -y
 sudo apt-get -qq update
 sudo apt-get install -y \
         build-essential \
         checkinstall \
         cmake \
         libasound2-dev \
+        libavcodec-ffmpeg-dev \
+        libavdevice-ffmpeg-dev \
+        libavfilter-ffmpeg-dev \
+        libavformat-ffmpeg-dev \
+        libavutil-ffmpeg-dev \
         libcurl4-openssl-dev \
         libfdk-aac-dev \
         libfontconfig-dev \
@@ -17,6 +23,8 @@ sudo apt-get install -y \
         libpulse-dev \
         libqt5x11extras5-dev \
         libspeexdsp-dev \
+        libswresample-ffmpeg-dev \
+        libswscale-ffmpeg-dev \
         libudev-dev \
         libv4l-dev \
         libvlc-dev \
@@ -27,14 +35,4 @@ sudo apt-get install -y \
         libxcomposite-dev \
         libxinerama-dev \
         pkg-config \
-        qtbase5-dev \
-        yasm \
-        zlib1g-dev
-
-# FFmpeg
-cd ..
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git
-cd ffmpeg
-./configure --enable-shared
-make -j2
-sudo make install
+        qtbase5-dev
