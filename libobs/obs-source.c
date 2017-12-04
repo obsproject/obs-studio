2a6f3e47f27a847c09f5c00fb49744a2b72c30ef
SuslikV <vx2010@i.ua>
2017-12-04 17:34:42 +0200

    libobs: Fix bad source release placement
    (This commit also modifies the UI)
    
    The source release was being done in the wrong file.

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -430,8 +430,6 @@ void obs_source_copy_filters(obs_source_t *dst, obs_source_t *src)
 	duplicate_filters(dst, src, dst->context.private ?
 					OBS_SCENE_DUP_PRIVATE_COPY :
 					OBS_SCENE_DUP_COPY);
-
-	obs_source_release(src);
 }
 
 obs_source_t *obs_source_duplicate(obs_source_t *source,
