bf31e1c2331bc1aeea702077116b48f388d64ce4
jp9000 <obs.jim@gmail.com>
2017-12-25 13:42:23 -0800

    UI/obs-frontend-api: Move function declarations

--- UI/obs-frontend-api/obs-frontend-api.h
+++ UI/obs-frontend-api/obs-frontend-api.h
@@ -13,6 +13,36 @@ typedef struct config_data config_t;
 struct obs_data;
 typedef struct obs_data obs_data_t;
 
+enum obs_frontend_event {
+	OBS_FRONTEND_EVENT_STREAMING_STARTING,
+	OBS_FRONTEND_EVENT_STREAMING_STARTED,
+	OBS_FRONTEND_EVENT_STREAMING_STOPPING,
+	OBS_FRONTEND_EVENT_STREAMING_STOPPED,
+	OBS_FRONTEND_EVENT_RECORDING_STARTING,
+	OBS_FRONTEND_EVENT_RECORDING_STARTED,
+	OBS_FRONTEND_EVENT_RECORDING_STOPPING,
+	OBS_FRONTEND_EVENT_RECORDING_STOPPED,
+	OBS_FRONTEND_EVENT_SCENE_CHANGED,
+	OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED,
+	OBS_FRONTEND_EVENT_TRANSITION_CHANGED,
+	OBS_FRONTEND_EVENT_TRANSITION_STOPPED,
+	OBS_FRONTEND_EVENT_TRANSITION_LIST_CHANGED,
+	OBS_FRONTEND_EVENT_SCENE_COLLECTION_CHANGED,
+	OBS_FRONTEND_EVENT_SCENE_COLLECTION_LIST_CHANGED,
+	OBS_FRONTEND_EVENT_PROFILE_CHANGED,
+	OBS_FRONTEND_EVENT_PROFILE_LIST_CHANGED,
+	OBS_FRONTEND_EVENT_EXIT,
+
+	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING,
+	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED,
+	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPING,
+	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED,
+
+	OBS_FRONTEND_EVENT_STUDIO_MODE_ENABLED,
+	OBS_FRONTEND_EVENT_STUDIO_MODE_DISABLED,
+	OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED
+};
+
 /* ------------------------------------------------------------------------- */
 
 struct obs_frontend_source_list {
@@ -62,6 +92,40 @@ EXPORT char **obs_frontend_get_profiles(void);
 EXPORT char *obs_frontend_get_current_profile(void);
 EXPORT void obs_frontend_set_current_profile(const char *profile);
 
+typedef void (*obs_frontend_cb)(void *private_data);
+
+EXPORT void *obs_frontend_add_tools_menu_qaction(const char *name);
+EXPORT void obs_frontend_add_tools_menu_item(const char *name,
+		obs_frontend_cb callback, void *private_data);
+
+typedef void (*obs_frontend_event_cb)(enum obs_frontend_event event,
+		void *private_data);
+
+EXPORT void obs_frontend_add_event_callback(obs_frontend_event_cb callback,
+		void *private_data);
+EXPORT void obs_frontend_remove_event_callback(obs_frontend_event_cb callback,
+		void *private_data);
+
+typedef void (*obs_frontend_save_cb)(obs_data_t *save_data, bool saving,
+		void *private_data);
+
+EXPORT void obs_frontend_add_save_callback(obs_frontend_save_cb callback,
+		void *private_data);
+EXPORT void obs_frontend_remove_save_callback(obs_frontend_save_cb callback,
+		void *private_data);
+
+EXPORT void obs_frontend_add_preload_callback(obs_frontend_save_cb callback,
+		void *private_data);
+EXPORT void obs_frontend_remove_preload_callback(obs_frontend_save_cb callback,
+		void *private_data);
+
+typedef bool (*obs_frontend_translate_ui_cb)(const char *text,
+		const char **out);
+
+EXPORT void obs_frontend_push_ui_translation(
+		obs_frontend_translate_ui_cb translate);
+EXPORT void obs_frontend_pop_ui_translation(void);
+
 EXPORT void obs_frontend_streaming_start(void);
 EXPORT void obs_frontend_streaming_stop(void);
 EXPORT bool obs_frontend_streaming_active(void);
@@ -75,58 +139,7 @@ EXPORT void obs_frontend_replay_buffer_save(void);
 EXPORT void obs_frontend_replay_buffer_stop(void);
 EXPORT bool obs_frontend_replay_buffer_active(void);
 
-typedef void (*obs_frontend_cb)(void *private_data);
-
-EXPORT void *obs_frontend_add_tools_menu_qaction(const char *name);
-EXPORT void obs_frontend_add_tools_menu_item(const char *name,
-		obs_frontend_cb callback, void *private_data);
-
-enum obs_frontend_event {
-	OBS_FRONTEND_EVENT_STREAMING_STARTING,
-	OBS_FRONTEND_EVENT_STREAMING_STARTED,
-	OBS_FRONTEND_EVENT_STREAMING_STOPPING,
-	OBS_FRONTEND_EVENT_STREAMING_STOPPED,
-	OBS_FRONTEND_EVENT_RECORDING_STARTING,
-	OBS_FRONTEND_EVENT_RECORDING_STARTED,
-	OBS_FRONTEND_EVENT_RECORDING_STOPPING,
-	OBS_FRONTEND_EVENT_RECORDING_STOPPED,
-	OBS_FRONTEND_EVENT_SCENE_CHANGED,
-	OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED,
-	OBS_FRONTEND_EVENT_TRANSITION_CHANGED,
-	OBS_FRONTEND_EVENT_TRANSITION_STOPPED,
-	OBS_FRONTEND_EVENT_TRANSITION_LIST_CHANGED,
-	OBS_FRONTEND_EVENT_SCENE_COLLECTION_CHANGED,
-	OBS_FRONTEND_EVENT_SCENE_COLLECTION_LIST_CHANGED,
-	OBS_FRONTEND_EVENT_PROFILE_CHANGED,
-	OBS_FRONTEND_EVENT_PROFILE_LIST_CHANGED,
-	OBS_FRONTEND_EVENT_EXIT,
-
-	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING,
-	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED,
-	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPING,
-	OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED,
-
-	OBS_FRONTEND_EVENT_STUDIO_MODE_ENABLED,
-	OBS_FRONTEND_EVENT_STUDIO_MODE_DISABLED,
-	OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED
-};
-
-typedef void (*obs_frontend_event_cb)(enum obs_frontend_event event,
-		void *private_data);
-
-EXPORT void obs_frontend_add_event_callback(obs_frontend_event_cb callback,
-		void *private_data);
-EXPORT void obs_frontend_remove_event_callback(obs_frontend_event_cb callback,
-		void *private_data);
-
-typedef void (*obs_frontend_save_cb)(obs_data_t *save_data, bool saving,
-		void *private_data);
-
 EXPORT void obs_frontend_save(void);
-EXPORT void obs_frontend_add_save_callback(obs_frontend_save_cb callback,
-		void *private_data);
-EXPORT void obs_frontend_remove_save_callback(obs_frontend_save_cb callback,
-		void *private_data);
 
 EXPORT obs_output_t *obs_frontend_get_streaming_output(void);
 EXPORT obs_output_t *obs_frontend_get_recording_output(void);
@@ -134,18 +147,6 @@ EXPORT obs_output_t *obs_frontend_get_replay_buffer_output(void);
 
 EXPORT config_t *obs_frontend_get_profile_config(void);
 EXPORT config_t *obs_frontend_get_global_config(void);
-
-EXPORT void obs_frontend_add_preload_callback(obs_frontend_save_cb callback,
-		void *private_data);
-EXPORT void obs_frontend_remove_preload_callback(obs_frontend_save_cb callback,
-		void *private_data);
-
-typedef bool (*obs_frontend_translate_ui_cb)(const char *text,
-		const char **out);
-
-EXPORT void obs_frontend_push_ui_translation(
-		obs_frontend_translate_ui_cb translate);
-EXPORT void obs_frontend_pop_ui_translation(void);
 
 EXPORT void obs_frontend_set_streaming_service(obs_service_t *service);
 EXPORT obs_service_t* obs_frontend_get_streaming_service(void);
