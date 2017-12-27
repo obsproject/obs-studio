a8d92850da9f03f4a6465ad4f96d2b2244779a32
jp9000 <obs.jim@gmail.com>
2017-12-27 13:54:05 -0800

    Revert "libobs/util: Add LOG_TEXTBLOCK logging flag"
    This reverts commit 112529a376ed716ab19747cd7bf0f1a851e66f74.

--- libobs/util/base.h
+++ libobs/util/base.h
@@ -62,13 +62,7 @@ enum {
 	/**
 	 * Debug message to be used mostly by developers.
 	 */
-	LOG_DEBUG   = 400,
-
-	/**
-	 * Marks the text as a block of text, not a line
-	 * (in other words, a newline character will not be appended)
-	 */
-	LOG_TEXTBLOCK = (1<<31)
+	LOG_DEBUG   = 400
 };
 
 typedef void (*log_handler_t)(int lvl, const char *msg, va_list args, void *p);
