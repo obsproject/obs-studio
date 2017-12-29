5d17e6f6eaac2366e85b7d2fb9115a9ab2ac14bf
Colin Edwards <colin@recursivepenguin.com>
2017-12-29 12:56:45 -0600

    deps/scripting: Add python rpaths to the obs binary on OSX
    Closes jp9000/obs-studio#1127

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,4 +3,4 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 
 mkdir build
 cd build
-cmake -DENABLE_SPARKLE_UPDATER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
+cmake -DPYTHON_LIBRARY=/usr/local/Cellar/python3/3.6.4/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6.dylib -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python3/3.6.4/Frameworks/Python.framework/Versions/3.6/include/python3.6m -DENABLE_SPARKLE_UPDATER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 -DDepsPath=/tmp/obsdeps -DVLCPath=$PWD/../../vlc-master -DBUILD_BROWSER=ON -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_BUILD_VERSION}_macosx64 ..
