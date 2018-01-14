e1a6961ff590fe896c4f901686265a335f1cdd8d
jp9000 <obs.jim@gmail.com>
2018-01-14 01:28:28 -0800

    test/test-input: Add audio buffering sync test source
    Adds a source which tests whether audio buffering affects audio sync.
    This sync test cycles through 7 audio/video frequencies at 250
    millisecond intervals, and via a hotkey, will artificially move audio
    time back by one second (and the audio cycle back by 4 frequencies).
    This will artificially increase audio buffering by approximately 750
    milliseconds.
    
    Results from this test as of this writing: this test proves that dynamic
    audio buffering does not affect sync.

--- test/test-input/test-input.c
+++ test/test-input/test-input.c
@@ -6,6 +6,7 @@ extern struct obs_source_info test_random;
 extern struct obs_source_info test_sinewave;
 extern struct obs_source_info test_filter;
 extern struct obs_source_info async_sync_test;
+extern struct obs_source_info buffering_async_sync_test;
 extern struct obs_source_info sync_video;
 extern struct obs_source_info sync_audio;
 
@@ -15,6 +16,7 @@ bool obs_module_load(void)
 	obs_register_source(&test_sinewave);
 	obs_register_source(&test_filter);
 	obs_register_source(&async_sync_test);
+	obs_register_source(&buffering_async_sync_test);
 	obs_register_source(&sync_video);
 	obs_register_source(&sync_audio);
 	return true;
