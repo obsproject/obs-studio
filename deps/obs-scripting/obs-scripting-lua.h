d01632a4f0d856c8ee94da33bda89916f88c12d5
jp9000 <obs.jim@gmail.com>
2018-01-18 05:35:19 -0800

    deps/obs-scripting: Fix a few more VC warnings from swigluarun.h

--- deps/obs-scripting/obs-scripting-lua.h
+++ deps/obs-scripting/obs-scripting-lua.h
@@ -25,6 +25,8 @@
 
 #ifdef _MSC_VER
 #pragma warning(push)
+#pragma warning(disable : 4100)
+#pragma warning(disable : 4189)
 #pragma warning(disable : 4244)
 #pragma warning(disable : 4267)
 #endif
