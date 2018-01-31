3040ff6347d006e52c90b5be365cae7a04aa07d6
Colin Edwards <colin@recursivepenguin.com>
2018-01-30 22:42:07 -0600

    CI: Don't install python for OSX

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,9 +3,7 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 
 mkdir build
 cd build
-cmake -DPYTHON_LIBRARY=/usr/local/Cellar/python3/3.6.4_2/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6.dylib \
--DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python3/3.6.4_2/Frameworks/Python.framework/Versions/3.6/include/python3.6m \
--DENABLE_SPARKLE_UPDATER=ON \
+cmake -DENABLE_SPARKLE_UPDATER=ON \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
 -DDepsPath=/tmp/obsdeps \
 -DVLCPath=$PWD/../../vlc-master \
