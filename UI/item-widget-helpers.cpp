8398eaabd8f03473dee512bd7c75441a161ac8c7
craftwar <craftwar@users.noreply.github.com>
2018-05-15 15:49:52 +0800

    UI: Simplify ClearListItems function

--- UI/item-widget-helpers.cpp
+++ UI/item-widget-helpers.cpp
@@ -38,9 +38,7 @@ void DeleteListItem(QListWidget *widget, QListWidgetItem *item)
 void ClearListItems(QListWidget *widget)
 {
 	widget->setCurrentItem(nullptr, QItemSelectionModel::Clear);
-
-	for (int i = 0; i < widget->count(); i++)
-		delete widget->itemWidget(widget->item(i));
-
-	widget->clear();
+	int count = widget->count();
+	for (int i = 0; i < count; ++i)
+		delete widget->takeItem(0);
 }
