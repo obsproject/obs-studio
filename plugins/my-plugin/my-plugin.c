7d729511c16e5d9e757cb7b27118adf4122c8021
Charles Crossan <crossan007@gmail.com>
2018-05-27 23:45:04 -0400

    working gstreamer test src/sink

---
+++ plugins/my-plugin/my-plugin.c
@@ -0,0 +1,141 @@
+#include <stdlib.h>
+#include <util/threading.h>
+#include <util/platform.h>
+#include <obs.h>
+#include <obs-module.h>
+#include <gst/gst.h> 
+#include <glib.h> 
+
+
+
+OBS_DECLARE_MODULE()
+
+struct my_tex {
+	obs_source_t *source;
+	os_event_t   *stop_signal;
+	pthread_t    thread;
+	bool         initialized;
+};
+
+static const char *my_getname(void *unused)
+{
+	UNUSED_PARAMETER(unused);
+	return "My Plugin";
+}
+
+static void my_destroy(void *data)
+{
+	struct my_tex *rt = data;
+
+	if (rt) {
+		if (rt->initialized) {
+			os_event_signal(rt->stop_signal);
+			pthread_join(rt->thread, NULL);
+		}
+
+		os_event_destroy(rt->stop_signal);
+		bfree(rt);
+	}
+}
+
+static inline void fill_texture(uint32_t *pixels)
+{
+	size_t x, y;
+
+	for (y = 0; y < 20; y++) {
+		for (x = 0; x < 20; x++) {
+			uint32_t pixel = 0;
+			pixel |= (rand()%256);
+			pixel |= (rand()%256) << 8;
+			pixel |= (rand()%256) << 16;
+			//pixel |= (rand()%256) << 24;
+			//pixel |= 0xFFFFFFFF;
+			pixels[y*20 + x] = pixel;
+		}
+	}
+}
+
+static void *video_thread(void *data)
+{
+	struct my_tex   *rt = data;
+	uint32_t            pixels[20*20];
+	uint64_t            cur_time = os_gettime_ns();
+
+	struct obs_source_frame frame = {
+		.data     = {[0] = (uint8_t*)pixels},
+		.linesize = {[0] = 20*4},
+		.width    = 20,
+		.height   = 20,
+		.format   = VIDEO_FORMAT_BGRX
+	};
+
+	while (os_event_try(rt->stop_signal) == EAGAIN) {
+		fill_texture(pixels);
+
+		frame.timestamp = cur_time;
+
+		obs_source_output_video(rt->source, &frame);
+
+		os_sleepto_ns(cur_time += 250000000);
+	}
+
+	return NULL;
+}
+
+static void *my_create(obs_data_t *settings, obs_source_t *source)
+{
+	struct my_tex *rt = bzalloc(sizeof(struct my_tex));
+	rt->source = source;
+
+	if (os_event_init(&rt->stop_signal, OS_EVENT_TYPE_MANUAL) != 0) {
+		my_destroy(rt);
+		return NULL;
+	}
+
+	if (pthread_create(&rt->thread, NULL, video_thread, rt) != 0) {
+		my_destroy(rt);
+		return NULL;
+	}
+
+	rt->initialized = true;
+
+	GstElement *pipeline;
+	GstBus *bus;
+	GstMessage *msg;
+	char argc, argv;
+	gst_init (&argc, &argv);
+	pipeline = gst_parse_launch ("videotestsrc ! appsink ", NULL);
+
+  	/* Start playing */
+	gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+	/* Wait until error or EOS */
+	bus = gst_element_get_bus (pipeline);
+	msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+	/* Free resources */
+	if (msg != NULL)
+		gst_message_unref (msg);
+	gst_object_unref (bus);
+	gst_element_set_state (pipeline, GST_STATE_NULL);
+	gst_object_unref (pipeline);
+
+	UNUSED_PARAMETER(settings);
+	UNUSED_PARAMETER(source);
+	return rt;
+}
+
+struct obs_source_info my_plugin = {
+	.id           = "my-plugin",
+	.type         = OBS_SOURCE_TYPE_INPUT,
+	.output_flags = OBS_SOURCE_ASYNC_VIDEO,
+	.get_name     = my_getname,
+	.create       = my_create,
+	.destroy      = my_destroy,
+};
+
+
+bool obs_module_load(void)
+{
+	obs_register_source(&my_plugin);
+}
