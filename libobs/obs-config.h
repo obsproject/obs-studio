2647da568f72fac0dd31b99fc764a8940860d826
jp9000 <obs.jim@gmail.com>
2018-08-18 04:56:42 -0700

    libobs: Revert version update to 22.0.0 (instead, do RC2)
    This reverts commit 3d4b277a6099ebedf51b880f9ceb67e424a185c7.
    
    Reverting temporarily for the sake of completeness and confirming
    testing results.

--- libobs/obs-config.h
+++ libobs/obs-config.h
@@ -27,21 +27,21 @@
 /*
  * Increment if major breaking API changes
  */
-#define LIBOBS_API_MAJOR_VER  22
+#define LIBOBS_API_MAJOR_VER  21
 
 /*
  * Increment if backward-compatible additions
  *
  * Reset to zero each major version
  */
-#define LIBOBS_API_MINOR_VER  0
+#define LIBOBS_API_MINOR_VER  1
 
 /*
  * Increment if backward-compatible bug fix
  *
  * Reset to zero each major or minor version
  */
-#define LIBOBS_API_PATCH_VER  0
+#define LIBOBS_API_PATCH_VER  2
 
 #define MAKE_SEMANTIC_VERSION(major, minor, patch) \
                              ((major << 24) | \
