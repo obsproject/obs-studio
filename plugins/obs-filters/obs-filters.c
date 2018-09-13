7cfbdcf18497813b0f55e46e8e6fbe706798a811
Philip Loche <ploche@physik.fu-berlin.de>
2018-09-13 02:49:21 -0700

    obs-filters: Add "Invert Polarity" audio filter
    Allows inverting the polarity of the audio data, which can be useful for
    ether purposefully canceling out audio or preventing audio cancellation.
    
    Amends pull request obsproject/obs-studio#1404

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -20,6 +20,7 @@ extern struct obs_source_info async_delay_filter;
 #if SPEEXDSP_ENABLED
 extern struct obs_source_info noise_suppress_filter;
 #endif
+extern struct obs_source_info invert_polarity_filter;
 extern struct obs_source_info noise_gate_filter;
 extern struct obs_source_info compressor_filter;
 
@@ -40,6 +41,7 @@ bool obs_module_load(void)
 #if SPEEXDSP_ENABLED
 	obs_register_source(&noise_suppress_filter);
 #endif
+	obs_register_source(&invert_polarity_filter);
 	obs_register_source(&noise_gate_filter);
 	obs_register_source(&compressor_filter);
 	return true;
