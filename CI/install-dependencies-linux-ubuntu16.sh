c3b2380b13a661557436a7196821ee384fa131ab
Ryan Foster <RytoEX@gmail.com>
2018-09-18 15:12:33 -0400

    CI: Build on Linux with AppVeyor
    Build on Linux (Ubuntu 16.04) with AppVeyor. Disable PowerShell on
    Linux. Ubuntu 16.04's dependencies are different from the existing
    Ubuntu 14.04 dependencies, so a separate script for installing those
    dependencies has been added.

---
+++ CI/install-dependencies-linux-ubuntu16.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -ex
+
+sudo apt-get -qq update
+sudo apt-get install -y \
+        build-essential \
+        checkinstall \
+        cmake \
+        libasound2-dev \
+        libavcodec-dev \
+        libavdevice-dev \
+        libavfilter-dev \
+        libavformat-dev \
+        libavutil-dev \
+        libcurl4-openssl-dev \
+        libfdk-aac-dev \
+        libfontconfig-dev \
+        libfreetype6-dev \
+        libgl1-mesa-dev \
+        libjack-jackd2-dev \
+        libjansson-dev \
+        libluajit-5.1-dev \
+        libpulse-dev \
+        libqt5x11extras5-dev \
+        libspeexdsp-dev \
+        libswresample-dev \
+        libswscale-dev \
+        libudev-dev \
+        libv4l-dev \
+        libvlc-dev \
+        libx11-dev \
+        libx264-dev \
+        libxcb-shm0-dev \
+        libxcb-xinerama0-dev \
+        libxcomposite-dev \
+        libxinerama-dev \
+        pkg-config \
+        python3-dev \
+        qtbase5-dev \
+        swig
