fcc9f4e1f9d2a160a27925cce41ac0114df870a3
pkviet <pkv.stream@gmail.com>
2018-11-30 02:56:19 +0100

    obs-filters: Add downward expander filter
    The downward expander acts as an advanced gate with smoother effect
    since it lowers the level instead of discarding audio when below the
    threshold.

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -27,6 +27,7 @@ extern struct obs_source_info invert_polarity_filter;
 extern struct obs_source_info noise_gate_filter;
 extern struct obs_source_info compressor_filter;
 extern struct obs_source_info limiter_filter;
+extern struct obs_source_info expander_filter;
 
 bool obs_module_load(void)
 {
@@ -49,5 +50,6 @@ bool obs_module_load(void)
 	obs_register_source(&noise_gate_filter);
 	obs_register_source(&compressor_filter);
 	obs_register_source(&limiter_filter);
+	obs_register_source(&expander_filter);
 	return true;
 }
