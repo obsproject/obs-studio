270dc07e498f8eade8f76e5a3113d8abd72c22ac
probonopd <probonopd@users.noreply.github.com>
2018-12-02 14:51:55 +0000

    Update script-linux.sh
    [ci skip]

--- CI/script-linux.sh
+++ CI/script-linux.sh
@@ -10,7 +10,7 @@ chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
 which qmake
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -verbose=2 -qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 
 find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 curl --upload-file OBS*.AppImage https://transfer.sh/OBS-git.$(git rev-parse --short HEAD)-x86_64.AppImage
