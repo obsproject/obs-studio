8df2a66af83dc086f4f179759620d34d24527ee5
probonopd <probonopd@users.noreply.github.com>
2018-12-02 14:54:13 +0000

    Update script-linux.sh
    [ci skip]

--- CI/script-linux.sh
+++ CI/script-linux.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 set -ex
 
+# Compile and install to an AppDir
 cd ./build
 make -j$(nproc)
 make DESTDIR=appdir -j$(nproc) install ; find appdir/
+find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 
+# Finalize the AppDir and convert it to AppImage
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
-which qmake
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -qmake=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 
-find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+# TODO: The next line should be replaced by a native upload mechanism defined in .travis.yml,
+# e.g., using https://github.com/probonopd/uploadtool
 curl --upload-file OBS*.AppImage https://transfer.sh/OBS-git.$(git rev-parse --short HEAD)-x86_64.AppImage
