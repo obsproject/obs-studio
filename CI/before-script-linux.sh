e0c531cc45c1e9167c4e93ec84101e3b2e10460d
probonopd <probonopd@users.noreply.github.com>
2018-12-02 18:47:53 +0000

    Update before-script-linux.sh
    [ci skip]

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -ex
 
-. /opt/qt*/bin/qt*-env.sh
+export LD_LIBRARY_PATH=/opt/qt59/lib/x86_64-linux-gnu:/opt/qt59/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/opt/qt59/lib/pkgconfig:$PKG_CONFIG_PATH
+export PATH=/opt/qt59/bin:$PATH
+
 ccache -s || echo "CCache is not available."
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
