ee5664e90dbdb81ae871daf0f348eb93e0b61d56
probonopd <probonopd@users.noreply.github.com>
2018-12-02 16:43:27 +0000

    Update before-script-linux.sh

--- CI/before-script-linux.sh
+++ CI/before-script-linux.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 
-source /opt/qt*/bin/qt*-env.sh
+. /opt/qt*/bin/qt*-env.sh
 ccache -s || echo "CCache is not available."
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
