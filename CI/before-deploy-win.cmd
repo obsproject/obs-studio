8bc8ee963d43d6dcc676e468f40b0c7bbe4e23cd
Branka Lakic <brankalakic@gmail.com>
2019-01-24 17:18:24 -0800

    Copy deps to libobs release

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,3 +1,9 @@
-robocopy .\build32\rundir\RelWithDebInfo .\build\ /E /XF .gitignore
-robocopy .\build64\rundir\RelWithDebInfo .\build\ /E /XC /XN /XO /XF .gitignore
-7z a build.zip .\build\*
+
+robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XF .gitignore
+robocopy C:\projects\obs-studio\build64\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XC /XN /XO /XF .gitignore
+cd C:projects\obs-studio\build\
+mkdir deps
+robocopy C:\projects\obs-studio\deps C:projects\obs-studio\build\deps /E
+cd C:\projects\obs-studio\deps
+dir
+7z a build.zip C:\projects\obs-studio\build\*
