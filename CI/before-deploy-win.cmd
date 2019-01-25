c9f2e99090a08411bb48336928b2f3ebb53b3e2b
Branka Lakic <brankalakic@gmail.com>
2019-01-25 15:04:14 -0800

    Copy only the necessary dependency

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,7 +1,6 @@
 
 robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XF .gitignore
 robocopy C:\projects\obs-studio\build64\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XC /XN /XO /XF .gitignore
-cd C:projects\obs-studio\build\
-mkdir deps
-robocopy C:\projects\obs-studio\deps C:projects\obs-studio\build\deps /E
+mkdir C:projects\obs-studio\build\deps\w32-pthreads
+robocopy C:\projects\obs-studio\deps\w32-pthreads\pthread.h C:projects\obs-studio\build\deps\w32-pthreads\ /E
 7z a build.zip C:\projects\obs-studio\build\*
