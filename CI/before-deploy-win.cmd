76d4a714c90ba059c4e8835598d7255da4e9b5db
Branka Lakic <brankalakic@gmail.com>
2019-01-25 16:22:05 -0800

    Fix syntax of robocopy command and typo

--- CI/before-deploy-win.cmd
+++ CI/before-deploy-win.cmd
@@ -1,6 +1,6 @@
 
 robocopy C:\projects\obs-studio\build32\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XF .gitignore
 robocopy C:\projects\obs-studio\build64\rundir\RelWithDebInfo C:\projects\obs-studio\build\ /E /XC /XN /XO /XF .gitignore
-mkdir C:projects\obs-studio\build\deps\w32-pthreads
-robocopy C:\projects\obs-studio\deps\w32-pthreads\pthread.h C:projects\obs-studio\build\deps\w32-pthreads\ /E
+mkdir C:\projects\obs-studio\build\deps\w32-pthreads
+robocopy C:\projects\obs-studio\deps\w32-pthreads C:\projects\obs-studio\build\deps\w32-pthreads pthread.h
 7z a build.zip C:\projects\obs-studio\build\*
