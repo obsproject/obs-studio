693e5f21b41acadc962a339ffad2ebb8aeb2c0a1
jp9000 <obs.jim@gmail.com>
2019-02-13 16:08:40 -0800

    UI: Clear key in autoconfig when service disconnected

--- UI/window-basic-auto-config.cpp
+++ UI/window-basic-auto-config.cpp
@@ -463,6 +463,7 @@ void AutoConfigStreamPage::on_disconnectAccount_clicked()
 	ui->streamKeyLabel->setVisible(true);
 	ui->connectAccount2->setVisible(true);
 	ui->disconnectAccount->setVisible(false);
+	ui->key->setText("");
 }
 
 void AutoConfigStreamPage::on_useStreamKey_clicked()
