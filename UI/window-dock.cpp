1b166e022aad5e5eb8852429b18c124e8d79d83d
Clayton Groeneveld <claytong1214@gmail.com>
2019-03-05 17:16:16 -0600

    UI: Add (Do not show again) checkbox to dock closing warning

--- UI/window-dock.cpp
+++ UI/window-dock.cpp
@@ -2,24 +2,26 @@
 #include "obs-app.hpp"
 
 #include <QMessageBox>
+#include <QCheckBox>
 
 void OBSDock::closeEvent(QCloseEvent *event)
 {
-	auto msgBox = [] ()
-	{
-		QMessageBox::information(App()->GetMainWindow(),
-				QTStr("DockCloseWarning.Title"),
-				QTStr("DockCloseWarning.Text"));
-	};
-
 	bool warned = config_get_bool(App()->GlobalConfig(), "General",
 			"WarnedAboutClosingDocks");
 	if (!warned) {
-		QMetaObject::invokeMethod(App(), "Exec",
-				Qt::QueuedConnection,
-				Q_ARG(VoidFunc, msgBox));
+		QMessageBox *msgbox = new QMessageBox(this);
+		msgbox->setWindowTitle(QTStr("DockCloseWarning.Title"));
+		msgbox->setText(QTStr("DockCloseWarning.Text"));
+		msgbox->setIcon(QMessageBox::Icon::Information);
+		msgbox->addButton(QMessageBox::Ok);
+
+		QCheckBox *cb = new QCheckBox(QTStr("DoNotShowAgain"));
+		msgbox->setCheckBox(cb);
+
+		msgbox->exec();
+
 		config_set_bool(App()->GlobalConfig(), "General",
-				"WarnedAboutClosingDocks", true);
+				"WarnedAboutClosingDocks", cb->isChecked());
 		config_save_safe(App()->GlobalConfig(), "tmp", nullptr);
 	}
 
