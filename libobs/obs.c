ec9e48f3c7772718f3348e7376ea0903f5f04aa2
jp9000 <obs.jim@gmail.com>
2019-03-06 07:43:22 -0800

    libobs: Remove unnecessary count check

--- libobs/obs.c
+++ libobs/obs.c
@@ -1823,11 +1823,10 @@ void obs_load_sources(obs_data_array_t *array, obs_load_source_cb cb,
 			if (source->info.type == OBS_SOURCE_TYPE_TRANSITION)
 				obs_transition_load(source, source_data);
 			obs_source_load(source);
-			if (source->filters.num) {
-				for (size_t i = source->filters.num; i > 0; i--) {
-					obs_source_t *filter = source->filters.array[i - 1];
-					obs_source_load(filter);
-				}
+			for (size_t i = source->filters.num; i > 0; i--) {
+				obs_source_t *filter =
+					source->filters.array[i - 1];
+				obs_source_load(filter);
 			}
 			if (cb)
 				cb(private_data, source);
