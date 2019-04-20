21a32def3553ffafc23b2cb4925aa0edbdef0846
Clayton Groeneveld <claytong1214@gmail.com>
2019-04-20 09:08:36 -0500

    UI: Simplify locked/visibility checkboxes

--- UI/visibility-checkbox.hpp
+++ UI/visibility-checkbox.hpp
@@ -1,19 +1,7 @@
 #pragma once
 
 #include <QCheckBox>
-#include <QPixmap>
-
-class QPaintEvernt;
 
 class VisibilityCheckBox : public QCheckBox {
 	Q_OBJECT
-
-	QPixmap checkedImage;
-	QPixmap uncheckedImage;
-
-public:
-	VisibilityCheckBox();
-
-protected:
-	void paintEvent(QPaintEvent *event) override;
 };
