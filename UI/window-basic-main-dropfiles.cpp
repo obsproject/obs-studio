c280e620e3fe3401aac8e055e658d526163ca748
Michael Fabian Dirks <info@xaymar.com>
2019-05-17 16:56:27 +0200

    UI: Remove 5 file limit for drag & drop

--- UI/window-basic-main-dropfiles.cpp
+++ UI/window-basic-main-dropfiles.cpp
@@ -148,7 +148,7 @@ void OBSBasic::dropEvent(QDropEvent *event)
 	if (mimeData->hasUrls()) {
 		QList<QUrl> urls = mimeData->urls();
 
-		for (int i = 0; i < urls.size() && i < 5; i++) {
+		for (int i = 0; i < urls.size(); i++) {
 			QString file = urls.at(i).toLocalFile();
 			QFileInfo fileInfo(file);
 
