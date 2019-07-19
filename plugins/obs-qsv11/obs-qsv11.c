0610bc75700e5d2466daa72ae63412fdadde6e2e
brittneysclark <brittney.s.clark@intel.com>
2019-07-19 02:51:49 -0700

    obs-qsv: Enable high profile for QSV H.264
    Changing QSV H.264 default profile from "main" to "high"

--- plugins/obs-qsv11/obs-qsv11.c
+++ plugins/obs-qsv11/obs-qsv11.c
@@ -145,7 +145,7 @@ static void obs_qsv_defaults(obs_data_t *settings)
 	obs_data_set_default_string(settings, "target_usage", "balanced");
 	obs_data_set_default_int(settings, "bitrate", 2500);
 	obs_data_set_default_int(settings, "max_bitrate", 3000);
-	obs_data_set_default_string(settings, "profile", "main");
+	obs_data_set_default_string(settings, "profile", "high");
 	obs_data_set_default_int(settings, "async_depth", 4);
 	obs_data_set_default_string(settings, "rate_control", "CBR");
 
