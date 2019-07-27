baddca25365c75518df8d687547fbabe536fa349
Gol-D-Ace <Gol-D-Ace@users.noreply.github.com>
2019-07-27 06:44:24 +0200

    CI: Only download Qt if it doesn't exist already

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,4 +1,4 @@
-curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -
+if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
 7z x Qt_5.10.1.7z -oQt
 mv Qt C:\QtDep
 dir C:\QtDep
