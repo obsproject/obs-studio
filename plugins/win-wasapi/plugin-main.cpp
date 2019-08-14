48f22d9b91a088dc24d57b8d5b5c33d4a75be17d
Clayton Groeneveld <claytong1214@gmail.com>
2019-08-14 00:54:28 -0500

    win-wasapi: Fix typo with description

--- plugins/win-wasapi/plugin-main.cpp
+++ plugins/win-wasapi/plugin-main.cpp
@@ -4,7 +4,7 @@ OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("win-wasapi", "en-US")
 MODULE_EXPORT const char *obs_module_description(void)
 {
-	return "Windows WASAPI audio input/ouput sources";
+	return "Windows WASAPI audio input/output sources";
 }
 
 void RegisterWASAPIInput();
