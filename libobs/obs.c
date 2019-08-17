74a3e7246e2c77ea8714508000d1c6fa51006d92
jp9000 <obs.jim@gmail.com>
2019-08-17 06:18:37 -0700

    libobs: Fix browser source settings resetting pre-24
    Fixes an issue where the browser source settings will continually reset
    pre-24.  Note that this is not 23.2.2, but the version is being
    temporarily updated in order to fix the issue for the release candidate
    build.

--- libobs/obs.c
+++ libobs/obs.c
@@ -1817,7 +1817,7 @@ static obs_source_t *obs_load_source_type(obs_data_t *source_data)
 		source, (enum obs_deinterlace_field_order)di_order);
 
 	monitoring_type = (int)obs_data_get_int(source_data, "monitoring_type");
-	if (prev_ver < MAKE_SEMANTIC_VERSION(24, 0, 0)) {
+	if (prev_ver < MAKE_SEMANTIC_VERSION(23, 2, 2)) {
 		if ((caps & OBS_SOURCE_MONITOR_BY_DEFAULT) != 0) {
 			/* updates older sources to enable monitoring
 			 * automatically if they added monitoring by default in
