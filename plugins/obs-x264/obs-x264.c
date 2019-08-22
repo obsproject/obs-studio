68631403816f1246127e9bd8b8ff913d3bf5952c
jp9000 <obs.jim@gmail.com>
2019-08-22 16:21:59 -0700

    obs-x264: Do not display log messages every update
    Prevents log spam when dynamic bitrate is used

--- plugins/obs-x264/obs-x264.c
+++ plugins/obs-x264/obs-x264.c
@@ -549,7 +549,8 @@ static bool update_settings(struct obs_x264 *obsx264, obs_data_t *settings,
 
 	paramlist = strlist_split(opts, ' ', false);
 
-	blog(LOG_INFO, "---------------------------------");
+	if (!update)
+		blog(LOG_INFO, "---------------------------------");
 
 	if (!obsx264->context) {
 		override_base_params(obsx264, paramlist, &preset, &profile,
@@ -567,7 +568,7 @@ static bool update_settings(struct obs_x264 *obsx264, obs_data_t *settings,
 
 	if (success) {
 		update_params(obsx264, settings, paramlist, update);
-		if (opts && *opts)
+		if (opts && *opts && !update)
 			info("custom settings: %s", opts);
 
 		if (!obsx264->context)
