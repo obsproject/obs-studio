1c005fcb51c1bb7d561af91f838aac703a7c2549
Clayton Groeneveld <claytong1214@gmail.com>
2019-10-23 16:11:14 -0500

    UI: Add fade to black in studio mode

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -86,12 +86,15 @@ struct QuickTransition {
 	obs_hotkey_id hotkey = OBS_INVALID_HOTKEY_ID;
 	int duration = 0;
 	int id = 0;
+	bool fadeToBlack = false;
 
 	inline QuickTransition() {}
-	inline QuickTransition(OBSSource source_, int duration_, int id_)
+	inline QuickTransition(OBSSource source_, int duration_, int id_,
+			       bool fadeToBlack_ = false)
 		: source(source_),
 		  duration(duration_),
 		  id(id_),
+		  fadeToBlack(fadeToBlack_),
 		  renamedSignal(std::make_shared<OBSSignal>(
 			  obs_source_get_signal_handler(source), "rename",
 			  SourceRenamed, this))
@@ -477,7 +480,8 @@ public slots:
 			       bool direct = false);
 	void TransitionToScene(OBSSource scene, bool force = false,
 			       bool direct = false,
-			       bool quickTransition = false);
+			       bool quickTransition = false,
+			       bool black = false);
 	void SetCurrentScene(OBSSource scene, bool force = false,
 			     bool direct = false);
 
@@ -595,6 +599,8 @@ private:
 
 	bool LowDiskSpace();
 	void DiskSpaceMessage();
+
+	OBSSource prevFTBSource = nullptr;
 
 public:
 	OBSSource GetProgramSource();
