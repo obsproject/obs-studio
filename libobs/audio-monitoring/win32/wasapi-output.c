db7418e0ef2e8fd61fb0037d1a4b20ac27a3bf67
Exeldro <exelsor@hotmail.com>
2019-12-15 17:04:16 +0100

    libobs/audio-monitoring: only remove monitor from monitors array if set

--- libobs/audio-monitoring/win32/wasapi-output.c
+++ libobs/audio-monitoring/win32/wasapi-output.c
@@ -451,11 +451,11 @@ void audio_monitor_destroy(struct audio_monitor *monitor)
 {
 	if (monitor) {
 		audio_monitor_free(monitor);
-
-		pthread_mutex_lock(&obs->audio.monitoring_mutex);
-		da_erase_item(obs->audio.monitors, &monitor);
-		pthread_mutex_unlock(&obs->audio.monitoring_mutex);
-
+		if (obs->audio.monitors.num) {
+			pthread_mutex_lock(&obs->audio.monitoring_mutex);
+			da_erase_item(obs->audio.monitors, &monitor);
+			pthread_mutex_unlock(&obs->audio.monitoring_mutex);
+		}
 		bfree(monitor);
 	}
 }
