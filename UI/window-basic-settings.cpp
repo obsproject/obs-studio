a86912f0dba35b782ba3273b020e1ac0d7fedbe0
Clayton Groeneveld <claytong1214@gmail.com>
2020-01-27 23:45:49 -0600

    UI: Use input validator on resolution line edit

--- UI/window-basic-settings.cpp
+++ UI/window-basic-settings.cpp
@@ -793,6 +793,11 @@ OBSBasicSettings::OBSBasicSettings(QWidget *parent)
 
 	channelIndex = ui->channelSetup->currentIndex();
 	sampleRateIndex = ui->sampleRate->currentIndex();
+
+	QRegExp rx("\\d{1,5}x\\d{1,5}");
+	QValidator *validator = new QRegExpValidator(rx, this);
+	ui->baseResolution->lineEdit()->setValidator(validator);
+	ui->outputResolution->lineEdit()->setValidator(validator);
 }
 
 OBSBasicSettings::~OBSBasicSettings()
