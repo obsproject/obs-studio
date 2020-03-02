8f4f33aed8d34d6f9c632acdbb386c2f5bf4bd38
Richard Stanway <r.stanway@gmail.com>
2020-03-02 23:21:26 +0100

    UI: Move OBSBasicSettings to scoped block
    This ensures that the OBSBasicSettings destructor has been run before
    the restart prompt appears, preventing bugs related to OBS shutting down
    with the settings window still having active callbacks.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -4043,8 +4043,11 @@ void OBSBasic::on_action_Settings_triggered()
 
 	settings_already_executing = true;
 
-	OBSBasicSettings settings(this);
-	settings.exec();
+	{
+		OBSBasicSettings settings(this);
+		settings.exec();
+	}
+
 	SystemTray(false);
 
 	settings_already_executing = false;
