360af79db2fcd0e27239b908ed1d338fe49a049f
jp9000 <obs.jim@gmail.com>
2020-03-14 09:03:21 -0700

    Revert "libobs: Add extra reference when source is displayed"
    This reverts commit 9cff2ae6bb3b6e30447520ec7bd3223735e9bd5d.

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -974,8 +974,6 @@ static void deactivate_source(obs_source_t *source)
 
 static void show_source(obs_source_t *source)
 {
-	obs_source_addref(source);
-
 	if (source->context.data && source->info.show)
 		source->info.show(source->context.data);
 	obs_source_dosignal(source, "source_show", "show");
@@ -986,8 +984,6 @@ static void hide_source(obs_source_t *source)
 	if (source->context.data && source->info.hide)
 		source->info.hide(source->context.data);
 	obs_source_dosignal(source, "source_hide", "hide");
-
-	obs_source_release(source);
 }
 
 static void activate_tree(obs_source_t *parent, obs_source_t *child,
