c49c0576c411080b37250901375b076a86ca094d
hgonomeg <hgonomeg@gmail.com>
2020-10-10 00:59:31 +0200

    UI: Emit the replay buffer saved event to the api
    Send the OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED to api (as in rfc33)

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -6141,6 +6141,8 @@ void OBSBasic::ReplayBufferSave()
 		obs_output_get_proc_handler(outputHandler->replayBuffer);
 	proc_handler_call(ph, "save", &cd);
 	calldata_free(&cd);
+	if (api)
+		api->on_event(OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED);
 }
 
 void OBSBasic::ReplayBufferStop(int code)
