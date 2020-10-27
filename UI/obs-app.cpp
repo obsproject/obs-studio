ad528bef17280be4558e7cceb2dd7545eb0aa4aa
Christian Lockley <clockley1@gmail.com>
2020-10-27 17:42:45 -0500

    UI: Detect other instances of obs on Linux
    fixes: obsproject#3053

--- UI/obs-app.cpp
+++ UI/obs-app.cpp
@@ -1941,6 +1941,8 @@ static int run_program(fstream &logFile, int argc, char *argv[])
 		RunOnceMutex rom = GetRunOnceMutex(already_running);
 #elif defined(__APPLE__)
 		CheckAppWithSameBundleID(already_running);
+#elif defined(__linux__)
+		RunningInstanceCheck(already_running);
 #endif
 
 		if (!already_running) {
