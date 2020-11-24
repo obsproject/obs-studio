2d084db00caabda00d7f6e9f1e266e97c40b5ee5
Exeldro <exelsor@hotmail.com>
2020-11-24 07:11:49 +0100

    frontend-tools: Don't give every loaded filter focus
    Speeds up loading with large number of filters and prevents deadlock by
    not getting the properties of filters during
    OBSBasicFilters::UpdateFilters() which uses the source->filter_mutex

--- UI/window-basic-filters.cpp
+++ UI/window-basic-filters.cpp
@@ -225,7 +225,7 @@ void OBSBasicFilters::UpdateProperties(void *data, calldata_t *)
 				  "ReloadProperties");
 }
 
-void OBSBasicFilters::AddFilter(OBSSource filter)
+void OBSBasicFilters::AddFilter(OBSSource filter, bool focus)
 {
 	uint32_t flags = obs_source_get_output_flags(filter);
 	bool async = (flags & OBS_SOURCE_ASYNC) != 0;
@@ -238,7 +238,8 @@ void OBSBasicFilters::AddFilter(OBSSource filter)
 	item->setData(Qt::UserRole, QVariant::fromValue(filter));
 
 	list->addItem(item);
-	list->setCurrentItem(item);
+	if (focus)
+		list->setCurrentItem(item);
 	SetupVisibilityItem(list, item, filter);
 }
 
@@ -352,9 +353,15 @@ void OBSBasicFilters::UpdateFilters()
 			OBSBasicFilters *window =
 				reinterpret_cast<OBSBasicFilters *>(p);
 
-			window->AddFilter(filter);
+			window->AddFilter(filter, false);
 		},
 		this);
+
+	if (ui->asyncFilters->count() > 0) {
+		ui->asyncFilters->setCurrentItem(ui->asyncFilters->item(0));
+	} else if (ui->effectFilters->count() > 0) {
+		ui->effectFilters->setCurrentItem(ui->effectFilters->item(0));
+	}
 
 	main->SaveProject();
 }
