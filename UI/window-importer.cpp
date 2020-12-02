85d319b6c15947798b082bc3b5e7f4c78906fbae
Ryan Foster <RytoEX@gmail.com>
2020-12-02 14:29:30 -0500

    UI: Replace deprecated QLayout->setMargin with setContentsMargin
    QLayout->setMargin has been deprecated since Qt 5.13 (though marked
    obsolete since at least Qt 4.8) and removed in Qt6. It was replaced by
    QLayout->setContentsMargins, which is available in all versions of Qt5.
    Building against Qt 5.13+ can produce compiler warnings when using
    QLayout->setMargin, and warnings are bad.
    
    https://doc.qt.io/archives/qt-4.8/qlayout-obsolete.html
    https://doc.qt.io/qt-5/qlayout-obsolete.html
    https://github.com/qt/qtbase/commit/d6d33f0b80dd85043c71f71a3ed5485d6014e6c4
    https://github.com/qt/qtbase/blame/5.15.2/src/widgets/kernel/qlayout.h

--- UI/window-importer.cpp
+++ UI/window-importer.cpp
@@ -81,7 +81,7 @@ QWidget *ImporterEntryPathItemDelegate::createEditor(
 	};
 
 	QHBoxLayout *layout = new QHBoxLayout();
-	layout->setMargin(0);
+	layout->setContentsMargins(0, 0, 0, 0);
 	layout->setSpacing(0);
 
 	QLineEdit *text = new QLineEdit();
