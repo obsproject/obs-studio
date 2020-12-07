5e39d7ea1fc9b0ebab459bb9946d63e50100b698
Scratch <Scrxtchy@users.noreply.github.com>
2020-12-07 15:02:41 +1100

    obs-scripting: Fix removing signal handlers in lua

--- deps/obs-scripting/obs-scripting-lua.c
+++ deps/obs-scripting/obs-scripting-lua.c
@@ -456,7 +456,7 @@ static int obs_lua_signal_handler_disconnect(lua_State *script)
 		const char *cb_signal =
 			calldata_string(&cb->base.extra, "signal");
 
-		if (cb_signal && strcmp(signal, cb_signal) != 0 &&
+		if (cb_signal && strcmp(signal, cb_signal) == 0 &&
 		    handler == cb_handler)
 			break;
 
