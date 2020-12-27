37ab00eccf7fd0e8a3c631f2ed7c3c576e90963b
Justin Bull <me@justinbull.ca>
2020-12-27 15:37:50 -0500

    image-source: Add .webp to "All formats" option

--- plugins/image-source/image-source.c
+++ plugins/image-source/image-source.c
@@ -211,7 +211,7 @@ static void image_source_tick(void *data, float seconds)
 }
 
 static const char *image_filter =
-	"All formats (*.bmp *.tga *.png *.jpeg *.jpg *.gif *.psd);;"
+	"All formats (*.bmp *.tga *.png *.jpeg *.jpg *.gif *.psd *.webp);;"
 	"BMP Files (*.bmp);;"
 	"Targa Files (*.tga);;"
 	"PNG Files (*.png);;"
