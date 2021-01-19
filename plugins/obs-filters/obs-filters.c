8c7820026b68986fad9e2fb24f5e5ec2a75e6e5b
jpark37 <jpark37@users.noreply.github.com>
2021-01-19 15:48:47 -0800

    obs-filters: Apply sharpness filter in linear space

--- plugins/obs-filters/obs-filters.c
+++ plugins/obs-filters/obs-filters.c
@@ -21,6 +21,7 @@ extern struct obs_source_info color_key_filter;
 extern struct obs_source_info color_key_filter_v2;
 extern struct obs_source_info color_grade_filter;
 extern struct obs_source_info sharpness_filter;
+extern struct obs_source_info sharpness_filter_v2;
 extern struct obs_source_info chroma_key_filter;
 extern struct obs_source_info chroma_key_filter_v2;
 extern struct obs_source_info async_delay_filter;
@@ -51,6 +52,7 @@ bool obs_module_load(void)
 	obs_register_source(&color_key_filter_v2);
 	obs_register_source(&color_grade_filter);
 	obs_register_source(&sharpness_filter);
+	obs_register_source(&sharpness_filter_v2);
 	obs_register_source(&chroma_key_filter);
 	obs_register_source(&chroma_key_filter_v2);
 	obs_register_source(&async_delay_filter);
