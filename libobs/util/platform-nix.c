54047ef9ca06c483832d55ca839df8e00f3d8cab
jpark37 <jpark37@users.noreply.github.com>
2021-01-30 00:09:46 -0800

    libobs: Avoid strncpy warning

--- libobs/util/platform-nix.c
+++ libobs/util/platform-nix.c
@@ -460,7 +460,10 @@ struct os_dirent *os_readdir(os_dir_t *dir)
 	if (!dir->cur_dirent)
 		return NULL;
 
-	strncpy(dir->out.d_name, dir->cur_dirent->d_name, 255);
+	const size_t length = strlen(dir->cur_dirent->d_name);
+	if (sizeof(dir->out.d_name) <= length)
+		return NULL;
+	memcpy(dir->out.d_name, dir->cur_dirent->d_name, length + 1);
 
 	dstr_copy(&file_path, dir->path);
 	dstr_cat(&file_path, "/");
