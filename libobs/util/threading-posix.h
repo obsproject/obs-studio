8e2e1c4d0a4093de4116d0a6e87068cde98935f1
jpark37 <jpark37@users.noreply.github.com>
2021-02-02 22:37:08 -0800

    libobs/util: Various atomic improvements
    Add exchange functions to alias the poorly named set functions.
    
    Add store without reading previous. Faster on non-x86 processors.
    
    Add compare-exchange that updates previous to avoid redundant fetch.
    
    On Windows, load bool without conversion from char.
    
    On Windows, load using mov with compiler barrier. Still seq_cst.
    
    On POSIX, use GCC __atomic builtins.

--- libobs/util/threading-posix.h
+++ libobs/util/threading-posix.h
@@ -18,17 +18,27 @@
 
 static inline long os_atomic_inc_long(volatile long *val)
 {
-	return __sync_add_and_fetch(val, 1);
+	return __atomic_add_fetch(val, 1, __ATOMIC_SEQ_CST);
 }
 
 static inline long os_atomic_dec_long(volatile long *val)
 {
-	return __sync_sub_and_fetch(val, 1);
+	return __atomic_sub_fetch(val, 1, __ATOMIC_SEQ_CST);
+}
+
+static inline void os_atomic_store_long(volatile long *ptr, long val)
+{
+	__atomic_store_n(ptr, val, __ATOMIC_SEQ_CST);
 }
 
 static inline long os_atomic_set_long(volatile long *ptr, long val)
 {
-	return __sync_lock_test_and_set(ptr, val);
+	return __atomic_exchange_n(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+static inline long os_atomic_exchange_long(volatile long *ptr, long val)
+{
+	return os_atomic_set_long(ptr, val);
 }
 
 static inline long os_atomic_load_long(const volatile long *ptr)
@@ -39,12 +49,30 @@ static inline long os_atomic_load_long(const volatile long *ptr)
 static inline bool os_atomic_compare_swap_long(volatile long *val, long old_val,
 					       long new_val)
 {
-	return __sync_bool_compare_and_swap(val, old_val, new_val);
+	return __atomic_compare_exchange_n(val, &old_val, new_val, false,
+					   __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+}
+
+static inline bool os_atomic_compare_exchange_long(volatile long *val,
+						   long *old_val, long new_val)
+{
+	return __atomic_compare_exchange_n(val, old_val, new_val, false,
+					   __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+}
+
+static inline void os_atomic_store_bool(volatile bool *ptr, bool val)
+{
+	__atomic_store_n(ptr, val, __ATOMIC_SEQ_CST);
 }
 
 static inline bool os_atomic_set_bool(volatile bool *ptr, bool val)
 {
-	return __sync_lock_test_and_set(ptr, val);
+	return __atomic_exchange_n(ptr, val, __ATOMIC_SEQ_CST);
+}
+
+static inline bool os_atomic_exchange_bool(volatile bool *ptr, bool val)
+{
+	return os_atomic_set_bool(ptr, val);
 }
 
 static inline bool os_atomic_load_bool(const volatile bool *ptr)
