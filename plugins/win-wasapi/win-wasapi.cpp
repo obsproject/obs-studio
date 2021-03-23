016d4aa9f18dd40168aea8e382ab65123b33c9a7
Vladimir <summeroff@gmail.com>
2021-03-23 12:02:12 -0700

    check for device not to be null even if res is OK (#295)
    * check for device not to be null even if res is OK

--- plugins/win-wasapi/win-wasapi.cpp
+++ plugins/win-wasapi/win-wasapi.cpp
@@ -422,8 +422,9 @@ void WASAPISource::Initialize()
 
 	res = InitDevice(enumerator);
 
-	if (FAILED(res)) {
+	if (FAILED(res) || device.Get() == nullptr) {
 		// fail early
+		blog(LOG_ERROR, "[WASAPISource::Initialize] Device pointer is %p res is %d", device.Get(), res);
 		throw HRError("[WASAPISource::Initialize] Failed to init device", res);
 	}
 
