e624118e6c91ca52f962bebb14b1d464ec7d3412
Exeldro <exelsor@hotmail.com>
2021-03-24 10:50:28 +0100

    libobs: fix property group check

--- libobs/obs-properties.c
+++ libobs/obs-properties.c
@@ -752,7 +752,7 @@ static bool check_property_group_recursion(obs_properties_t *parent,
 				 * lets verify anyway. */
 				return true;
 			}
-			if (check_property_group_recursion(cprops, group))
+			if (check_property_group_recursion(parent, cprops))
 				return true;
 		}
 
