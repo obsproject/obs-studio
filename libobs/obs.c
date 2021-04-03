244cc5e7e1efea84aee3965a1ace8fdf6c52ee82
Richard Stanway <r.stanway@gmail.com>
2021-04-04 00:07:59 +0200

    libobs: Don't save temporarily removed sources
    The only references to these sources exist in the undo buffer, they are
    not attached to a scene and cause issues when loaded on the next
    startup.

--- libobs/obs.c
+++ libobs/obs.c
@@ -2023,7 +2023,7 @@ obs_data_array_t *obs_save_sources_filtered(obs_save_source_filter_cb cb,
 	while (source) {
 		if ((source->info.type != OBS_SOURCE_TYPE_FILTER) != 0 &&
 		    !source->context.private && !source->removed &&
-		    cb(data_, source)) {
+		    !source->temp_removed && cb(data_, source)) {
 			obs_data_t *source_data = obs_save_source(source);
 
 			obs_data_array_push_back(array, source_data);
