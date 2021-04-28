ae5ac8fa82897047e2430c10f14825bdd3c7f79d
jp9000 <obs.jim@gmail.com>
2021-04-27 20:37:37 -0700

    UI: Add repeat protection for Undo/Redo
    This allows the ability to mark an action as repeatable, which when set,
    makes it so that it will change only the redo value for the last undo
    item in the undo stack within the last three seconds. This allows the
    ability to, for example, scroll a spinbox or change a slider value
    without creating many unnecessarily duplicated undo/redo actions.

--- UI/undo-stack-obs.hpp
+++ UI/undo-stack-obs.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <QObject>
 #include <QString>
+#include <QTimer>
 
 #include <deque>
 #include <functional>
@@ -9,7 +11,9 @@
 
 #include "ui_OBSBasic.h"
 
-class undo_stack {
+class undo_stack : public QObject {
+	Q_OBJECT
+
 	typedef std::function<void(const std::string &data)> undo_redo_cb;
 	typedef std::function<void(bool is_undo)> func;
 	typedef std::unique_ptr<Ui::OBSBasic> &ui_ptr;
@@ -26,8 +30,14 @@ class undo_stack {
 	std::deque<undo_redo_t> undo_items;
 	std::deque<undo_redo_t> redo_items;
 	bool disabled = false;
+	bool last_is_repeatable = false;
+
+	QTimer repeat_reset_timer;
 
 	void clear_redo();
+
+private slots:
+	void reset_repeatable_state();
 
 public:
 	undo_stack(ui_ptr ui);
@@ -38,7 +48,7 @@ public:
 	void clear();
 	void add_action(const QString &name, undo_redo_cb undo,
 			undo_redo_cb redo, std::string undo_data,
-			std::string redo_data);
+			std::string redo_data, bool repeatable = false);
 	void undo();
 	void redo();
 };
