128bd26a1f5a99c643d52c9b4d3fa7b2384c78bc
jpark37 <jpark37@users.noreply.github.com>
2021-05-02 22:40:40 -0700

    text-freetype2: Remove SRGB code
    Not having OBS_SOURCE_SRGB is good enough now.

--- plugins/text-freetype2/text-freetype2.c
+++ plugins/text-freetype2/text-freetype2.c
@@ -248,8 +248,6 @@ static void ft2_source_render(void *data, gs_effect_t *effect)
 	if (srcdata->text == NULL || *srcdata->text == 0)
 		return;
 
-	const bool previous = gs_set_linear_srgb(false);
-
 	gs_reset_blend_state();
 	if (srcdata->outline_text)
 		draw_outlines(srcdata);
@@ -258,8 +256,6 @@ static void ft2_source_render(void *data, gs_effect_t *effect)
 
 	draw_uv_vbuffer(srcdata->vbuf, srcdata->tex, srcdata->draw_effect,
 			(uint32_t)wcslen(srcdata->text) * 6);
-
-	gs_set_linear_srgb(previous);
 
 	UNUSED_PARAMETER(effect);
 }
