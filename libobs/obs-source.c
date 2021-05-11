c1948c16b071752407ac24c469da760ca71f11e3
jpark37 <jpark37@users.noreply.github.com>
2021-05-11 03:08:17 -0700

    libobs: Fix direct rendering test
    Pass test if the filter wants SRGB, and the source supports SRGB.

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -3684,7 +3684,7 @@ static inline bool can_bypass(obs_source_t *target, obs_source_t *parent,
 	       ((parent_flags & OBS_SOURCE_CUSTOM_DRAW) == 0) &&
 	       ((parent_flags & OBS_SOURCE_ASYNC) == 0) &&
 	       (((filter_flags & OBS_SOURCE_SRGB) == 0) ||
-		((parent_flags & OBS_SOURCE_SRGB) == 0));
+		((parent_flags & OBS_SOURCE_SRGB) != 0));
 }
 
 bool obs_source_process_filter_begin(obs_source_t *filter,
