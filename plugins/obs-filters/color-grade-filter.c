002d26f51b81c4beb302e078dedaff296bf27030
jpark37 <jpark37@users.noreply.github.com>
2021-05-14 00:29:08 -0700

    obs-filters: Premultiply alpha for precision
    For Apply LUT, and Luma Key, multiply alpha in shader instead of blend
    unit for extra precision.

--- plugins/obs-filters/color-grade-filter.c
+++ plugins/obs-filters/color-grade-filter.c
@@ -452,8 +452,13 @@ static void color_grade_filter_render(void *data, gs_effect_t *effect)
 	param = gs_effect_get_param_by_name(filter->effect, "cube_width_i");
 	gs_effect_set_float(param, 1.0f / filter->cube_width);
 
+	gs_blend_state_push();
+	gs_blend_function(GS_BLEND_ONE, GS_BLEND_INVSRCALPHA);
+
 	obs_source_process_filter_tech_end(filter->context, filter->effect, 0,
 					   0, tech_name);
+
+	gs_blend_state_pop();
 
 	UNUSED_PARAMETER(effect);
 }
