6e4a5454e7f5f98f5d5fee2d464c668bfac42baa
EddyGharbi <eddygharbiema@gmail.com>
2021-05-14 13:48:59 -0700

    fix path

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -47,10 +47,10 @@ cp ../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded
 cp ../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded\ Framework.framework/Libraries/libswiftshader_libGLESv2.dylib \
 ./obs-plugins/libswiftshader_libGLESv2.dylib
 
-cp -R "../build/rundir/${BUILDCONFIG}/bin/obs64 Helper.app" "./Frameworks/obs64 Helper.app"
-cp -R "../build/rundir/${BUILDCONFIG}/bin/obs64 Helper (GPU).app" "./Frameworks/obs64 Helper (GPU).app"
-cp -R "../build/rundir/${BUILDCONFIG}/bin/obs64 Helper (Plugin).app" "./Frameworks/obs64 Helper (Plugin).app"
-cp -R "../build/rundir/${BUILDCONFIG}/bin/obs64 Helper (Renderer).app" "./Frameworks/obs64 Helper (Renderer).app"
+cp -R "../build/plugins/obs-browser/obs64 Helper.app" "./Frameworks/obs64 Helper.app"
+cp -R "../build/plugins/obs-browser/obs64 Helper (GPU).app" "./Frameworks/obs64 Helper (GPU).app"
+cp -R "../build/plugins/obs-browser/obs64 Helper (Plugin).app" "./Frameworks/obs64 Helper (Plugin).app"
+cp -R "../build/plugins/obs-browser/obs64 Helper (Renderer).app" "./Frameworks/obs64 Helper (Renderer).app"
 
 # Apply new Framework load path
 sudo install_name_tool -change \
