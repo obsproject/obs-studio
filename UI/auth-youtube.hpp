18f40be820f8abd365758dafb2dc3ffc962f9234
derrod <xlnedder@gmail.com>
2021-08-16 06:26:01 +0200

    UI: Add YouTube Chat Dock
    Since embedded browsers are no longer allowed to log into Google from the chat
    dock is effectively read-only.
    To prevent users from even trying to log in the input field is hidden
    via custom CSS.

--- UI/auth-youtube.hpp
+++ UI/auth-youtube.hpp
@@ -7,6 +7,10 @@
 
 #include "auth-oauth.hpp"
 
+#ifdef BROWSER_AVAILABLE
+class BrowserDock;
+#endif
+
 inline const std::vector<Auth::Def> youtubeServices = {
 	{"YouTube - RTMP", Auth::Type::OAuth_LinkedAccount, true},
 	{"YouTube - RTMPS", Auth::Type::OAuth_LinkedAccount, true},
@@ -19,6 +23,11 @@ class YoutubeAuth : public OAuthStreamKey {
 	std::mt19937 randomSeed;
 	std::string section;
 
+#ifdef BROWSER_AVAILABLE
+	QSharedPointer<BrowserDock> chat;
+	QSharedPointer<QAction> chatMenu;
+#endif
+
 	virtual bool RetryLogin() override;
 	virtual void SaveInternal() override;
 	virtual bool LoadInternal() override;
@@ -28,6 +37,9 @@ class YoutubeAuth : public OAuthStreamKey {
 
 public:
 	YoutubeAuth(const Def &d);
+
+	void SetChatId(QString &chat_id);
+	void ResetChat();
 
 	static std::shared_ptr<Auth> Login(QWidget *parent,
 					   const std::string &service);
