900d5645d60eff0d3f20884c4e8859c7fef3804a
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-08-26 11:13:54 +0200

    obs-internal: creation of a C++ header for mix compatibility  C++/C

---
+++ libobs/obs-internal.hpp
@@ -0,0 +1,18 @@
+#ifndef OBS_INTERNAL_HPP
+#define OBS_INTERNAL_HPP
+
+#ifdef __cplusplus
+#define private _private
+#define public _public
+extern "C" {
+#endif
+
+#include "obs-internal.h"
+
+#ifdef __cplusplus
+}
+#undef private
+#undef public
+#endif
+
+#endif
