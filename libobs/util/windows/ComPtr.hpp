83175f8662c55c4ce939a29dfb8f24999df3c8d3
jpark37 <jpark37@users.noreply.github.com>
2021-09-05 01:12:52 -0700

    libobs: FIx missing noexcept warnings
    warning C26439: This kind of function may not throw. Declare it
    'noexcept' (f.6).

--- libobs/util/windows/ComPtr.hpp
+++ libobs/util/windows/ComPtr.hpp
@@ -57,7 +57,7 @@ public:
 		if (ptr)
 			ptr->AddRef();
 	}
-	inline ComPtr(ComPtr<T> &&c) : ptr(c.ptr) { c.ptr = nullptr; }
+	inline ComPtr(ComPtr<T> &&c) noexcept : ptr(c.ptr) { c.ptr = nullptr; }
 	inline ~ComPtr() { Kill(); }
 
 	inline void Clear()
@@ -80,7 +80,7 @@ public:
 		return *this;
 	}
 
-	inline ComPtr<T> &operator=(ComPtr<T> &&c)
+	inline ComPtr<T> &operator=(ComPtr<T> &&c) noexcept
 	{
 		if (&ptr != &c.ptr) {
 			Kill();
