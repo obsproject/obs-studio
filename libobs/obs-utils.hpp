c8888c6057a26dc008bb1a5734e56a87aa2ddc1a
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-09-14 12:00:36 +0200

    libobs: add header file for obs utils function

---
+++ libobs/obs-utils.hpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+    Copyright (C) 2013 by Hugh Bailey <obs.jim@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+/* Function C ++ utilities for basic obs data */
+
+#pragma once
