a0da9220401c29161f5eadb62a8cd1ac51ca0e18
Vladimir <summeroff@gmail.com>
2021-09-17 08:50:31 -0700

    remove old amd plugin binaries (#340)

--- slobs_CI/install-script-win.cmd
+++ slobs_CI/install-script-win.cmd
@@ -5,8 +5,6 @@ set CEFURL=https://s3-us-west-2.amazonaws.com/streamlabs-cef-dist
 set CMakeGenerator=Visual Studio 16 2019
 set CefFileName=cef_binary_%CEF_VERSION%_windows64_minimal
 set GPUPriority=1
-set AMD_OLD=enc-amf_old
-set AMD_URL=https://obs-studio-deployment.s3-us-west-2.amazonaws.com/%AMD_OLD%.zip
 set OBS_VIRTUALCAM=obs-virtualsource_32bit
 set OBS_VIRTUALCAM_URL=https://obs-studio-deployment.s3-us-west-2.amazonaws.com/%OBS_VIRTUALCAM%.zip
 
@@ -16,13 +14,11 @@ cd build
 if exist %DEPS%.zip (curl -kLO %DepsURL% -f --retry 5 -z %DEPS%.zip) else (curl -kLO %DepsURL% -f --retry 5 -C -)
 if exist vlc.zip (curl -kLO %VLCURL% -f --retry 5 -z vlc.zip) else (curl -kLO %VLCURL% -f --retry 5 -C -)
 if exist %CefFileName%.zip (curl -kLO %CEFURL%/%CefFileName%.zip -f --retry 5 -z %CefFileName%.zip) else (curl -kLO %CEFURL%/%CefFileName%.zip -f --retry 5 -C -)
-if exist %AMD_OLD%.zip (curl -kLO %AMD_URL% -f --retry 5 -z %AMD_OLD%.zip) else (curl -kLO %AMD_URL% -f --retry 5 -C -)
 if exist %OBS_VIRTUALCAM%.zip (curl -kLO %OBS_VIRTUALCAM_URL% -f --retry 5 -z %OBS_VIRTUALCAM%.zip) else (curl -kLO %OBS_VIRTUALCAM_URL% -f --retry 5 -C -)
 
 7z x %DEPS%.zip -aoa -o%DEPS%
 7z x vlc.zip -aoa -ovlc
 7z x %CefFileName%.zip -aoa -oCEF
-7z x %AMD_OLD%.zip -aoa -o%AMD_OLD%
 7z x %OBS_VIRTUALCAM%.zip -aoa -o%OBS_VIRTUALCAM%
 
 set CEFPATH=%CD%\CEF\%CefFileName%
@@ -57,8 +53,6 @@ cmake -H. ^
          -DCHECK_FOR_SERVICE_UPDATES=true
 
 cmake --build %CD%\build --target install --config %BuildConfig% -v
-
-move %CD%\build\%AMD_OLD% %CD%\%InstallPath%\%AMD_OLD%
 
 mkdir %CD%\%InstallPath%\data\obs-plugins\obs-virtualoutput
 move %CD%\build\%OBS_VIRTUALCAM% %CD%\%InstallPath%\data\obs-plugins\obs-virtualoutput\%OBS_VIRTUALCAM%
