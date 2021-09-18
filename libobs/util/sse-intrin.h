c9f9bf7ced5c4ec7b7808dc6b31e843b31b5f2ae
jpark37 <jpark37@users.noreply.github.com>
2021-09-17 21:19:58 -0700

    libobs/util: Simplify emmintrin.h wrapper macro
    Also add WIN32_LEAN_AND_MEAN for external Windows.h include.

--- libobs/util/sse-intrin.h
+++ libobs/util/sse-intrin.h
@@ -17,11 +17,13 @@
 
 #pragma once
 
-#if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_IX86)) && \
-	!(defined(_M_ARM64) || defined(_M_ARM64EC))
-
+#if defined(_MSC_VER) && \
+	((defined(_M_X64) && !defined(_M_ARM64EC)) || defined(_M_IX86))
 #include <emmintrin.h>
 #else
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #define SIMDE_ENABLE_NATIVE_ALIASES
 #include "simde/x86/sse2.h"
 #endif
