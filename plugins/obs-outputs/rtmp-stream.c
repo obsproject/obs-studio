e581802812467a47680cca230e0a869b696f1bae
jpark37 <jpark37@users.noreply.github.com>
2021-09-17 22:15:56 -0700

    obs-outputs: Remove WIN32_LEAN_AND_MEAN define
    It should already be defined near the Windows.h include.

--- plugins/obs-outputs/rtmp-stream.c
+++ plugins/obs-outputs/rtmp-stream.c
@@ -15,10 +15,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
-#ifdef _MSC_VER
-#define WIN32_LEAN_AND_MEAN
-#endif
-
 #include "rtmp-stream.h"
 
 #ifndef SEC_TO_NSEC
