c7ccc3249b91a58d44a707dcea61556d2ee297a3
tytan652 <tytan652@tytanium.xyz>
2021-10-05 14:23:32 +0200

    linux-v4l2: Switch virtual camera settings to flag
    Also change "virtualcam_output" to "v4l2_output"

--- plugins/linux-v4l2/v4l2-output.c
+++ plugins/linux-v4l2/v4l2-output.c
@@ -18,7 +18,7 @@ struct virtualcam_data {
 static const char *virtualcam_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
-	return "Virtual Camera Output";
+	return "V4L2 Virtual Camera Output";
 }
 
 static void virtualcam_destroy(void *data)
@@ -162,7 +162,7 @@ static bool try_connect(void *data, const char *device)
 	vsi.height = height;
 	obs_output_set_video_conversion(vcam->output, &vsi);
 
-	blog(LOG_INFO, "Virtual camera started");
+	blog(LOG_INFO, "V4L2 Virtual camera started");
 	obs_output_begin_data_capture(vcam->output, 0);
 
 	return true;
@@ -217,7 +217,7 @@ static bool virtualcam_start(void *data)
 	free(list);
 
 	if (!success)
-		blog(LOG_WARNING, "Failed to start virtual camera");
+		blog(LOG_WARNING, "Failed to start V4L2 virtual camera");
 
 	return success;
 }
@@ -228,7 +228,7 @@ static void virtualcam_stop(void *data, uint64_t ts)
 	obs_output_end_data_capture(vcam->output);
 	close(vcam->device);
 
-	blog(LOG_INFO, "Virtual camera stopped");
+	blog(LOG_INFO, "V4L2 Virtual camera stopped");
 
 	UNUSED_PARAMETER(ts);
 }
@@ -247,8 +247,8 @@ static void virtual_video(void *param, struct video_data *frame)
 }
 
 struct obs_output_info virtualcam_info = {
-	.id = "virtualcam_output",
-	.flags = OBS_OUTPUT_VIDEO,
+	.id = "v4l2_output",
+	.flags = OBS_OUTPUT_VIDEO | OBS_OUTPUT_VIRTUALCAM,
 	.get_name = virtualcam_name,
 	.create = virtualcam_create,
 	.destroy = virtualcam_destroy,
