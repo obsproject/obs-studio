307d169bd898912f082a82864dda414bdd448304
Paul Hindt <paulh@aja.com>
2021-10-12 23:00:00 -0700

    aja: Remove disablement of clang-format in routing table and SDIWireFormat map

--- plugins/aja/routing/hdmi_rgb_display.h
+++ plugins/aja/routing/hdmi_rgb_display.h
@@ -2,46 +2,27 @@
 
 #include "../aja-routing.hpp"
 
-// clang-format off
 static const std::map<HDMIWireFormat, RoutingConfig> kHDMIRGBDisplayConfigs = {
-	{ HDMIWireFormat::HD_RGB_LFR,
-		{
-			NTV2_MODE_DISPLAY,
-			1,
-			1,
-			true,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			"fb[{ch1}][0]->hdmi[0][0];"
-		}
-	},
-	{ HDMIWireFormat::TTAP_PRO,
-		{
-			NTV2_MODE_DISPLAY,
-			1,
-			1,
-			true,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			false,
-			"fb[{ch1}][2]->dlo[{ch1}][0];"\
-			"dlo[{ch1}][0]->sdi[{ch1}][0];"\
-			"dlo[{ch1}][1]->sdi[{ch1}][1];"\
-			"fb[{ch1}][2]->hdmi[{ch1}][0];",
-		}
-	}
-};
-// clang-format on
+	{HDMIWireFormat::HD_RGB_LFR,
+	 {NTV2_MODE_DISPLAY, 1, 1, true, false, false, false, false, false,
+	  false, false, false, false, "fb[{ch1}][0]->hdmi[0][0];"}},
+	{HDMIWireFormat::TTAP_PRO,
+	 {
+		 NTV2_MODE_DISPLAY,
+		 1,
+		 1,
+		 true,
+		 false,
+		 false,
+		 false,
+		 false,
+		 false,
+		 false,
+		 false,
+		 false,
+		 false,
+		 "fb[{ch1}][2]->dlo[{ch1}][0];"
+		 "dlo[{ch1}][0]->sdi[{ch1}][0];"
+		 "dlo[{ch1}][1]->sdi[{ch1}][1];"
+		 "fb[{ch1}][2]->hdmi[{ch1}][0];",
+	 }}};
