4b6170f258156abcbc5d3b9f8a06eaead7b5e55c
jpark37 <jpark37@users.noreply.github.com>
2022-02-27 12:48:28 -0800

    win-dshow: Fix hwdevice_ctx leak

--- plugins/win-dshow/ffmpeg-decode.c
+++ plugins/win-dshow/ffmpeg-decode.c
@@ -61,6 +61,7 @@ static void init_hw_decoder(struct ffmpeg_decode *d)
 	}
 
 	if (hw_ctx) {
+		d->hw_device_ctx = hw_ctx;
 		d->decoder->hw_device_ctx = av_buffer_ref(hw_ctx);
 		d->hw = true;
 	}
@@ -116,6 +117,9 @@ void ffmpeg_decode_free(struct ffmpeg_decode *decode)
 
 	if (decode->frame)
 		av_frame_free(&decode->frame);
+
+	if (decode->hw_device_ctx)
+		av_buffer_unref(&decode->hw_device_ctx);
 
 	if (decode->packet_buffer)
 		bfree(decode->packet_buffer);
