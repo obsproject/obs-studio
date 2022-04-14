8e25ca47b0346636861368dccdcda25def9d74ed
EddyGharbi <eddygharbiema@gmail.com>
2022-04-14 13:14:21 -0700

    slobs_ci: fix sentry org (#422)

--- slobs_CI/sentry-osx.py
+++ slobs_CI/sentry-osx.py
@@ -7,13 +7,13 @@ def process_sentry(directory):
             if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
                 path = os.path.join(directory, file)
                 os.system("dsymutil " + path)
-                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
     for r, d, f in os.walk(directory):
         for file in f:
             if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
                 path = os.path.join(directory, file)
                 os.system("dsymutil " + path)
-                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server-preview " + path + ".dSYM/Contents/Resources/DWARF/" + file)
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server-preview " + path + ".dSYM/Contents/Resources/DWARF/" + file)
 
 # Upload obs debug files
 process_sentry(os.path.join(os.environ['PWD'], 'build', 'rundir', os.environ['BUILDCONFIG'], 'bin'))
