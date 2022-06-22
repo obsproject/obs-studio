obsproject/obs-studio

commit a147315f33edc59c165f8cda1120d6d4219cf6ee

project: Migrate PipeWire capture into linux-pipewire
The capture ids are preserved, so that it's compatible with the
previous code. No code changes were necessary whatsoever.

plugins/linux-pipewire/pipewire-capture.c
@@ -174,13 +174,4 @@ void pipewire_capture_load(void)
 	};
 	if (window_capture_available)
 		obs_register_source(&pipewire_window_capture_info);
-
-	pw_init(NULL, NULL);
-	initialized = true;
-}
-
-void pipewire_capture_unload(void)
-{
-	if (initialized)
-		pw_deinit();
 }

plugins/linux-capture/CMakeLists.txt
@@ -1,10 +1,5 @@
 project(linux-capture)
 
-option(ENABLE_PIPEWIRE "Enable PipeWire support" ON)
-if(NOT ENABLE_PIPEWIRE)
-  message(STATUS "OBS:    -        PipeWire support disabled")
-endif()
-
 find_package(X11 REQUIRED)
 if(NOT TARGET X11::Xcomposite)
   message(
@@ -45,35 +40,5 @@ target_link_libraries(
           XCB::XINERAMA)
 
 set_target_properties(linux-capture PROPERTIES FOLDER "plugins")
-
-if(ENABLE_PIPEWIRE)
-  find_package(PipeWire 0.3.32 QUIET)
-  find_package(Gio QUIET)
-  find_package(Libdrm QUIET)
-
-  if(NOT TARGET PipeWire::PipeWire)
-    message(
-      FATAL_ERROR
-        "OBS:    -        PipeWire library not found! Please install PipeWire or set ENABLE_PIPEWIRE=OFF"
-    )
-  elseif(NOT TARGET GIO::GIO)
-    message(
-      FATAL_ERROR
-        "OBS:    -        Gio library not found! Please install GLib2 (or Gio) or set ENABLE_PIPEWIRE=OFF"
-    )
-  elseif(NOT TARGET Libdrm::Libdrm)
-    message(
-      FATAL_ERROR
-        "OBS:    -        libdrm headers not found! Please install libdrm or set ENABLE_PIPEWIRE=OFF"
-    )
-  endif()
-
-  target_sources(linux-capture PRIVATE pipewire.c pipewire.h pipewire-capture.c
-                                       pipewire-capture.h portal.c portal.h)
-
-  target_link_libraries(linux-capture PRIVATE PipeWire::PipeWire GIO::GIO
-                                              Libdrm::Libdrm)
-  target_compile_definitions(linux-capture PRIVATE ENABLE_PIPEWIRE)
-endif()
 
 setup_plugin_target(linux-capture)

plugins/linux-capture/data/locale/ar-SA.ini
@@ -11,8 +11,3 @@ CropBottom="الاقتطاع من الأسفل (بكسل)"
 SwapRedBlue="مبادلة بين الأحمر و الأزرق"
 LockX="قفل السيرفر X عند الالتقاط"
 IncludeXBorder="تضمين حواف X"
-PipeWireDesktopCapture="التقاط الشاشة (PipeWire)"
-PipeWireSelectMonitor="حدد المراقب"
-PipeWireSelectWindow="حدد نافذة"
-PipeWireWindowCapture="التقاط الشاشة (PipeWire)"
-ShowCursor="إظهار المؤشر"

plugins/linux-capture/data/locale/bn-BD.ini
@@ -13,8 +13,3 @@ SwapRedBlue="লাল এবং নীল রঙের অদলবদল ক�
 LockX="ক্যাপচার করার সময় এক্স সার্ভার লক করুন"
 IncludeXBorder="এক্স বর্ডার অন্তর্ভুক্ত করুন"
 ExcludeAlpha="আলফা-কম গঠন বিন্যাস (মেসা পরামর্শ) ব্যবহার করুন"
-PipeWireDesktopCapture="স্ক্রিন ক্যাপচার (PipeWire)"
-PipeWireSelectMonitor="মনিটর নির্বাচন করুন"
-PipeWireSelectWindow="উইন্ডো নির্বাচন করুন"
-PipeWireWindowCapture="উইন্ডো ক্যাপচার (PipeWire)"
-ShowCursor="কার্সার দেখান"

plugins/linux-capture/data/locale/ca-ES.ini
@@ -13,8 +13,3 @@ SwapRedBlue="Intercanvia els colors vermell i blau"
 LockX="Bloca el servidor X quan s'estigui capturant"
 IncludeXBorder="Inclou-hi la vora X"
 ExcludeAlpha="Usar format de textura sense alpha (solució de Mesa)"
-PipeWireDesktopCapture="Captura de pantalla (PipeWire)"
-PipeWireSelectMonitor="Seleccioneu el monitor"
-PipeWireSelectWindow="Seleccioneu la finestra"
-PipeWireWindowCapture="Captura de finestres (PipeWire)"
-ShowCursor="Mostra el cursor"

plugins/linux-capture/data/locale/cs-CZ.ini
@@ -13,8 +13,3 @@ SwapRedBlue="Prohodit červenou a modrou"
 LockX="Zamknout server X při záznamu"
 IncludeXBorder="Zahrnout hranice X"
 ExcludeAlpha="Použít formát textury bez alpha kanálu (Mesa workaround)"
-PipeWireDesktopCapture="Záznam obrazovky (PipeWire)"
-PipeWireSelectMonitor="Vybrat monitor"
-PipeWireSelectWindow="Vybrat okno"
-PipeWireWindowCapture="Záznam okna (PipeWire)"
-ShowCursor="Zobrazit kurzor"

plugins/linux-capture/data/locale/da-DK.ini
@@ -12,8 +12,3 @@ SwapRedBlue="Ombyt rød og blå"
 LockX="Lås X server under optagelse"
 IncludeXBorder="Inkludér X-kant"
 ExcludeAlpha="Benyt alpha-fri teksturformat (Mesa løsning)"
-PipeWireDesktopCapture="Skærmoptagelse (PipeWire)"
-PipeWireSelectMonitor="Vælg skærm"
-PipeWireSelectWindow="Vælg vindue"
-PipeWireWindowCapture="Vindueoptagelse (PipeWire)"
-ShowCursor="Vis markør"

plugins/linux-capture/data/locale/de-DE.ini
@@ -13,8 +13,3 @@ SwapRedBlue="Rot und Blau tauschen"
 LockX="X-Server während der Aufnahme fixieren"
 IncludeXBorder="X-Rahmen anzeigen"
 ExcludeAlpha="Alphaloses Texturformat verwenden (Mesa-Problemumgehung)"
-PipeWireDesktopCapture="Bildschirmaufnahme (PipeWire)"
-PipeWireSelectMonitor="Bildschirm auswählen"
-PipeWireSelectWindow="Fenster auswählen"
-PipeWireWindowCapture="Fensteraufname (PipeWire)"
-ShowCursor="Mauszeiger anzeigen"

commit d00d1e2926f53a8889521fa482057aa2bb127d55

linux-pipewire: Remove unused variable
A fallout from a147315f33edc59c165f8cda1120d6d4219cf6ee

plugins/linux-pipewire/pipewire-capture.c
@@ -113,8 +113,6 @@ static void pipewire_capture_video_render(void *data, gs_effect_t *effect)
 	obs_pipewire_video_render(data, effect);
 }
 
-static bool initialized = false;
-
 void pipewire_capture_load(void)
 {
 	uint32_t available_capture_types = portal_get_available_capture_types();