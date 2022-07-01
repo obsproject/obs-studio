d09f0dea300cd155b03fdc21e869539b6bb04a09
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2022-07-01 18:28:34 -0300

    linux-pipewire: Track streams
    Make obs_pipewire hold a list of streams created by itself, and
    destroy these streams when closing the connection.

--- plugins/linux-pipewire/pipewire.c
+++ plugins/linux-pipewire/pipewire.c
@@ -64,6 +64,7 @@ struct _obs_pipewire {
 
 	struct obs_pw_version server_version;
 
+	GPtrArray *streams;
 	DARRAY(struct format_info) format_info;
 };
 
@@ -802,6 +803,8 @@ obs_pipewire *obs_pipewire_create(int pipewire_fd)
 	pw_thread_loop_wait(obs_pw->thread_loop);
 	pw_thread_loop_unlock(obs_pw->thread_loop);
 
+	obs_pw->streams = g_ptr_array_new();
+
 	return obs_pw;
 }
 
@@ -809,6 +812,13 @@ void obs_pipewire_destroy(obs_pipewire *obs_pw)
 {
 	if (!obs_pw)
 		return;
+
+	while (obs_pw->streams->len > 0) {
+		obs_pipewire_stream *obs_pw_stream =
+			g_ptr_array_index(obs_pw->streams, 0);
+		obs_pipewire_stream_destroy(obs_pw_stream);
+	}
+	g_clear_pointer(&obs_pw->streams, g_ptr_array_unref);
 
 	teardown_pipewire(obs_pw);
 	clear_format_info(obs_pw);
@@ -868,6 +878,8 @@ obs_pipewire_connect_stream(obs_pipewire *obs_pw, int pipewire_node,
 
 	pw_thread_loop_unlock(obs_pw->thread_loop);
 	bfree(params);
+
+	g_ptr_array_add(obs_pw->streams, obs_pw_stream);
 
 	return obs_pw_stream;
 }
@@ -956,6 +968,8 @@ void obs_pipewire_stream_destroy(obs_pipewire_stream *obs_pw_stream)
 {
 	if (!obs_pw_stream)
 		return;
+
+	g_ptr_array_remove(obs_pw_stream->obs_pw->streams, obs_pw_stream);
 
 	obs_enter_graphics();
 	g_clear_pointer(&obs_pw_stream->cursor.texture, gs_texture_destroy);
