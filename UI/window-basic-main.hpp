0a864ea31b40e37fd863f286345a1a7bc371e55b
tytan652 <tytan652@tytanium.xyz>
2022-07-17 14:54:46 +0200

    UI: Fix undo stack uninitialized ui warning
    Moving the declaration of `ui` above `undo_stack` fix the warning.
    
    Co-authored-by: Richard Stanway <r1ch@r1ch.net>

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -818,7 +818,6 @@ private:
 	OBSSource prevFTBSource = nullptr;
 
 public:
-	undo_stack undo_s;
 	OBSSource GetProgramSource();
 	OBSScene GetCurrentScene();
 
@@ -1150,7 +1149,14 @@ public slots:
 	void UpdateContextBarDeferred(bool force = false);
 	void UpdateContextBarVisibility();
 
+private:
+	std::unique_ptr<Ui::OBSBasic> ui;
+
 public:
+	/* `undo_s` needs to be declared after `ui` to prevent an uninitialized
+	 * warning for `ui` while initializing `undo_s`. */
+	undo_stack undo_s;
+
 	explicit OBSBasic(QWidget *parent = 0);
 	virtual ~OBSBasic();
 
@@ -1162,9 +1168,6 @@ public:
 				   const char *file) const override;
 
 	static void InitBrowserPanelSafeBlock();
-
-private:
-	std::unique_ptr<Ui::OBSBasic> ui;
 };
 
 class SceneRenameDelegate : public QStyledItemDelegate {
