614ff22e8d72aa11f27e8bd767e400aa359ce521
Ryan Foster <ryan@obsproject.com>
2022-07-22 15:34:26 -0400

    obs-outputs: Fix TLS_client init for mbedTLS 3.1.0+
    When trying to update to mbedTLS 3.2.1, I ran into failures with RTMPS
    output. After consulting an mbedTLS implementation example, I determined
    that we were not setting up the SSL/TLS context config in the correct
    order, causing the connection to fail. Performing the setup in the
    recommended order fixes that.

--- plugins/obs-outputs/librtmp/rtmp.h
+++ plugins/obs-outputs/librtmp/rtmp.h
@@ -92,10 +92,10 @@ typedef tls_ctx *TLS_CTX;
 #define TLS_client(ctx,s)	\
   s = malloc(sizeof(mbedtls_ssl_context));\
   mbedtls_ssl_init(s);\
-  mbedtls_ssl_setup(s, &ctx->conf);\
-	mbedtls_ssl_config_defaults(&ctx->conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);\
+  mbedtls_ssl_config_defaults(&ctx->conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);\
+  mbedtls_ssl_conf_rng(&ctx->conf, mbedtls_ctr_drbg_random, &ctx->ctr_drbg);\
   mbedtls_ssl_conf_authmode(&ctx->conf, MBEDTLS_SSL_VERIFY_REQUIRED);\
-	mbedtls_ssl_conf_rng(&ctx->conf, mbedtls_ctr_drbg_random, &ctx->ctr_drbg)
+  mbedtls_ssl_setup(s, &ctx->conf)
 
 #define TLS_setfd(s,fd)	mbedtls_ssl_set_bio(s, fd, mbedtls_net_send, mbedtls_net_recv, NULL)
 #define TLS_connect(s)	mbedtls_ssl_handshake(s)
