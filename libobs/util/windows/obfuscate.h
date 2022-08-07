86963f65d2b83de0616e16b87332dd1f1672387d
Richard Stanway <r1ch@r1ch.net>
2022-08-07 19:18:04 +0200

    libobs, win-capture: Don't export ms_get_obfuscated_func
    Since this was included in things like the inject helper and game
    capture DLLs, it added an additional export to those programs. Instead,
    simply share the source files directly in the projects that need it.
    
    Co-authored-by: jpark37 <jpark37@users.noreply.github.com>

--- libobs/util/windows/obfuscate.h
+++ libobs/util/windows/obfuscate.h
@@ -9,8 +9,7 @@ extern "C" {
 
 /* this is a workaround to A/Vs going crazy whenever certain functions (such as
  * OpenProcess) are used */
-EXPORT void *ms_get_obfuscated_func(HMODULE module, const char *str,
-				    uint64_t val);
+void *ms_get_obfuscated_func(HMODULE module, const char *str, uint64_t val);
 
 #ifdef __cplusplus
 }
