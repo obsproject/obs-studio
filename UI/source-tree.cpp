obsproject/obs-studio

commit adba393ca85fba19ed1bf6cd825ab8188beb2d16

UI: Fix source item widget color height
The background color of the source tree item widgets wouldn't be
rendered properly.

UI/source-tree.cpp
@@ -1088,6 +1088,8 @@ SourceTree::SourceTree(QWidget *parent_) : QListView(parent_)
 	connect(App(), &OBSApp::StyleChanged, this,
 		&SourceTree::UpdateNoSourcesMessage);
 	connect(App(), &OBSApp::StyleChanged, this, &SourceTree::UpdateIcons);
+
+	setItemDelegate(new SourceTreeDelegate(this));
 }
 
 void SourceTree::UpdateIcons()
@@ -1742,4 +1744,18 @@ void SourceTree::paintEvent(QPaintEvent *event)
 	} else {
 		QListView::paintEvent(event);
 	}
+}
+
+SourceTreeDelegate::SourceTreeDelegate(QObject *parent)
+	: QStyledItemDelegate(parent)
+{
+}
+
+QSize SourceTreeDelegate::sizeHint(const QStyleOptionViewItem &,
+				   const QModelIndex &index) const
+{
+	SourceTree *tree = qobject_cast<SourceTree *>(parent());
+	QWidget *item = tree->indexWidget(index);
+
+	return (QSize(item->width(), item->height()));
 }

UI/data/themes/Acri.qss
@@ -189,11 +189,17 @@ SourceTree {
     padding: 3px;
 }
 
+SourceTreeItem,
+QMenu::item,
+SceneTree::item {
+    padding: 6px;
+}
+
+SourceTreeItem,
 QMenu::item,
 SceneTree::item,
 SourceTree::item {
     border-radius: 4px;
-    padding: 6px;
     color: palette(text);
 	border: 0px solid transparent;
 }

UI/data/themes/Grey.qss
@@ -189,11 +189,17 @@ SourceTree {
     padding: 3px;
 }
 
+SourceTreeItem,
+QMenu::item,
+SceneTree::item {
+    padding: 6px;
+}
+
+SourceTreeItem,
 QMenu::item,
 SceneTree::item,
 SourceTree::item {
     border-radius: 4px;
-    padding: 6px;
     color: palette(text);
 	border: 0px solid transparent;
 }

UI/data/themes/Light.qss
@@ -189,11 +189,17 @@ SourceTree {
     padding: 3px;
 }
 
+SourceTreeItem,
+QMenu::item,
+SceneTree::item {
+    padding: 6px;
+}
+
+SourceTreeItem,
 QMenu::item,
 SceneTree::item,
 SourceTree::item {
     border-radius: 4px;
-    padding: 6px;
     color: palette(text);
 	border: 0px solid transparent;
 }

UI/data/themes/Rachni.qss
@@ -191,11 +191,17 @@ SourceTree {
     padding: 3px;
 }
 
+SourceTreeItem,
+QMenu::item,
+SceneTree::item {
+    padding: 6px;
+}
+
+SourceTreeItem,
 QMenu::item,
 SceneTree::item,
 SourceTree::item {
     border-radius: 4px;
-    padding: 6px;
     color: palette(text);
 	border: 0px solid transparent;
 }

UI/data/themes/Yami.qss
@@ -189,11 +189,17 @@ SourceTree {
     padding: 3px;
 }
 
+SourceTreeItem,
+QMenu::item,
+SceneTree::item {
+    padding: 6px;
+}
+
+SourceTreeItem,
 QMenu::item,
 SceneTree::item,
 SourceTree::item {
     border-radius: 4px;
-    padding: 6px;
     color: palette(text);
 	border: 0px solid transparent;
 }

UI/source-tree.hpp
@@ -8,6 +8,7 @@
 #include <QStaticText>
 #include <QSvgRenderer>
 #include <QAbstractListModel>
+#include <QStyledItemDelegate>
 #include <obs.hpp>
 #include <obs-frontend-api.h>
 
@@ -25,7 +26,7 @@ class SourceTreeSubItemCheckBox : public QCheckBox {
 	Q_OBJECT
 };
 
-class SourceTreeItem : public QWidget {
+class SourceTreeItem : public QFrame {
 	Q_OBJECT
 
 	friend class SourceTree;
@@ -217,4 +218,13 @@ protected:
 	virtual void
 	selectionChanged(const QItemSelection &selected,
 			 const QItemSelection &deselected) override;
+};
+
+class SourceTreeDelegate : public QStyledItemDelegate {
+	Q_OBJECT
+
+public:
+	SourceTreeDelegate(QObject *parent);
+	virtual QSize sizeHint(const QStyleOptionViewItem &option,
+			       const QModelIndex &index) const override;
 };

commit e0b56a434aee24072cd393dfd23711afc4a405bf

UI: Fix position of Sources dock actions on horizontal resize
Attempting to resize the Sources dock to be smaller than when initially
loaded would result in the contents of the list to never shrink.
Switching to another scene & back would temporary fix the sizing.

Fixes an issue introduced in adba393ca85fba19ed1bf6cd825ab8188beb2d16

UI/source-tree.cpp
@@ -1751,7 +1751,7 @@ SourceTreeDelegate::SourceTreeDelegate(QObject *parent)
 {
 }
 
-QSize SourceTreeDelegate::sizeHint(const QStyleOptionViewItem &,
+QSize SourceTreeDelegate::sizeHint(const QStyleOptionViewItem &option,
 				   const QModelIndex &index) const
 {
 	SourceTree *tree = qobject_cast<SourceTree *>(parent());
@@ -1760,5 +1760,5 @@ QSize SourceTreeDelegate::sizeHint(const QStyleOptionViewItem &,
 	if (!item)
 		return (QSize(0, 0));
 
-	return (QSize(item->width(), item->height()));
+	return (QSize(option.widget->minimumWidth(), item->height()));
 }