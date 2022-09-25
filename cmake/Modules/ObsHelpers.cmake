72683f3b6e5cf49ea2ceb7873cfd42c17f1457e1
derrod <dennis@obsproject.com>
2022-09-25 06:16:04 +0200

    cmake: Add PVS Studio integration
    This add the new "ENABLE_PVS" CMake option which enables PVS Studio
    analysis to run as part of a build.
    
    Also adds a patched version of the PVS-Studio CMake module and a new
    utility function to get all set build targets.

--- cmake/Modules/ObsHelpers.cmake
+++ cmake/Modules/ObsHelpers.cmake
@@ -546,3 +546,28 @@ function(_install_obs_datatarget target destination)
     COMMENT "Installing ${target} to OBS rundir"
     VERBATIM)
 endfunction()
+
+# Helper function to get all added build targets
+function(get_all_targets var)
+  set(targets)
+  get_all_targets_recursive(targets ${CMAKE_CURRENT_SOURCE_DIR})
+  set(${var}
+      ${targets}
+      PARENT_SCOPE)
+endfunction()
+
+macro(get_all_targets_recursive targets dir)
+  get_property(
+    subdirectories
+    DIRECTORY ${dir}
+    PROPERTY SUBDIRECTORIES)
+  foreach(subdir ${subdirectories})
+    get_all_targets_recursive(${targets} ${subdir})
+  endforeach()
+
+  get_property(
+    current_targets
+    DIRECTORY ${dir}
+    PROPERTY BUILDSYSTEM_TARGETS)
+  list(APPEND ${targets} ${current_targets})
+endmacro()
