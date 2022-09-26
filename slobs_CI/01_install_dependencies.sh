8e5fd5cd5e8d71f2187b6434ab5f5aab18386db9
EddyGharbi <eddygharbiema@gmail.com>
2022-09-26 15:45:43 -0700

    Merge obs28 mac2 (#466)
    * mediasoup ci fixes
    
    * fix installing webrtc and mediasoup in the wrong dir
    
    * update mediasoup
    
    * use macos-latest instead
    
    * don't use latest xcode
    
    * update mediasoup connector
    
    * comment out frontend plugins
    
    * attempt to fix finding libmediasoup
    
    * use old deps dir
    
    * go back to the root dir after building cef
    
    * go back to using checkout_dir
    
    * print pwd
    
    * add more debug print
    
    * hardcode deps full path
    
    * debug
    
    * fix hardcoded path to mediasoup deps
    
    * attempt to fix mediasoup
    
    * log cmake version
    
    * prevent install libmediasoup from failing
    
    * comment out catching error
    
    * comment out catching error 2
    
    * comment out halt on errors
    
    * change install deps order
    
    * install mediasoup plugin deps inside the build script
    
    * fix unknown hr cmd
    
    * disable sparkle
    
    * fix building mediasoup
    
    * cleanup
    
    * mac browser source
    
    * sign code
    
    * disable panels and re enbale mediasoup
    
    * obs-browser fixes
    
    * obs-browser: put back the flip flag
    
    * revert back gh actions changes

--- slobs_CI/01_install_dependencies.sh
+++ slobs_CI/01_install_dependencies.sh
@@ -139,7 +139,6 @@ install_dependencies() {
         "qt-deps ${MACOS_DEPS_VERSION:-${CI_DEPS_VERSION}} ${QT_HASH:-${CI_QT_HASH}}"
         "cef ${MACOS_CEF_BUILD_VERSION:-${CI_MACOS_CEF_VERSION}} ${CEF_HASH:-${CI_CEF_HASH}}"
         "vlc ${VLC_VERSION:-${CI_VLC_VERSION}} ${VLC_HASH:-${CI_VLC_HASH}}"
-        "sparkle ${SPARKLE_VERSION:-${CI_SPARKLE_VERSION}}"
     )
 
     install_homebrew_deps
--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -10,8 +10,19 @@ GENERATOR="Xcode"
 
 CHECKOUT_DIR="$(/usr/bin/git rev-parse --show-toplevel)"
 source "${CHECKOUT_DIR}/slobs_CI/01_install_dependencies.sh"
+cd "${CHECKOUT_DIR}"
 DEPS_BUILD_DIR="${CHECKOUT_DIR}/../obs-build-dependencies"
 BUILD_DIR="${CHECKOUT_DIR}/build"
+
+# Fetch and unzip prebuilt WEBRTC deps
+wget --quiet --retry-connrefused --waitretry=1 https://obs-studio-deployment.s3.us-west-2.amazonaws.com/webrtc_dist_m94_mac.zip
+unzip -q webrtc_dist_m94_mac.zip
+rm ./webrtc_dist_m94_mac.zip
+
+# Fetch and unzip prebuilt LIBMEDIASOUP deps
+wget --quiet --retry-connrefused --waitretry=1 https://obs-studio-deployment.s3.us-west-2.amazonaws.com/libmediasoupclient_dist_8b36a915_mac.zip
+unzip -q libmediasoupclient_dist_8b36a915_mac.zip
+rm ./libmediasoupclient_dist_8b36a915_mac.zip
 
 cmake \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-${CI_MACOSX_DEPLOYMENT_TARGET}} \
@@ -28,17 +39,18 @@ cmake \
     -DCOPIED_DEPENDENCIES=false \
     -DCOPY_DEPENDENCIES=true \
     -DENABLE_SCRIPTING=false \
+    -DENABLE_BROWSER=true \
     -DBROWSER_FRONTEND_API_SUPPORT=false \
-    -DBROWSER_PANEL_SUPPORT=false \
+    -DENABLE_BROWSER_PANELS=false \
     -DUSE_UI_LOOP=true \
     -DCHECK_FOR_SERVICE_UPDATES=true \
-    -DOBS_CODESIGN_LINKER=false \
-    -DWEBRTC_INCLUDE_PATH=$DEPS_DIR/webrtc_dist \
-    -DWEBRTC_LIB_PATH=$DEPS_DIR/webrtc_dist/libwebrtc.a \
-    -DMEDIASOUP_INCLUDE_PATH=$DEPS_DIR/libmediasoupclient_dist/include/mediasoupclient/ \
-    -DMEDIASOUP_LIB_PATH=$DEPS_DIR/libmediasoupclient_dist/lib/libmediasoupclient.a \
-    -DMEDIASOUP_SDP_LIB_PATH=$DEPS_DIR/libmediasoupclient_dist/lib/libsdptransform.a \
-    -DMEDIASOUP_SDP_INCLUDE_PATH=$DEPS_DIR/libmediasoupclient_dist/include/sdptransform \
+    -DOBS_CODESIGN_LINKER=true \
+    -DWEBRTC_INCLUDE_PATH=$PWD/webrtc_dist \
+    -DWEBRTC_LIB_PATH=$PWD/webrtc_dist/libwebrtc.a \
+    -DMEDIASOUP_INCLUDE_PATH=$PWD/libmediasoupclient_dist/include/mediasoupclient/ \
+    -DMEDIASOUP_LIB_PATH=$PWD/libmediasoupclient_dist/lib/libmediasoupclient.a \
+    -DMEDIASOUP_SDP_LIB_PATH=$PWD/libmediasoupclient_dist/lib/libsdptransform.a \
+    -DMEDIASOUP_SDP_INCLUDE_PATH=$PWD/libmediasoupclient_dist/include/sdptransform \
     -DOPENSSL_CRYPTO_LIBRARY=/usr/local/opt/openssl@3/lib/libcrypto.a \
     -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@3/include \
     -DOPENSSL_SSL_LIBRARY=/usr/local/opt/openssl@3/lib/libssl.a \
