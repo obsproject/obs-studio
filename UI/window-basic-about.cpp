602ad995815b5b2251fb80b8d5b19578e19f5243
derrod <dennis@obsproject.com>
2022-10-16 02:00:00 +0200

    UI: Hide donation CTA when running via Steam

--- UI/window-basic-about.cpp
+++ UI/window-basic-about.cpp
@@ -33,11 +33,14 @@ OBSAbout::OBSAbout(QWidget *parent) : QDialog(parent), ui(new Ui::OBSAbout)
 	ui->version->setText(ver + bitness);
 
 	ui->contribute->setText(QTStr("About.Contribute"));
-	ui->donate->setText(
-		"&nbsp;&nbsp;<a href='https://obsproject.com/contribute'>" +
-		QTStr("About.Donate") + "</a>");
-	ui->donate->setTextInteractionFlags(Qt::TextBrowserInteraction);
-	ui->donate->setOpenExternalLinks(true);
+
+	if (!steam) {
+		ui->donate->setText(
+			"&nbsp;&nbsp;<a href='https://obsproject.com/contribute'>" +
+			QTStr("About.Donate") + "</a>");
+		ui->donate->setTextInteractionFlags(Qt::TextBrowserInteraction);
+		ui->donate->setOpenExternalLinks(true);
+	}
 
 	ui->getInvolved->setText(
 		"&nbsp;&nbsp;<a href='https://github.com/obsproject/obs-studio/blob/master/CONTRIBUTING.rst'>" +
