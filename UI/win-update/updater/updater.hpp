bf1894b3821839b0080c041e4e53cdcb14afe3bb
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2022-11-20 06:18:09 -0600

    win-update: Add metadata for Windows ARM64

--- UI/win-update/updater/updater.hpp
+++ UI/win-update/updater/updater.hpp
@@ -66,6 +66,13 @@
 			"processorArchitecture='amd64' "            \
 			"publicKeyToken='6595b64144ccf1df' "        \
 			"language='*'\"")
+#elif defined _M_ARM64
+#pragma comment(linker, "/manifestdependency:\"type='win32' "       \
+			"name='Microsoft.Windows.Common-Controls' " \
+			"version='6.0.0.0' "                        \
+			"processorArchitecture='arm64' "            \
+			"publicKeyToken='6595b64144ccf1df' "        \
+			"language='*'\"")
 #else
 #pragma comment(linker, "/manifestdependency:\"type='win32' "          \
 			"name='Microsoft.Windows.Common-Controls' "    \
