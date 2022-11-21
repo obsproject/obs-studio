02d20e9f36c21069f59269ea94bf393231217e34
Jim <obs.jim@gmail.com>
2022-11-21 14:48:28 -0800

    libobs: Fix adapter index not getting applied to resets

--- libobs/obs.c
+++ libobs/obs.c
@@ -477,8 +477,6 @@ static int obs_init_graphics(struct obs_video_info *ovi)
 		}
 	}
 
-	ovi->adapter = video->adapter_index;
-
 	gs_enter_context(video->graphics);
 
 	char *filename = obs_find_data_file("default.effect");
@@ -1400,6 +1398,8 @@ int obs_reset_video(struct obs_video_info *ovi)
 			return errorcode;
 		}
 	}
+
+	ovi->adapter = obs->video.adapter_index;
 
 	const char *scale_type_name = "";
 	switch (ovi->scale_type) {
