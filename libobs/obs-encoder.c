7227f1ba8a933396be23c836ee33612aa97af31d
tt2468 <tt2468@gmail.com>
2023-01-15 20:33:58 -0800

    libobs: Remove unused internal encoder util function
    Was previously introduced in a0f679bc40cc9452f84bb7c63143bffb1ac6c348,
    but has since been made unused.

--- libobs/obs-encoder.c
+++ libobs/obs-encoder.c
@@ -175,16 +175,6 @@ static inline void get_video_info(struct obs_encoder *encoder,
 		obs_encoder_set_scaled_size(encoder, info->width, info->height);
 }
 
-static inline bool has_scaling(const struct obs_encoder *encoder)
-{
-	uint32_t video_width = video_output_get_width(encoder->media);
-	uint32_t video_height = video_output_get_height(encoder->media);
-
-	return encoder->scaled_width && encoder->scaled_height &&
-	       (video_width != encoder->scaled_width ||
-		video_height != encoder->scaled_height);
-}
-
 static inline bool gpu_encode_available(const struct obs_encoder *encoder)
 {
 	struct obs_core_video_mix *video = get_mix_for_video(encoder->media);
