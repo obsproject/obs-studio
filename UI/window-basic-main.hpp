f4d02cfa42b09c9df0462c1b2dd9266af81404cd
cg2121 <claytong1214@gmail.com>
2023-02-08 03:57:51 -0600

    UI: Add scene grid mode to view menu
    Makes toggling of the scene list grid mode also available in the
    view menu.

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -1060,6 +1060,8 @@ private slots:
 					  QListWidgetItem *prev);
 	void on_scenes_customContextMenuRequested(const QPoint &pos);
 	void GridActionClicked();
+	void on_actionSceneListMode_triggered();
+	void on_actionSceneGridMode_triggered();
 	void on_actionAddScene_triggered();
 	void on_actionRemoveScene_triggered();
 	void on_actionSceneUp_triggered();
