9af74f6d72c58cdd8cccd8cf99400fda7b5c366b
Norihiro Kamae <norihiro@nagater.net>
2023-05-27 01:13:32 +0900

    UI: Fix crash on patronJsonThread
    If a user exit obs while patronJsonThread is running, the thread crashed
    with the error below.
      QThread: Destroyed while thread is still running

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -2803,6 +2803,9 @@ OBSBasic::~OBSBasic()
 	if (updateCheckThread && updateCheckThread->isRunning())
 		updateCheckThread->wait();
 
+	if (patronJsonThread && patronJsonThread->isRunning())
+		patronJsonThread->wait();
+
 	delete screenshotData;
 	delete previewProjector;
 	delete studioProgramProjector;
