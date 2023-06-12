5c79aa288e14858a0511707f5a5d411275484b1b
tytan652 <tytan652@tytanium.xyz>
2023-06-12 14:06:18 +0200

    obs-webrtc: Remove WHIP custom service
    This type of services is provided through the custom-services plugin.

--- plugins/obs-webrtc/obs-webrtc.cpp
+++ plugins/obs-webrtc/obs-webrtc.cpp
@@ -1,7 +1,6 @@
 #include <obs-module.h>
 
 #include "whip-output.h"
-#include "whip-service.h"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("obs-webrtc", "en-US")
@@ -13,7 +12,6 @@ MODULE_EXPORT const char *obs_module_description(void)
 bool obs_module_load()
 {
 	register_whip_output();
-	register_whip_service();
 
 	return true;
 }
