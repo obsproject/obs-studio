89625715fc90624a059dd18a81b4df4432e2558f
PatTheMav <PatTheMav@users.noreply.github.com>
2023-06-30 15:11:14 +0200

    cmake: Fix configuration errors with Windows legacy path
    By default Windows builds use the legacy CMake code paths, which ideally
    work as-is with existing build directories.

--- plugins/obs-ffmpeg/obs-nvenc-test/cmake/legacy.cmake
+++ plugins/obs-ffmpeg/obs-nvenc-test/cmake/legacy.cmake
@@ -5,6 +5,7 @@ add_executable(obs-nvenc-test)
 find_package(FFnvcodec 12 REQUIRED)
 
 target_sources(obs-nvenc-test PRIVATE obs-nvenc-test.c ../obs-nvenc-ver.h)
+target_compile_definitions(obs-nvenc-test PRIVATE OBS_LEGACY)
 target_link_libraries(obs-nvenc-test d3d11 dxgi dxguid FFnvcodec::FFnvcodec)
 
 set_target_properties(obs-nvenc-test PROPERTIES FOLDER "plugins/obs-ffmpeg")
--- plugins/win-capture/cmake/legacy.cmake
+++ plugins/win-capture/cmake/legacy.cmake
@@ -52,7 +52,7 @@ if(MSVC)
 endif()
 
 target_compile_definitions(win-capture PRIVATE UNICODE _UNICODE _CRT_SECURE_NO_WARNINGS _CRT_NONSTDC_NO_WARNINGS
-                                               OBS_VERSION="${OBS_VERSION_CANONICAL}")
+                                               OBS_VERSION="${OBS_VERSION_CANONICAL}" OBS_LEGACY)
 
 set_property(GLOBAL APPEND PROPERTY OBS_MODULE_LIST "win-capture")
 
