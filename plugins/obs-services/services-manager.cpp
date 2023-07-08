f926d3bf0a30f4d09dff3adca8efde182e3525dd
tytan652 <tytan652@tytanium.xyz>
2023-07-08 15:50:02 +0200

    obs-services,json-schema: Add services update

--- plugins/obs-services/services-manager.cpp
+++ plugins/obs-services/services-manager.cpp
@@ -7,6 +7,12 @@
 #include <fstream>
 #include <inttypes.h>
 
+#ifdef UPDATE_URL
+#include <file-updater/file-updater.h>
+
+constexpr const char *OBS_SERVICE_UPDATE_URL = (const char *)UPDATE_URL;
+#endif
+
 constexpr int OBS_SERVICES_FORMAT_VER = JSON_FORMAT_VER;
 
 std::shared_ptr<ServicesManager> manager = nullptr;
@@ -90,9 +96,43 @@ bool ServicesManager::RegisterServices()
 	return true;
 }
 
+#ifdef UPDATE_URL
+static bool ConfirmServicesFile(void *, file_download_data *file)
+{
+	std::string name(file->name);
+	if (name != "services.json")
+		return false;
+
+	OBSServices::ServicesJson json;
+	try {
+		json = nlohmann::json::parse((char *)file->buffer.array);
+	} catch (nlohmann::json::exception &e) {
+		blog(LOG_DEBUG, "[obs-services][ConfirmServicesFile] %s",
+		     e.what());
+		return false;
+	}
+
+	return json.formatVersion == OBS_SERVICES_FORMAT_VER;
+}
+#endif
+
 bool ServicesManager::Initialize()
 {
 	if (!manager) {
+#ifdef UPDATE_URL
+		BPtr<char> localDir = obs_module_file("");
+		BPtr<char> cacheDir = obs_module_config_path("");
+		std::string userAgent = "obs-services (libobs ";
+		userAgent += obs_get_version_string();
+		userAgent += ")";
+		if (cacheDir) {
+			update_info_t *update = update_info_create(
+				"[obs-services][update] ", userAgent.c_str(),
+				OBS_SERVICE_UPDATE_URL, localDir, cacheDir,
+				ConfirmServicesFile, nullptr);
+			update_info_destroy(update);
+		}
+#endif
 		manager = std::make_shared<ServicesManager>();
 		return manager->RegisterServices();
 	}
