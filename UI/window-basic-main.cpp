2fbc0dabdacdf2a3c8be65c0774370b605f44333
Benjamin Schubert <brm.schubert@gmail.com>
2023-07-09 23:43:53 -0400

    UI: Don't call activateWindow() when hidden

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -2275,7 +2275,8 @@ void OBSBasic::OBSInit()
 	UpdatePreviewProgramIndicators();
 	OnFirstLoad();
 
-	activateWindow();
+	if (!hideWindowOnStart)
+		activateWindow();
 
 	/* ------------------------------------------- */
 	/* display warning message for failed modules  */
