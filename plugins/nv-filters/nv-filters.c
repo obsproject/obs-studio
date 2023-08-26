607bbeb21f7d1d6423ce5de84dead1c99d20099c
pkv <pkv@obsproject.com>
2023-08-26 21:40:09 +0200

    obs-filters: Add NVIDIA Blur Filter & Background Blur
    This adds a Background Blur filter which blurs the background for a
    foreground speaker identified by NVIDIA AI Greenscreen FX.
    Secondly, this adds a Blur filter based on NVIDIA Video Effects,
    leveraging the Background Blur Filter where the mask used just sets the
    whole frame as background.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- plugins/nv-filters/nv-filters.c
+++ plugins/nv-filters/nv-filters.c
@@ -14,6 +14,8 @@ extern void unload_nvidia_afx(void);
 #endif
 #ifdef LIBNVVFX_ENABLED
 extern struct obs_source_info nvidia_greenscreen_filter_info;
+extern struct obs_source_info nvidia_blur_filter_info;
+extern struct obs_source_info nvidia_background_blur_filter_info;
 extern bool load_nvidia_vfx(void);
 extern void unload_nvidia_vfx(void);
 #endif
@@ -29,8 +31,11 @@ bool obs_module_load(void)
 	obs_enter_graphics();
 	const bool direct3d = gs_get_device_type() == GS_DEVICE_DIRECT3D_11;
 	obs_leave_graphics();
-	if (direct3d && load_nvidia_vfx())
+	if (direct3d && load_nvidia_vfx()) {
 		obs_register_source(&nvidia_greenscreen_filter_info);
+		obs_register_source(&nvidia_blur_filter_info);
+		obs_register_source(&nvidia_background_blur_filter_info);
+	}
 #endif
 	return true;
 }
