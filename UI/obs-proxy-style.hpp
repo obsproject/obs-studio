c3fa7dad26bd035cfe53b2aa209e9a336b124626
derrod <dennis@obsproject.com>
2023-09-03 06:25:27 +0200

    UI: Add workaround for Qt tooltip stylesheet bug

--- UI/obs-proxy-style.hpp
+++ UI/obs-proxy-style.hpp
@@ -2,11 +2,19 @@
 
 #include <QProxyStyle>
 
+#if defined(_WIN32) && QT_VERSION == QT_VERSION_CHECK(6, 5, 2)
+#define QT_TOOLTIP_WORKAROUND_NEEDED
+#endif
+
 class OBSProxyStyle : public QProxyStyle {
 public:
 	int styleHint(StyleHint hint, const QStyleOption *option,
 		      const QWidget *widget,
 		      QStyleHintReturn *returnData) const override;
+
+#ifdef QT_TOOLTIP_WORKAROUND_NEEDED
+	void polish(QWidget *widget) override;
+#endif
 };
 
 class OBSContextBarProxyStyle : public OBSProxyStyle {
