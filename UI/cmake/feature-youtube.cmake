3239e2a1f2ef5d9a4c16b9b4e7528f127cfc2928
Ryan Foster <ryan@obsproject.com>
2023-09-06 13:01:28 -0400

    UI: Restrict regex pattern for OAuth secrets
    The current regex pattern will successfully match if the cache variable
    contains the pattern anywhere inside the string. Let's restrict the
    regex pattern such that it requires the pattern to be precisely between
    the beginning and end of the string with no other characters in between
    those anchor points.

--- UI/cmake/feature-youtube.cmake
+++ UI/cmake/feature-youtube.cmake
@@ -1,7 +1,7 @@
 if(YOUTUBE_CLIENTID
    AND YOUTUBE_SECRET
-   AND YOUTUBE_CLIENTID_HASH MATCHES "(0|[a-fA-F0-9]+)"
-   AND YOUTUBE_SECRET_HASH MATCHES "(0|[a-fA-F0-9]+)")
+   AND YOUTUBE_CLIENTID_HASH MATCHES "^(0|[a-fA-F0-9]+)$"
+   AND YOUTUBE_SECRET_HASH MATCHES "^(0|[a-fA-F0-9]+)$")
   target_sources(
     obs-studio
     PRIVATE # cmake-format: sortable
