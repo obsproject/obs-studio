e065f2a839157074b79eec7935a277bb716d495b
PatTheMav <PatTheMav@users.noreply.github.com>
2023-09-09 00:06:53 +0200

    libobs: Update CMake target source lists with alphabetic sorting

---
+++ libobs/cmake/obs-version.cmake
@@ -0,0 +1,13 @@
+add_library(libobs-version OBJECT)
+add_library(OBS::libobs-version ALIAS libobs-version)
+
+configure_file(obsversion.c.in obsversion.c @ONLY)
+
+target_sources(
+  libobs-version
+  PRIVATE obsversion.c
+  PUBLIC obsversion.h)
+
+target_include_directories(libobs-version PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>")
+
+set_property(TARGET libobs-version PROPERTY FOLDER core)
--- libobs/cmake/os-macos.cmake
+++ libobs/cmake/os-macos.cmake
@@ -1,28 +1,30 @@
 target_link_libraries(
   libobs
-  PRIVATE "$<LINK_LIBRARY:FRAMEWORK,Cocoa.framework>"
-          "$<LINK_LIBRARY:FRAMEWORK,CoreAudio.framework>"
+  PRIVATE # cmake-format: sortable
+          "$<LINK_LIBRARY:FRAMEWORK,AppKit.framework>"
           "$<LINK_LIBRARY:FRAMEWORK,AudioToolbox.framework>"
           "$<LINK_LIBRARY:FRAMEWORK,AudioUnit.framework>"
-          "$<LINK_LIBRARY:FRAMEWORK,AppKit.framework>"
-          "$<LINK_LIBRARY:FRAMEWORK,IOKit.framework>"
-          "$<LINK_LIBRARY:FRAMEWORK,Carbon.framework>")
+          "$<LINK_LIBRARY:FRAMEWORK,Carbon.framework>"
+          "$<LINK_LIBRARY:FRAMEWORK,Cocoa.framework>"
+          "$<LINK_LIBRARY:FRAMEWORK,CoreAudio.framework>"
+          "$<LINK_LIBRARY:FRAMEWORK,IOKit.framework>")
 
 target_sources(
   libobs
-  PRIVATE obs-cocoa.m
+  PRIVATE # cmake-format: sortable
           audio-monitoring/osx/coreaudio-enum-devices.c
           audio-monitoring/osx/coreaudio-monitoring-available.c
           audio-monitoring/osx/coreaudio-output.c
           audio-monitoring/osx/mac-helpers.h
+          obs-cocoa.m
+          util/apple/cfstring-utils.h
           util/pipe-posix.c
           util/platform-cocoa.m
           util/platform-nix.c
           util/threading-posix.c
-          util/threading-posix.h
-          util/apple/cfstring-utils.h)
+          util/threading-posix.h)
 
 target_compile_options(libobs PUBLIC -Wno-strict-prototypes -Wno-shorten-64-to-32)
 
-set_property(SOURCE util/platform-cocoa.m obs-cocoa.m PROPERTY COMPILE_FLAGS -fobjc-arc)
+set_property(SOURCE obs-cocoa.m util/platform-cocoa.m PROPERTY COMPILE_FLAGS -fobjc-arc)
 set_property(TARGET libobs PROPERTY FRAMEWORK TRUE)
