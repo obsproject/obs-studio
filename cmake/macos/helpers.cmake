621fefb82a0ce1606e24ff2993b4cb83ed93b210
PatTheMav <PatTheMav@users.noreply.github.com>
2023-11-16 20:39:49 +0100

    cmake: Align Product Name and Bundle Name on macOS

--- cmake/macos/helpers.cmake
+++ cmake/macos/helpers.cmake
@@ -50,7 +50,7 @@ function(set_target_properties_obs target)
     if(target STREQUAL obs-studio)
       set_target_properties(
         ${target}
-        PROPERTIES OUTPUT_NAME OBS
+        PROPERTIES OUTPUT_NAME "OBS Studio"
                    MACOSX_BUNDLE TRUE
                    MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_SOURCE_DIR}/cmake/macos/Info.plist.in"
                    XCODE_EMBED_FRAMEWORKS_REMOVE_HEADERS_ON_COPY YES
@@ -62,7 +62,7 @@ function(set_target_properties_obs target)
       set_target_xcode_properties(
         ${target}
         PROPERTIES PRODUCT_BUNDLE_IDENTIFIER com.obsproject.obs-studio
-                   PRODUCT_NAME OBS
+                   PRODUCT_NAME "OBS Studio"
                    ASSETCATALOG_COMPILER_APPICON_NAME AppIcon
                    CURRENT_PROJECT_VERSION ${OBS_BUILD_NUMBER}
                    MARKETING_VERSION ${OBS_VERSION_CANONICAL}
@@ -75,6 +75,7 @@ function(set_target_properties_obs target)
                    INFOPLIST_KEY_NSHumanReadableCopyright "(c) 2012-${CURRENT_YEAR} Lain Bailey"
                    INFOPLIST_KEY_NSCameraUsageDescription "OBS needs to access the camera to enable camera sources to work."
                    INFOPLIST_KEY_NSMicrophoneUsageDescription "OBS needs to access the microphone to enable audio input.")
+
       # cmake-format: on
 
       get_property(obs_dependencies GLOBAL PROPERTY _OBS_DEPENDENCIES)
