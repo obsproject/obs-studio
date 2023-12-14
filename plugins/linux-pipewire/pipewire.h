afab71aa8070685230e3545260ae72d1466f6931
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-12-13 21:14:35 -0300

    linux-pipewire: Standardize argument names
    Always name obs_pipewire_stream pointers 'obs_pw_stream', so as not
    to confuse with obs_pipewire pointers which are called 'obs_pw'.

--- plugins/linux-pipewire/pipewire.h
+++ plugins/linux-pipewire/pipewire.h
@@ -36,13 +36,13 @@ obs_pipewire_connect_stream(obs_pipewire *obs_pw, obs_source_t *source,
 			    int pipewire_node, const char *stream_name,
 			    struct pw_properties *stream_properties);
 
-void obs_pipewire_stream_show(obs_pipewire_stream *obs_pw);
-void obs_pipewire_stream_hide(obs_pipewire_stream *obs_pw);
-uint32_t obs_pipewire_stream_get_width(obs_pipewire_stream *obs_pw);
-uint32_t obs_pipewire_stream_get_height(obs_pipewire_stream *obs_pw);
-void obs_pipewire_stream_video_render(obs_pipewire_stream *obs_pw,
+void obs_pipewire_stream_show(obs_pipewire_stream *obs_pw_stream);
+void obs_pipewire_stream_hide(obs_pipewire_stream *obs_pw_stream);
+uint32_t obs_pipewire_stream_get_width(obs_pipewire_stream *obs_pw_stream);
+uint32_t obs_pipewire_stream_get_height(obs_pipewire_stream *obs_pw_stream);
+void obs_pipewire_stream_video_render(obs_pipewire_stream *obs_pw_stream,
 				      gs_effect_t *effect);
 
-void obs_pipewire_stream_set_cursor_visible(obs_pipewire_stream *obs_pw,
+void obs_pipewire_stream_set_cursor_visible(obs_pipewire_stream *obs_pw_stream,
 					    bool cursor_visible);
 void obs_pipewire_stream_destroy(obs_pipewire_stream *obs_pw_stream);
