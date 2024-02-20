337b36c3acf0fa7b5138a4ab1d7b753de4abfe6c
pkv <pkv@obsproject.com>
2024-02-20 16:52:09 +0100

    libobs: Add ASIO monitoring track
    On windows, this adds an additional audio track (Track 7) which is used
    to output a mix of monitored sources. Currently the mixing is left to
    WASAPI on Windows for WASAPI monitoring devices.
    But ASIO SDK doesn't allow multiple clients (although individual drivers
     might allow it) and therefore can not mix them. So we have to do the
    mixing on OBS side.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- libobs/media-io/audio-io.h
+++ libobs/media-io/audio-io.h
@@ -24,13 +24,20 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+#ifdef _WIN32
+// extra track for ASIO monitoring on windows only
+#define MAX_AUDIO_MONITORING_MIXES 1
+#else
+#define MAX_AUDIO_MONITORING_MIXES 0
+#endif
 
 #define MAX_AUDIO_MIXES 6
+
 #define MAX_AUDIO_CHANNELS 8
 #define MAX_DEVICE_INPUT_CHANNELS 64
 #define AUDIO_OUTPUT_FRAMES 1024
 
-#define TOTAL_AUDIO_SIZE (MAX_AUDIO_MIXES * MAX_AUDIO_CHANNELS * AUDIO_OUTPUT_FRAMES * sizeof(float))
+#define TOTAL_AUDIO_SIZE ((MAX_AUDIO_MIXES + MAX_AUDIO_MONITORING_MIXES) * MAX_AUDIO_CHANNELS * AUDIO_OUTPUT_FRAMES * sizeof(float))
 
 /*
  * Base audio output component.  Use this to create an audio output track
