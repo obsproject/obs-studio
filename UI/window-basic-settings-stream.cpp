a5665a1c9aeccaf0ca098956525dc22b285615d8
pkv <pkv@obsproject.com>
2024-02-27 00:03:34 +0100

    UI: Remove LoadOutputSettings from save sequence
    This removes the LoadOutputSettings() called in SaveStream1Settings.
    It caused a bug when trying to save Output settings when both Stream
    and Output settings have been changed but the changes have not been
    applied by hitting the 'Apply' button in Settings.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- UI/window-basic-settings-stream.cpp
+++ UI/window-basic-settings-stream.cpp
@@ -255,7 +255,6 @@ void OBSBasicSettings::SaveStream1Settings()
 
 	main->SetService(newService);
 	main->SaveService();
-	LoadOutputSettings();
 	main->auth = auth;
 	if (!!main->auth) {
 		main->auth->LoadUI();
