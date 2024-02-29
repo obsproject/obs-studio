7952cd40f9d3fe96dab332839f3db94e8f6ccc58
Vladimir <summeroff@gmail.com>
2024-02-28 16:05:44 -0800

    add audio processing to new transition (#574)

--- plugins/obs-transitions/transition-shuffle.c
+++ plugins/obs-transitions/transition-shuffle.c
@@ -93,6 +93,18 @@ void shuffle_video_render(void *data, gs_effect_t *effect)
 	UNUSED_PARAMETER(effect);
 }
 
+static float mix_a(void *data, float t)
+{
+	UNUSED_PARAMETER(data);
+	return 1.0f - cubic_ease_in_out(t);
+}
+
+static float mix_b(void *data, float t)
+{
+	UNUSED_PARAMETER(data);
+	return cubic_ease_in_out(t);
+}
+
 static bool shuffle_audio_render(void *data, uint64_t *ts_out,
 				 struct obs_source_audio_mix *audio,
 				 uint32_t mixers, size_t channels,
@@ -100,7 +112,7 @@ static bool shuffle_audio_render(void *data, uint64_t *ts_out,
 {
 	struct shuffle_info *pm = data;
 	return obs_transition_audio_render(pm->source, ts_out, audio, mixers,
-					   channels, sample_rate, NULL, NULL);
+					   channels, sample_rate, mix_a, mix_b);
 }
 
 static enum gs_color_space
