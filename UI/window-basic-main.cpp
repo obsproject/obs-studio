e4e544eae7b37cacaca50acbf49993845e4afc5f
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-02-29 20:46:07 +0000

    UI: Set recording paused before anything else
    When this call was first introduced in eab10d48b2, it was at top of this
    block, albeit after the calls to `pause`. Over time it has slowly
    shifted lower and lower in the block.
    
    In reality, it should be the first thing in the block to ensure that
    further calls have accurate information about the pause state to update
    themselves.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -10690,6 +10690,8 @@ void OBSBasic::PauseRecording()
 	obs_output_t *output = outputHandler->fileOutput;
 
 	if (obs_output_pause(output, true)) {
+		os_atomic_set_bool(&recording_paused, true);
+
 		pause->setAccessibleName(QTStr("Basic.Main.UnpauseRecording"));
 		pause->setToolTip(QTStr("Basic.Main.UnpauseRecording"));
 		pause->blockSignals(true);
@@ -10711,8 +10713,6 @@ void OBSBasic::PauseRecording()
 			trayIcon->setIcon(QIcon::fromTheme("obs-tray-paused",
 							   trayIconFile));
 		}
-
-		os_atomic_set_bool(&recording_paused, true);
 
 		auto replay = replayBufferButton ? replayBufferButton->second()
 						 : nullptr;
@@ -10736,6 +10736,8 @@ void OBSBasic::UnpauseRecording()
 	obs_output_t *output = outputHandler->fileOutput;
 
 	if (obs_output_pause(output, false)) {
+		os_atomic_set_bool(&recording_paused, false);
+
 		pause->setAccessibleName(QTStr("Basic.Main.PauseRecording"));
 		pause->setToolTip(QTStr("Basic.Main.PauseRecording"));
 		pause->blockSignals(true);
@@ -10757,8 +10759,6 @@ void OBSBasic::UnpauseRecording()
 			trayIcon->setIcon(QIcon::fromTheme("obs-tray-active",
 							   trayIconFile));
 		}
-
-		os_atomic_set_bool(&recording_paused, false);
 
 		auto replay = replayBufferButton ? replayBufferButton->second()
 						 : nullptr;
