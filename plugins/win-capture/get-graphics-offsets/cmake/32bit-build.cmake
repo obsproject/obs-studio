7899ac4a2e7e28023e4b3e58a4f0956d5d6ce4fd
PatTheMav <PatTheMav@users.noreply.github.com>
2024-03-16 15:19:02 +0100

    win-capture: Add missing MSVC runtime setting to 32-bit build
    (cherry picked from commit 14654807cb23868630cc7fbaac6bc83f77879b92)

--- plugins/win-capture/get-graphics-offsets/cmake/32bit-build.cmake
+++ plugins/win-capture/get-graphics-offsets/cmake/32bit-build.cmake
@@ -2,4 +2,7 @@ project(get-graphics-offsets)
 
 add_executable(get-graphics-offsets)
 target_link_libraries(get-graphics-offsets PRIVATE _get-graphics-offsets)
-set_property(TARGET get-graphics-offsets PROPERTY OUTPUT_NAME get-graphics-offsets32)
+
+# cmake-format: off
+set_target_properties(get-graphics-offsets PROPERTIES OUTPUT_NAME get-graphics-offsets32 MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+# cmake-format: on
