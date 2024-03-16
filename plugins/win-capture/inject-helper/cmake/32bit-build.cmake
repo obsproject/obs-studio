0dac4765d6aa6a708f16cdd032b14ff6d82d71b4
PatTheMav <PatTheMav@users.noreply.github.com>
2024-03-16 15:19:02 +0100

    win-capture: Add missing MSVC runtime setting to 32-bit build

--- plugins/win-capture/inject-helper/cmake/32bit-build.cmake
+++ plugins/win-capture/inject-helper/cmake/32bit-build.cmake
@@ -2,4 +2,7 @@ project(inject-helper)
 
 add_executable(inject-helper)
 target_link_libraries(inject-helper PRIVATE _inject-helper)
-set_property(TARGET inject-helper PROPERTY OUTPUT_NAME inject-helper32)
+
+# cmake-format: off
+set_target_properties(inject-helper PROPERTIES OUTPUT_NAME inject-helper32 MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+# cmake-format: on
