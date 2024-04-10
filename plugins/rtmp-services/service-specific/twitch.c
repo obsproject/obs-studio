1a7c1e65e9c84cecce91d9b49dceb83d1386966b
Ruwen Hahn <haruwenz@twitch.tv>
2024-04-10 16:39:17 +0200

    rtmp-services: Add Amazon IVS ingest refresh infrastructure

--- plugins/rtmp-services/service-specific/twitch.c
+++ plugins/rtmp-services/service-specific/twitch.c
@@ -33,6 +33,16 @@ static struct service_ingests twitch = {
 	.cache_old_filename = "twitch_ingests.json",
 	.cache_new_filename = "twitch_ingests.new.json"};
 
+static struct service_ingests amazon_ivs = {
+	.update_info = NULL,
+	.mutex = PTHREAD_MUTEX_INITIALIZER,
+	.ingests_refreshed = false,
+	.ingests_refreshing = false,
+	.ingests_loaded = false,
+	.cur_ingests = {0},
+	.cache_old_filename = "amazon_ivs_ingests.json",
+	.cache_new_filename = "amazon_ivs_ingests.new.json"};
+
 static void free_ingests(struct service_ingests *si)
 {
 	for (size_t i = 0; i < si->cur_ingests.num; i++) {
@@ -258,4 +268,50 @@ void unload_service_data(struct service_ingests *si)
 void unload_twitch_data(void)
 {
 	unload_service_data(&twitch);
+}
+
+void init_amazon_ivs_data(void)
+{
+	init_service_data(&amazon_ivs);
+}
+
+void load_amazon_ivs_data(void)
+{
+	struct ingest def = {
+		.name = bstrdup("Default"),
+		.url = bstrdup(
+			"rtmps://ingest.global-contribute.live-video.net:443/app/")};
+	load_service_data(&amazon_ivs, "amazon_ivs_ingests.json", &def);
+}
+
+void unload_amazon_ivs_data(void)
+{
+	unload_service_data(&amazon_ivs);
+}
+
+void amazon_ivs_ingests_refresh(int seconds)
+{
+	service_ingests_refresh(
+		&amazon_ivs, seconds, "[amazon ivs ingest update] ",
+		"https://ingest.contribute.live-video.net/ingests");
+}
+
+void amazon_ivs_ingests_lock(void)
+{
+	pthread_mutex_lock(&amazon_ivs.mutex);
+}
+
+void amazon_ivs_ingests_unlock(void)
+{
+	pthread_mutex_unlock(&amazon_ivs.mutex);
+}
+
+size_t amazon_ivs_ingest_count(void)
+{
+	return amazon_ivs.cur_ingests.num;
+}
+
+struct twitch_ingest amazon_ivs_ingest(size_t idx)
+{
+	return get_ingest(&amazon_ivs, idx);
 }
--- plugins/rtmp-services/service-specific/twitch.h
+++ plugins/rtmp-services/service-specific/twitch.h
@@ -9,3 +9,8 @@ extern void twitch_ingests_lock(void);
 extern void twitch_ingests_unlock(void);
 extern size_t twitch_ingest_count(void);
 extern struct twitch_ingest twitch_ingest(size_t idx);
+
+extern void amazon_ivs_ingests_lock(void);
+extern void amazon_ivs_ingests_unlock(void);
+extern size_t amazon_ivs_ingest_count(void);
+extern struct twitch_ingest amazon_ivs_ingest(size_t idx);
