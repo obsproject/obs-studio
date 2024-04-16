5aa187c040247894bada79833c9d17607d3a9261
Gale <thy-lan.gale@intel.com>
2024-04-16 15:21:57 -0700

    win-mf: Add Intel NPU AI Features

--- plugins/win-mediafoundation/capture/libmfcapture/win-mf.hpp
+++ plugins/win-mediafoundation/capture/libmfcapture/win-mf.hpp
@@ -1,6 +1,6 @@
 /*
 
-This is provided under a dual MIT/GPLv2 license.  When using or
+This is provided under a dual MIT/BSD/GPLv2 license.  When using or
 redistributing this, you may do so under either license.
 
 GPL LICENSE SUMMARY
@@ -42,6 +42,39 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE
+
+BSD LICENSE
+
+Copyright(c) 2025 Intel Corporation.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the
+distribution.
+
+* Neither the name of Intel Corporation nor the names of its
+contributors may be used to endorse or promote products derived
+from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 */
 
 #pragma once
@@ -117,7 +150,28 @@ const std::string_view TEXT_RANGE_DEFAULT = obs_module_text("ColorRange.Default"
 const std::string_view TEXT_RANGE_PARTIAL = obs_module_text("ColorRange.Partial");
 const std::string_view TEXT_RANGE_FULL = obs_module_text("ColorRange.Full");
 const char *TEXT_DWNS = obs_module_text("DeactivateWhenNotShowing");
+
+constexpr const char *INTELNPU_BLUR_TYPE = "intelnpu_blur_type";
+const char *TEXT_INTELNPU_BLUR_TYPE = obs_module_text("IntelNPU.BackgroundBlur");
+const char *TEXT_INTELNPU_BLUR_NONE = obs_module_text("IntelNPU.BlurNone");
+const char *TEXT_INTELNPU_BLUR_STANDARD = obs_module_text("IntelNPU.BlurStandard");
+const char *TEXT_INTELNPU_BLUR_PORTRAIT = obs_module_text("IntelNPU.Portrait");
+
+constexpr const char *INTELNPU_BACKGROUND_REMOVAL = "intelnpu_background_removal";
+const char *TEXT_INTELNPU_BACKGROUND_REMOVAL = obs_module_text("IntelNPU.BackgroundRemoval");
+
+constexpr const char *INTELNPU_AUTO_FRAMING = "intelnpu_auto_framing";
+const char *TEXT_INTELNPU_AUTO_FRAMING = obs_module_text("IntelNPU.AutoFraming");
+
+constexpr const char *INTELNPU_EYEGAZE_CORRECTION = "intelnpu_eyegaze_correction";
+const char *TEXT_INTELNPU_EYEGAZE_CORRECTION = obs_module_text("IntelNPU.EyeGazeCorrection");
 } // namespace
+
+enum BlurType {
+	NpuBlurType_None,
+	NpuBlurType_Standard,
+	NpuBlurType_Portrait,
+};
 
 enum ResType {
 	ResTypePreferred,
@@ -130,7 +184,7 @@ enum class BufferingType : int64_t {
 	Off,
 };
 
-enum class Action { None, Activate, ActivateBlock, Deactivate, Shutdown, SaveSettings, RestoreSettings };
+enum class Action { None, Activate, ActivateBlock, Deactivate, Shutdown, SaveSettings, RestoreSettings, NpuControl };
 
 struct MediaFoundationVideoInfo {
 	int minCX;
