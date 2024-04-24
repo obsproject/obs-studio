8382672aecb80a621f53bff4896be560c698f29c
Kurt Kartaltepe <kkartaltepe@gmail.com>
2024-04-23 20:31:10 -0700

    libobs: Remove extra gs_flush calls
    These calls introduce pipeline stalls in OpenGL on low end machines
    which can contribute to a fairly significant 10-30% of total rendering
    time.
    
    The driver already needs to synchronize events within the context so
    these calls are not needed for correctness for OpenGL.

--- libobs/obs-video.c
+++ libobs/obs-video.c
@@ -605,7 +605,6 @@ static inline void render_video(struct obs_core_video_mix *video,
 			copy_surfaces = video->copy_surfaces_encode;
 			channel_count = 1;
 #endif
-			gs_flush();
 		}
 
 		if (video->gpu_conversion) {
@@ -614,7 +613,6 @@ static inline void render_video(struct obs_core_video_mix *video,
 		}
 
 		if (gpu_active) {
-			gs_flush();
 			output_gpu_encoders(video, raw_active);
 		}
 
