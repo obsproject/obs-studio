ea4dc84e7e88ba802ec665f9cd6ed25d942f6c41
Suman Manjunath <msuman@google.com>
2024-05-17 16:43:19 +0000

    UI: Implement DeleteCookies for YT Service dock
    This will enable centralized cookie management for YouTube/Google
    account logins for other browser docks using the shared cookie manager.

--- UI/auth-youtube.cpp
+++ UI/auth-youtube.cpp
@@ -55,6 +55,14 @@ static inline void OpenBrowser(const QString auth_uri)
 	QDesktopServices::openUrl(url);
 }
 
+static void DeleteCookies()
+{
+	if (panel_cookies) {
+		panel_cookies->DeleteCookies("youtube.com", "");
+		panel_cookies->DeleteCookies("google.com", "");
+	}
+}
+
 void RegisterYoutubeAuth()
 {
 	for (auto &service : youtubeServices) {
@@ -64,7 +72,7 @@ void RegisterYoutubeAuth()
 				return std::make_shared<YoutubeApiWrappers>(
 					service);
 			},
-			YoutubeAuth::Login, []() { return; });
+			YoutubeAuth::Login, DeleteCookies);
 	}
 }
 
