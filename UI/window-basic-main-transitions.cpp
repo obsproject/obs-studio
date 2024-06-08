f9f4171d56022a3cf74f554802d84960adf78e02
Richard Stanway <r1ch@r1ch.net>
2024-06-08 17:44:29 +0200

    UI: Add null checks before doing some API calls
    Harmless, but generated a debug warning for null pointers passed into
    the API.

--- UI/window-basic-main-transitions.cpp
+++ UI/window-basic-main-transitions.cpp
@@ -445,7 +445,8 @@ void OBSBasic::SetTransition(OBSSource transition)
 	ui->transitionDurationLabel->setVisible(!fixed);
 	ui->transitionDuration->setVisible(!fixed);
 
-	bool configurable = obs_source_configurable(transition);
+	bool configurable = transition ? obs_source_configurable(transition)
+				       : false;
 	ui->transitionRemove->setEnabled(configurable);
 	ui->transitionProps->setEnabled(configurable);
 
