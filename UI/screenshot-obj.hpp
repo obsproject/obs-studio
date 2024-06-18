0306effc5f8c14829bfd7599b9513ef98c005767
cg2121 <claytong1214@gmail.com>
2024-06-18 15:41:07 -0500

    UI: Add pragma once to ScreenshotObj header
    Makes sure header is only included once preventing code clashes.

--- UI/screenshot-obj.hpp
+++ UI/screenshot-obj.hpp
@@ -15,6 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
+#pragma once
+
 #include <QObject>
 #include <string>
 #include <thread>
