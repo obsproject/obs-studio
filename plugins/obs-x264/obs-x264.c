edcda5a825a530a0d6f7bc9ee501e130ff0b6193
derrod <dennis@obsproject.com>
2024-06-22 17:29:41 +0200

    obs-x264: Ignore stats/qp file and multipass options

--- plugins/obs-x264/obs-x264.c
+++ plugins/obs-x264/obs-x264.c
@@ -309,7 +309,9 @@ static inline void set_param(struct obs_x264 *obsx264, struct obs_option option)
 	if (strcmp(name, "preset") != 0 && strcmp(name, "profile") != 0 &&
 	    strcmp(name, "tune") != 0 && strcmp(name, "fps") != 0 &&
 	    strcmp(name, "force-cfr") != 0 && strcmp(name, "width") != 0 &&
-	    strcmp(name, "height") != 0 && strcmp(name, "opencl") != 0) {
+	    strcmp(name, "height") != 0 && strcmp(name, "opencl") != 0 &&
+	    strcmp(name, "stats") != 0 && strcmp(name, "qpfile") != 0 &&
+	    strcmp(name, "pass") != 0) {
 		if (strcmp(option.name, OPENCL_ALIAS) == 0)
 			name = "opencl";
 		if (x264_param_parse(&obsx264->params, name, val) != 0)
