22076e1291fb222e08bb90e2f4652b2e4840b9eb
Penwywern <gaspard.larrouturou@protonmail.com>
2024-08-15 00:36:02 +0200

    frontend: Log item/filter visibility changes

--- frontend/components/SourceTreeItem.cpp
+++ frontend/components/SourceTreeItem.cpp
@@ -136,8 +136,13 @@ SourceTreeItem::SourceTreeItem(SourceTree *tree_, OBSSceneItem sceneitem_) : tre
 			OBSSourceAutoRelease s = obs_get_source_by_uuid(uuid.c_str());
 			obs_scene_t *sc = obs_group_or_scene_from_source(s);
 			obs_sceneitem_t *si = obs_scene_find_sceneitem_by_id(sc, id);
-			if (si)
+			if (si) {
 				obs_sceneitem_set_visible(si, val);
+				const char *item_name = obs_source_get_name(obs_sceneitem_get_source(si));
+				const char *name = obs_source_get_name(s);
+				blog(LOG_INFO, "User set sceneitem '%s' (%li) on scene '%s' to %s (undo_redo)",
+				     item_name, (long)id, name, val ? "enabled" : "disabled");
+			}
 		};
 
 		QString str = QTStr(val ? "Undo.ShowSceneItem" : "Undo.HideSceneItem");
@@ -149,6 +154,10 @@ SourceTreeItem::SourceTreeItem(SourceTree *tree_, OBSSceneItem sceneitem_) : tre
 
 		QSignalBlocker sourcesSignalBlocker(this);
 		obs_sceneitem_set_visible(sceneitem, val);
+
+		const char *item_name = obs_source_get_name(obs_sceneitem_get_source(sceneitem));
+		blog(LOG_INFO, "User set sceneitem '%s' (%li) on scene '%s' to %s", item_name, (long)id, name,
+		     val ? "enabled" : "disabled");
 	};
 
 	auto setItemLocked = [this](bool checked) {
