c521b23619f9938bc00de38d9ca15a3fe0061685
derrod <dennis@obsproject.com>
2024-08-29 00:39:33 +0200

    libobs: Always set initial scene item pos to top-left corner
    In relative mode (0, 0) is the center of the screen, so in order to
    maintain previous behaviour we need to convert the value here.

--- libobs/obs-scene.c
+++ libobs/obs-scene.c
@@ -2558,6 +2558,10 @@ static obs_sceneitem_t *obs_scene_add_internal(obs_scene_t *scene,
 	matrix4_identity(&item->draw_transform);
 	matrix4_identity(&item->box_transform);
 
+	/* Ensure initial position is still top-left corner in relative mode. */
+	if (!item->absolute_coordinates)
+		pos_from_absolute(&item->pos, &item->pos, item);
+
 	if (source_has_audio(source)) {
 		item->visible = false;
 		da_push_back(item->audio_actions, &action);
