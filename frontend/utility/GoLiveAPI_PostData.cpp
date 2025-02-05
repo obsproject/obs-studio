7c8d09b1f0feb214c59ee97547cca82bf79bfd54
Dennis Sädtler <saedtler@twitch.tv>
2025-02-05 13:30:12 +0100

    frontend: Always limit multitrack video encoders to libobs maximum

--- frontend/utility/GoLiveAPI_PostData.cpp
+++ frontend/utility/GoLiveAPI_PostData.cpp
@@ -71,5 +71,11 @@ GoLiveApi::PostData constructGoLivePost(QString streamKey, const std::optional<u
 	if (maximum_video_tracks.has_value())
 		preferences.maximum_video_tracks = maximum_video_tracks.value();
 
+	/* Always cap to maximum number of output encoders. */
+	if (!preferences.maximum_video_tracks.has_value() ||
+	    preferences.maximum_video_tracks.value() > MAX_OUTPUT_VIDEO_ENCODERS) {
+		preferences.maximum_video_tracks = MAX_OUTPUT_VIDEO_ENCODERS;
+	}
+
 	return post_data;
 }
