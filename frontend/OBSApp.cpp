63bd103b64bbf72d29d4c07a72edd745f88f2b42
Philippe Schommers <philippe@schommers.be>
2025-02-07 01:12:13 +0100

    frontend: Add --config-dir flag
    Allows users to set the directory where all configurations are stored.

--- frontend/OBSApp.cpp
+++ frontend/OBSApp.cpp
@@ -66,6 +66,7 @@ extern bool opt_disable_updater;
 extern bool opt_disable_missing_files_check;
 extern string opt_starting_collection;
 extern string opt_starting_profile;
+extern string opt_config_dir;
 
 extern QPointer<OBSLogViewer> obsLogViewer;
 
@@ -1459,6 +1460,14 @@ vector<pair<string, string>> GetLocaleNames()
 
 int GetAppConfigPath(char *path, size_t size, const char *name)
 {
+	if (!opt_config_dir.empty()) {
+		if (name && *name) {
+			return snprintf(path, size, "%s/%s", opt_config_dir.c_str(), name);
+		} else {
+			return snprintf(path, size, "%s", opt_config_dir.c_str());
+		}
+	}
+
 #if ALLOW_PORTABLE_MODE
 	if (portable_mode) {
 		if (name && *name) {
@@ -1476,6 +1485,16 @@ int GetAppConfigPath(char *path, size_t size, const char *name)
 
 char *GetAppConfigPathPtr(const char *name)
 {
+	if (!opt_config_dir.empty()) {
+		char path[512];
+
+		if (snprintf(path, sizeof(path), "%s/%s", opt_config_dir.c_str(), name) > 0) {
+			return bstrdup(path);
+		} else {
+			return NULL;
+		}
+	}
+
 #if ALLOW_PORTABLE_MODE
 	if (portable_mode) {
 		char path[512];
