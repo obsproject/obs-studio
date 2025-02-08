df97bddd879bc9bb20ddfae96395a7792754d9e2
PatTheMav <PatTheMav@users.noreply.github.com>
2025-02-08 17:00:33 +0100

    cmake: Update cross-platform build project management for Windows
    Enables creation of x64 and x86 child projects when building on ARM64
    and decouples functionality from legacy_check function

--- cmake/windows/buildspec.cmake
+++ cmake/windows/buildspec.cmake
@@ -24,6 +24,21 @@ function(_check_dependencies_windows)
   set(platform windows-${arch})
 
   _check_dependencies()
+
+  if(CMAKE_VS_PLATFORM_NAME STREQUAL ARM64 AND NOT QT_HOST_PATH)
+    file(READ "${CMAKE_CURRENT_SOURCE_DIR}/buildspec.json" buildspec)
+
+    string(JSON dependency_data GET ${buildspec} dependencies)
+    string(JSON data GET ${dependency_data} qt6)
+    string(JSON version GET ${data} version)
+    set(qt_x64_dir "${CMAKE_CURRENT_SOURCE_DIR}/.deps/obs-deps-qt6-${version}-x64")
+
+    if(IS_DIRECTORY "${qt_x64_dir}")
+      set(QT_HOST_PATH "${qt_x64_dir}" CACHE STRING "Qt Host Tools Path" FORCE)
+    else()
+      message(FATAL_ERROR "Building OBS Studio for Windows ARM64 requires x64 Qt dependencies")
+    endif()
+  endif()
 endfunction()
 
 _check_dependencies_windows()
