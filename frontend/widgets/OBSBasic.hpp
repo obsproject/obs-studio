22016a8da44f91713e13739c01dca6ecee008fac
tytan652 <tytan652@tytanium.xyz>
2025-02-18 16:07:03 +0100

    frontend: Reduce reliance on transition indexes

--- frontend/widgets/OBSBasic.hpp
+++ frontend/widgets/OBSBasic.hpp
@@ -1457,7 +1457,7 @@ private:
 	QPointer<QMenu> perSceneTransitionMenu;
 
 	std::unordered_map<std::string, OBSSource> transitions;
-	/* TODO: Reduce usages of an index to identify a transition */
+	/* NOTE: There is a reliance on the order of insertion */
 	std::vector<std::string> transitionUuids;
 	/* FIXME: Replace usages of a name to identify a transition */
 	std::unordered_map<std::string, std::string> transitionNameToUuids;
