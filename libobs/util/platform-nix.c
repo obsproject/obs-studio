obsproject/obs-studio

commit 935613816fd8ed384ba86b035bd73af025258a11

libobs/util: Update `os_get_free_size()`
`os_get_free_size()` was simply returning 0. For Linux,
implement the free size calculation based on the `sysinfo()`
system call.

libobs/util/platform-nix.c
@@ -1032,11 +1032,6 @@ uint64_t os_get_proc_virtual_size(void)
 	return (uint64_t)kinfo.ki_size;
 }
 #else
-uint64_t os_get_sys_free_size(void)
-{
-	return 0;
-}
-
 typedef struct {
 	unsigned long virtual_size;
 	unsigned long resident_size;
@@ -1115,6 +1110,20 @@ uint64_t os_get_sys_total_size(void)
 		os_get_sys_total_size_internal();
 
 	return total_memory;
+}
+
+uint64_t os_get_sys_free_size(void)
+{
+	uint64_t free_memory = 0;
+#ifndef __OpenBSD__
+	struct sysinfo info;
+	if (sysinfo(&info) < 0)
+		return 0;
+
+	free_memory = ((uint64_t)info.freeram + (uint64_t)info.bufferram) * info.mem_unit;
+#endif
+
+	return free_memory;
 }
 #endif
 

commit 5f69097cd0094e91beb09f8432779c9d05256470

libobs/util: Fix build on FreeBSD
FreeBSD already implemented os_get_free_size.  Move the new Linux
implementation into the existing not-__FreeBSD__ block.

Fixes: 935613816fd8 ("libobs/util: Update `os_get_free_size()`")

libobs/util/platform-nix.c
@@ -1087,6 +1087,20 @@ uint64_t os_get_proc_virtual_size(void)
 		return 0;
 	return (uint64_t)statm.virtual_size;
 }
+
+uint64_t os_get_sys_free_size(void)
+{
+	uint64_t free_memory = 0;
+#ifndef __OpenBSD__
+	struct sysinfo info;
+	if (sysinfo(&info) < 0)
+		return 0;
+
+	free_memory = ((uint64_t)info.freeram + (uint64_t)info.bufferram) * info.mem_unit;
+#endif
+
+	return free_memory;
+}
 #endif
 
 static uint64_t total_memory = 0;
@@ -1113,19 +1127,6 @@ uint64_t os_get_sys_total_size(void)
 	return total_memory;
 }
 
-uint64_t os_get_sys_free_size(void)
-{
-	uint64_t free_memory = 0;
-#ifndef __OpenBSD__
-	struct sysinfo info;
-	if (sysinfo(&info) < 0)
-		return 0;
-
-	free_memory = ((uint64_t)info.freeram + (uint64_t)info.bufferram) * info.mem_unit;
-#endif
-
-	return free_memory;
-}
 #endif
 
 #ifndef __APPLE__