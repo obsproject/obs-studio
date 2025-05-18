8077e8f2e4410a055e40ca0727256d23c623593c
pkv <pkv@obsproject.com>
2025-05-18 21:10:09 +0200

    UI: Add to Settings > Audio ASIO monitoring
    This adds a QToolButton to the Audio Settings on Windows.
    This triggers in turn a dedicated settings for the ASIO monitoring
    output.
    If no ASIO driver is detected in the system, the panel displays an
    explanatory message.
    The panel allows to:
    - select an ASIO monitoring device;
    - for each output channel of the ASIO device, one can select any channel
    from any of the 6 tracks or from the monitoring mix.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- frontend/plugins/asio-output-ui/asio-ui-main.cpp
+++ frontend/plugins/asio-output-ui/asio-ui-main.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
     Copyright (C) 2022-2026 pkv <pkv@obsproject.com>
 
-    This file is part of win-asio.
+    This file is part of asio-output-ui which requires win-asio.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@
 #include <util/util.hpp>
 
 #include <QMainWindow>
+#include <QScreen>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("asio-output-ui", "en-US")
@@ -35,11 +36,16 @@ struct asio_ui_output {
 	OBSData settings;
 };
 
+bool output_running = false;
+std::string g_currentDeviceName;
+
+namespace {
+constexpr int MAX_ASIO_DEVICE_CHANNELS = 32;
+constexpr int SHIFT_WINDOW = 100;
 // We use a global context for asio output because we allow a single output device.
 struct asio_ui_output context = {0};
-bool output_running = false;
 ASIOSettingsDialog *settingsDialog_ = nullptr;
-std::string g_currentDeviceName;
+} // namespace
 
 OBSData load_settings()
 {
@@ -54,8 +60,6 @@ OBSData load_settings()
 	return nullptr;
 }
 
-#define MAX_DEVICE_CHANNELS 32
-
 void save_default_settings(obs_data_t *settings)
 {
 	BPtr<char> modulePath = obs_module_get_config_path(obs_current_module(), "");
@@ -63,7 +67,7 @@ void save_default_settings(obs_data_t *settings)
 	BPtr<char> path = obs_module_get_config_path(obs_current_module(), "asioOutputProps.json");
 	obs_data_t *data = obs_data_create();
 	obs_data_set_string(data, "device_name", obs_data_get_string(settings, "device_name"));
-	for (int i = 0; i < MAX_DEVICE_CHANNELS; i++) {
+	for (int i = 0; i < MAX_ASIO_DEVICE_CHANNELS; i++) {
 		char key[32];
 		snprintf(key, sizeof(key), "device_ch%d", i);
 		obs_data_set_int(data, key, -1);
@@ -88,21 +92,53 @@ void output_start()
 	}
 }
 
+void callback()
+{
+	QMainWindow *mainWindow = (QMainWindow *)obs_frontend_get_main_window();
+	QWidget *obsSettingsDialog = nullptr;
+	const auto topLevels = QApplication::topLevelWidgets();
+	for (QWidget *widget : topLevels) {
+		if (widget->isVisible() && QString(widget->metaObject()->className()).contains("OBSBasicSettings")) {
+			obsSettingsDialog = widget;
+			break;
+		}
+	}
+	if (!settingsDialog_) {
+		if (!obsSettingsDialog) {
+			settingsDialog_ = new ASIOSettingsDialog(mainWindow, context.output, context.settings);
+		} else {
+			settingsDialog_ = new ASIOSettingsDialog(obsSettingsDialog, context.output, context.settings);
+		}
+
+		settingsDialog_->setAttribute(Qt::WA_DeleteOnClose);
+		QObject::connect(settingsDialog_, &QObject::destroyed, []() { settingsDialog_ = nullptr; });
+	}
+
+	settingsDialog_->showHideDialog(context.enabled);
+	if (obsSettingsDialog) {
+		QRect settingsRect = obsSettingsDialog->geometry();
+		QRect asioRect = settingsDialog_->geometry();
+		QPoint newPos(settingsRect.right() + SHIFT_WINDOW, settingsRect.top());
+		QScreen *screen = obsSettingsDialog->screen();
+		QRect desktopRect = screen->availableGeometry();
+		if (newPos.x() + asioRect.width() > desktopRect.right()) {
+			newPos.setX(desktopRect.right() - asioRect.width());
+		}
+
+		settingsDialog_->move(newPos);
+	}
+}
+
 void addOutputUI(void)
 {
 	QAction *action = (QAction *)obs_frontend_add_tools_menu_qaction(obs_module_text("AsioOutput.Menu"));
-
-	QMainWindow *mainWindow = (QMainWindow *)obs_frontend_get_main_window();
+	action->setObjectName("asioOutputSetupAction");
 
 	obs_frontend_push_ui_translation(obs_module_get_string);
-	settingsDialog_ = new ASIOSettingsDialog(mainWindow, context.output, context.settings);
 	obs_frontend_pop_ui_translation();
-
-	auto cb = []() {
-		settingsDialog_->ShowHideDialog();
-	};
-
-	action->connect(action, &QAction::triggered, cb);
+	// The UI is added through the callback, which is triggered in OBS Audio Settings.
+	action->connect(action, &QAction::triggered, callback);
+	action->setVisible(false);
 }
 
 static void OBSEvent(enum obs_frontend_event event, void *)
@@ -133,10 +169,15 @@ void obs_module_unload(void)
 	if (output_running)
 		output_stop();
 
-	obs_output_release(context.output);
-	context.output = nullptr;
-	obs_data_release(context.settings);
-	context.settings = nullptr;
+	if (context.output) {
+		obs_output_release(context.output);
+		context.output = nullptr;
+	}
+
+	if (context.settings) {
+		obs_data_release(context.settings);
+		context.settings = nullptr;
+	}
 	obs_frontend_remove_event_callback(OBSEvent, nullptr);
 }
 
@@ -146,8 +187,11 @@ void obs_module_post_load(void)
 		return;
 
 	context.settings = load_settings();
+
 	obs_output_t *const output = obs_output_create("asio_output", "asio_output", context.settings, NULL);
+
 	if (output != nullptr) {
+		context.enabled = true;
 		context.output = output;
 
 		if (!context.settings) {
@@ -158,5 +202,8 @@ void obs_module_post_load(void)
 		obs_frontend_add_event_callback(OBSEvent, nullptr);
 	} else {
 		blog(LOG_INFO, "Failed to create ASIO output");
+		// We add the UI even if there is no output to display a text saying ASIO is disabled
+		// so that users know that ASIO exists.
+		addOutputUI();
 	}
 }
