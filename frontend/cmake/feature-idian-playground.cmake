106984841154acba386d1f729c951235833a06bb
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-05-28 22:52:52 +0200

    frontend: Unify naming of Idian Playground
    It's called Idian Playground in some places and Widget Playground in
    others. Idian Playground is more precise so let's use that.

--- frontend/cmake/feature-idian-playground.cmake
+++ frontend/cmake/feature-idian-playground.cmake
@@ -1,11 +1,11 @@
-option(ENABLE_WIDGET_PLAYGROUND "Enable building custom widget demo window" OFF)
+option(ENABLE_IDIAN_PLAYGROUND "Enable building custom idian widget demo window" OFF)
 
-if(ENABLE_WIDGET_PLAYGROUND)
+if(ENABLE_IDIAN_PLAYGROUND)
   target_sources(
     obs-studio
     PRIVATE forms/OBSIdianPlayground.ui dialogs/OBSIdianPlayground.hpp dialogs/OBSIdianPlayground.cpp
   )
-  target_enable_feature(obs-studio "Widget Playground" ENABLE_WIDGET_PLAYGROUND)
+  target_enable_feature(obs-studio "Idian Playground" ENABLE_IDIAN_PLAYGROUND)
 else()
-  target_disable_feature(obs-studio "Widget Playground")
+  target_disable_feature(obs-studio "Idian Playground")
 endif()
