a9a506e984c0e149409082a0f5fbfac5ede64498
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-05-29 21:05:32 +0200

    shared/idian: Make checked status of collapsible rows public

--- shared/qt/idian/widgets/Row.cpp
+++ shared/qt/idian/widgets/Row.cpp
@@ -300,7 +300,6 @@ CollapsibleRow::CollapsibleRow(QWidget *parent) : GenericRow(parent)
 	actionRow->setFocusProxy(expandButton);
 
 	connect(expandButton, &QAbstractButton::clicked, this, &CollapsibleRow::toggleVisibility);
-
 	connect(actionRow, &Row::clicked, expandButton, &QAbstractButton::click);
 }
 
@@ -316,6 +315,7 @@ void CollapsibleRow::setCheckable(bool check)
 
 		actionRow->setSuffix(toggleSwitch, false);
 		connect(toggleSwitch, &ToggleSwitch::toggled, propertyList, &PropertiesList::setEnabled);
+		connect(toggleSwitch, &ToggleSwitch::toggled, this, &CollapsibleRow::toggled);
 	}
 
 	if (!checkable && toggleSwitch) {
@@ -324,6 +324,15 @@ void CollapsibleRow::setCheckable(bool check)
 
 		actionRow->suffix()->deleteLater();
 	}
+}
+
+void CollapsibleRow::setChecked(bool checked)
+{
+	if (!isCheckable()) {
+		throw std::logic_error("Called setChecked on a non-checkable row.");
+	}
+
+	toggleSwitch->setChecked(checked);
 }
 
 void CollapsibleRow::setTitle(const QString &name)
