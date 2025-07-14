2b3ce1e595fdada83690be56d400281853cae36f
PatTheMav <PatTheMav@users.noreply.github.com>
2025-07-14 21:20:16 +0200

    libobs: Ensure that C language compile options are not used for Swift
    Without this change any Swift target that links against libobs would
    inherit C compiler flags that are invalid for the Swift compiler.

--- libobs/cmake/os-macos.cmake
+++ libobs/cmake/os-macos.cmake
@@ -26,7 +26,7 @@ target_sources(
     util/threading-posix.h
 )
 
-target_compile_options(libobs PUBLIC -Wno-strict-prototypes -Wno-shorten-64-to-32)
+target_compile_options(libobs PUBLIC "$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:-Wno-strict-prototypes;-Wno-shorten-64-to-32>")
 
 set_property(SOURCE obs-cocoa.m util/platform-cocoa.m PROPERTY COMPILE_OPTIONS -fobjc-arc)
 set_property(TARGET libobs PROPERTY FRAMEWORK TRUE)
