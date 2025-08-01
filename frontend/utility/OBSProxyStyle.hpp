28ca612d9e3c22565d19e648dc01791a1684ac8a
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-08-01 19:05:45 +0200

    frontend: Remove unused OBSContextBarProxyStyle
    All usages of OBSContextBarProxyStyle were removed in [1], so the class
    is completely unused now. It should be deleted.
    
    [1] 8dcfae9a390763645d354700f36caef6b8f482c7

--- frontend/utility/OBSProxyStyle.hpp
+++ frontend/utility/OBSProxyStyle.hpp
@@ -13,9 +13,3 @@ public:
 	int styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget,
 		      QStyleHintReturn *returnData) const override;
 };
-
-class OBSContextBarProxyStyle : public OBSProxyStyle {
-public:
-	QPixmap generatedIconPixmap(QIcon::Mode iconMode, const QPixmap &pixmap,
-				    const QStyleOption *option) const override;
-};
