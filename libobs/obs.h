42c719a297167d9b134235a91ec73c87df33d628
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-08-09 18:42:28 +0200

    libobs: Remove obs_view_get_video_info
    Deprecated since fc891295b69c7a079d6168d686a222bf726ff171 (30.1).

--- libobs/obs.h
+++ libobs/obs.h
@@ -923,9 +923,6 @@ EXPORT video_t *obs_view_add2(obs_view_t *view, struct obs_video_info *ovi);
 /** Removes a view from the main render loop */
 EXPORT void obs_view_remove(obs_view_t *view);
 
-/** Gets the video settings currently in use for this view context, returns false if no video */
-OBS_DEPRECATED EXPORT bool obs_view_get_video_info(obs_view_t *view, struct obs_video_info *ovi);
-
 /** Enumerate the video info of all mixes using the specified view context */
 EXPORT void obs_view_enum_video_info(obs_view_t *view, bool (*enum_proc)(void *, struct obs_video_info *), void *param);
 
