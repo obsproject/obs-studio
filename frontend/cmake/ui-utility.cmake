861cc5f65483bf7865ce09732e58c58b0545543f
Warchamp7 <warchamp2003@hotmail.com>
2025-09-27 00:53:02 -0400

    frontend: Replace add source dropdown with dialog
    Co-Authored-By: Lain <134130700+Lain-B@users.noreply.github.com>

--- frontend/cmake/ui-utility.cmake
+++ frontend/cmake/ui-utility.cmake
@@ -47,6 +47,7 @@ target_sources(
     utility/RemuxQueueModel.hpp
     utility/RemuxWorker.cpp
     utility/RemuxWorker.hpp
+    utility/ResizeSignaler.hpp
     utility/SceneRenameDelegate.cpp
     utility/SceneRenameDelegate.hpp
     utility/ScreenshotObj.cpp
@@ -56,6 +57,8 @@ target_sources(
     utility/SimpleOutput.hpp
     utility/StartMultiTrackVideoStreamingGuard.hpp
     utility/SurfaceEventFilter.hpp
+    utility/ThumbnailManager.cpp
+    utility/ThumbnailManager.hpp
     utility/VCamConfig.hpp
     utility/VolumeMeterTimer.cpp
     utility/VolumeMeterTimer.hpp
