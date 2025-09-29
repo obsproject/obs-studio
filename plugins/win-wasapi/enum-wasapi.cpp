66c3693fb1a50763dfe67d14c3d260cafb3f9856
pkv <pkv@obsproject.com>
2025-09-30 01:30:38 +0200

    win-wasapi: Enable channel selections
    This adds the capability to select input channels exposed to obs.
    (The same is already possible for coreaudio input sources.)
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- plugins/win-wasapi/enum-wasapi.cpp
+++ plugins/win-wasapi/enum-wasapi.cpp
@@ -1,6 +1,7 @@
 #include "enum-wasapi.hpp"
 
 #include <util/base.h>
+#include <util/bmem.h>
 #include <util/platform.h>
 #include <util/windows/HRError.hpp>
 #include <util/windows/ComPtr.hpp>
@@ -32,6 +33,26 @@ string GetDeviceName(IMMDevice *device)
 	}
 
 	return device_name;
+}
+
+std::string GetWASAPIDefaultDeviceName(bool input)
+{
+	ComPtr<IMMDeviceEnumerator> enumerator;
+	ComPtr<IMMDevice> device;
+
+	HRESULT res = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator),
+				       (void **)enumerator.Assign());
+	if (FAILED(res)) {
+		return "";
+	}
+
+	res = enumerator->GetDefaultAudioEndpoint(input ? eCapture : eRender, input ? eCommunications : eConsole,
+						  device.Assign());
+	if (FAILED(res)) {
+		return "";
+	}
+
+	return GetDeviceName(device);
 }
 
 static void GetWASAPIAudioDevices_(vector<AudioDeviceInfo> &devices, bool input)
@@ -89,4 +110,46 @@ void GetWASAPIAudioDevices(vector<AudioDeviceInfo> &devices, bool input)
 	} catch (HRError &error) {
 		blog(LOG_WARNING, "[GetWASAPIAudioDevices] %s: %lX", error.str, error.hr);
 	}
+}
+
+int GetWASAPIDeviceInputChannels(const char *device_id)
+{
+	ComPtr<IMMDeviceEnumerator> enumerator;
+	ComPtr<IMMDevice> device;
+	ComPtr<IAudioClient> client;
+	CoTaskMemPtr<WAVEFORMATEX> wfex;
+
+	HRESULT res =
+		CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, IID_PPV_ARGS(enumerator.Assign()));
+	if (FAILED(res)) {
+		return 0;
+	}
+
+	if (strcmp(device_id, "default") == 0) {
+		res = enumerator->GetDefaultAudioEndpoint(eCapture, eCommunications, device.Assign());
+	} else {
+		wchar_t *w_id = nullptr;
+		os_utf8_to_wcs_ptr(device_id, strlen(device_id), &w_id);
+		if (!w_id) {
+			return 0;
+		}
+		res = enumerator->GetDevice(w_id, device.Assign());
+		bfree(w_id);
+	}
+
+	if (FAILED(res) || !device) {
+		return 0;
+	}
+
+	res = device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, (void **)client.Assign());
+	if (FAILED(res)) {
+		return 0;
+	}
+
+	res = client->GetMixFormat(&wfex);
+	if (FAILED(res) || !wfex) {
+		return 0;
+	}
+
+	return wfex->nChannels;
 }
