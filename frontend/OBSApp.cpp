fad011f976eec44f34cc70c2a8deeff41264ff98
Henri Kulotie <dankirk_88@msn.com>
2025-09-29 23:39:04 +0300

    logging: Use ISO 8601 time format for logging
    Switch locale-aware timestamping for logging / crash handling to %H:%M:%S
    
    Update frontend/OBSApp.cpp
    
    Co-authored-by: Patrick Heyer <PatTheMav@users.noreply.github.com>
    
    Update libobs/obs-win-crash-handler.c
    
    Co-authored-by: Patrick Heyer <PatTheMav@users.noreply.github.com>

--- frontend/OBSApp.cpp
+++ frontend/OBSApp.cpp
@@ -285,7 +285,7 @@ string CurrentDateTimeString()
 	struct tm tstruct;
 	char buf[80];
 	tstruct = *localtime(&now);
-	strftime(buf, sizeof(buf), "%Y-%m-%d, %X", &tstruct);
+	strftime(buf, sizeof(buf), "%Y-%m-%d, %H:%M:%S", &tstruct);
 	return buf;
 }
 
