157d3f4e9719cf49f3d2a22f19989c5a327d160c
tytan652 <tytan652@tytanium.xyz>
2025-10-18 17:07:14 +0200

    frontend: Move transition change events

--- frontend/widgets/OBSBasic_Transitions.cpp
+++ frontend/widgets/OBSBasic_Transitions.cpp
@@ -434,8 +434,6 @@ void OBSBasic::studioAPISetTransition(OBSSource transition)
 
 		emit currentTransitionChanged({}, false, false);
 	}
-
-	OnEvent(OBS_FRONTEND_EVENT_TRANSITION_CHANGED);
 }
 
 OBSSource OBSBasic::GetCurrentTransition()
@@ -1522,8 +1520,6 @@ void OBSBasic::setTransitionInternal(OBSSource transition)
 	} else {
 		obs_set_output_source(0, transition);
 	}
-
-	OnEvent(OBS_FRONTEND_EVENT_TRANSITION_CHANGED);
 }
 
 void OBSBasic::setCurrentTransition(const std::string &uuid)
@@ -1568,6 +1564,4 @@ void OBSBasic::SetTransitionDuration(int duration)
 	transitionDuration = duration;
 
 	emit TransitionDurationChanged(transitionDuration);
-
-	OnEvent(OBS_FRONTEND_EVENT_TRANSITION_DURATION_CHANGED);
 }
