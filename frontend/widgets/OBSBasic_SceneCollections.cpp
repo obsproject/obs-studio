29ecff953a5a7b25048cb414ed9fa9cf107a3b17
Exeldro <exelsor@hotmail.com>
2025-11-27 10:29:53 +0100

    frontend: Add logging for not default audio tracks

--- frontend/widgets/OBSBasic_SceneCollections.cpp
+++ frontend/widgets/OBSBasic_SceneCollections.cpp
@@ -1078,10 +1078,27 @@ static bool LogSceneItem(obs_scene_t *, obs_sceneitem_t *item, void *v_val)
 
 	blog(LOG_INFO, "%s- source: '%s' (%s)", indent.c_str(), name, id);
 
-	bool monitoring = obs_source_get_monitoring_enabled(source);
+	if (obs_source_get_output_flags(source) & OBS_SOURCE_AUDIO) {
+		const uint32_t all_mixers = (1 << MAX_AUDIO_MIXES) - 1;
+		uint32_t mixers = obs_source_get_audio_mixers(source);
+		if (mixers == 0) {
+			blog(LOG_INFO, "    %s- audio tracks: none", indent.c_str());
+		} else if ((mixers & all_mixers) != all_mixers) {
+			std::string tracks;
+			for (uint32_t i = 0; i < MAX_AUDIO_MIXES; i++) {
+				if (mixers & (1 << i)) {
+					tracks += " ";
+					tracks += std::to_string(i + 1);
+				}
+			}
+			blog(LOG_INFO, "    %s- audio tracks:%s", indent.c_str(), tracks.c_str());
+		}
 
-	if (monitoring) {
-		blog(LOG_INFO, "    %s- monitoring: enabled", indent.c_str());
+		bool monitoring = obs_source_get_monitoring_enabled(source);
+
+		if (monitoring) {
+			blog(LOG_INFO, "    %s- monitoring: enabled", indent.c_str());
+		}
 	}
 	int child_indent = 1 + indent_count;
 	obs_source_enum_filters(source, LogFilter, (void *)(intptr_t)child_indent);
