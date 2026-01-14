80031d0b1384a0db5c51e4ff264bee3e2bfcec16
rebstrike-code <87vhkfsqm8@privaterelay.appleid.com>
2026-01-14 14:12:03 -0500

    obs-scripting: Replace deprecated PySys_SetArgv

--- shared/obs-scripting/obs-scripting-python.c
+++ shared/obs-scripting/obs-scripting-python.c
@@ -1637,23 +1637,28 @@ bool obs_scripting_load_python(const char *python_path)
 		return false;
 #endif
 
-	/* ---------------------------------------------- */
-	/* Must set arguments for guis to work            */
-
-	wchar_t *argv[] = {L"", NULL};
-	int argc = sizeof(argv) / sizeof(wchar_t *) - 1;
-
-	//PySys_SetArgvEx(argc, argv, 0);
+//PySys_SetArgv is deprecated in 3.8+, so use the new initialization API if detected
+#if PY_VERSION_HEX >= 0x03080000
 	PyConfig config;
 	PyConfig_InitPythonConfig(&config);
-	config.parse_argv = 1;
 
-	PyStatus status = PyConfig_SetArgv(&config, argc, argv);
+	config.parse_argv = 0;
+	config.safe_path = 1;
+
+	PyWideStringList_Append(&config.argv, L"");
+
+	PyStatus status = Py_InitializeFromConfig(&config);
+	PyConfig_Clear(&config); //critical!
+
 	if (PyStatus_Exception(status)) {
+		Py_ExitStatusException(status);
 	}
-
-	Py_InitializeFromConfig(&config);
-	PyConfig_Clear(&config);
+#else
+	// Classic 3.6/3.7 path
+	Py_Initialize();
+	wchar_t *argv[] = {L"", NULL};
+	PySys_SetArgv(0, argv); // or PySys_SetArgvEx if you have 3.7+
+#endif
 
 #ifdef __APPLE__
 	PyRun_SimpleString("import sys");
