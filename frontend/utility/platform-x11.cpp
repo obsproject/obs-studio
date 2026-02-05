9776a227ef6f35c04f4a33dbe5ed1a0d9682036d
trilkk <10812332+trilkk@users.noreply.github.com>
2026-02-05 21:23:01 +0200

    frontend: Add needed FreeBSD sys/ headers for libprocstat.h to compile

--- frontend/utility/platform-x11.cpp
+++ frontend/utility/platform-x11.cpp
@@ -40,6 +40,9 @@
 
 #if defined(__FreeBSD__) || defined(__DragonFly__)
 #include <fcntl.h>
+#include <sys/param.h>
+#include <sys/queue.h>
+#include <sys/socket.h>
 #include <libprocstat.h>
 #include <pthread_np.h>
 #endif
