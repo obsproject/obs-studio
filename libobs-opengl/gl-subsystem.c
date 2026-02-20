365fe03135e7de894823425b9d3cae243cebd163
derrod <rodney@rodney.io>
2026-02-20 03:14:06 +0100

    libobs-opengl: Add AYUV support
    Tested on Linux with an NVIDIA GPU.

--- libobs-opengl/gl-subsystem.c
+++ libobs-opengl/gl-subsystem.c
@@ -1523,6 +1523,16 @@ bool device_p010_available(gs_device_t *device)
 #endif
 }
 
+bool device_ayuv_available(gs_device_t *device)
+{
+	UNUSED_PARAMETER(device);
+#ifdef _WIN32
+	return false;
+#else
+	return true; // RGBA texture
+#endif
+}
+
 uint32_t gs_voltexture_get_width(const gs_texture_t *voltex)
 {
 	/* TODO */
