87613770014772c906d2f65eb95999bf6943018c
Warchamp7 <warchamp2003@hotmail.com>
2026-02-27 17:00:29 -0500

    frontend: Make OBSAbout self contained
    This makes it so OBSAbout isn't tangled with OBSBasic.

--- frontend/dialogs/OBSAbout.hpp
+++ frontend/dialogs/OBSAbout.hpp
@@ -7,8 +7,12 @@
 class OBSAbout : public QDialog {
 	Q_OBJECT
 
+private:
+	QScopedPointer<QThread> patronJsonThread;
+
 public:
 	explicit OBSAbout(QWidget *parent = 0);
+	~OBSAbout();
 
 	std::unique_ptr<Ui::OBSAbout> ui;
 
@@ -16,4 +20,6 @@ private slots:
 	void ShowAbout();
 	void ShowAuthors();
 	void ShowLicense();
+
+	void updatePatronJson(const QString &text, const QString &error);
 };
--- frontend/widgets/OBSBasic.hpp
+++ frontend/widgets/OBSBasic.hpp
@@ -217,7 +217,6 @@ class OBSBasic : public OBSMainWindow {
 	Q_PROPERTY(QIcon audioProcessOutputIcon READ GetAudioProcessOutputIcon WRITE SetAudioProcessOutputIcon
 			   DESIGNABLE true)
 
-	friend class OBSAbout;
 	friend class OBSBasicPreview;
 	friend class OBSBasicStatusBar;
 	friend class OBSBasicSourceSelect;
@@ -272,9 +271,6 @@ private:
 
 	ConfigFile activeConfiguration;
 
-	QScopedPointer<QThread> patronJsonThread;
-	std::string patronJson;
-
 	std::unique_ptr<Ui::OBSBasic> ui;
 
 	void OnEvent(enum obs_frontend_event event);
@@ -302,7 +298,6 @@ private:
 
 public slots:
 	void close();
-	void UpdatePatronJson(const QString &text, const QString &error);
 	void UpdateEditMenu();
 	void applicationShutdown() noexcept;
 
