c657d8716ba6aa75a6b7f88414e1f4da7c5de24d
andrewsoncha <65234283+andrewsoncha@users.noreply.github.com>
2026-04-03 01:03:08 -0400

    Fix: Remove Unnecessary Code
    Removed unnecessary code in plugins/mac-capture/mac-audio.c
    
    Co-authored-by: jcm <6864788+jcm93@users.noreply.github.com>

--- plugins/mac-capture/mac-audio.c
+++ plugins/mac-capture/mac-audio.c
@@ -438,12 +438,6 @@ static OSStatus input_callback(void *data, AudioUnitRenderActionFlags *action_fl
 									     : ca->buf_list->mNumberBuffers;
 	audio.format = ca->format;
 	audio.samples_per_sec = ca->sample_rate;
-	static double factor = 0.;
-	static mach_timebase_info_data_t info = {0, 0};
-	if (info.numer == 0 && info.denom == 0) {
-		mach_timebase_info(&info);
-		factor = ((double)info.numer) / info.denom;
-	}
 	audio.timestamp = AudioConvertHostTimeToNanos(ts_data->mHostTime);
 
 	obs_source_output_audio(ca->source, &audio);
