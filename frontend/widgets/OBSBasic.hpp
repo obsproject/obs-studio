4044a1a142d648335563f44232a1b86e9b3d07ad
Warchamp7 <warchamp2003@hotmail.com>
2026-05-07 22:15:59 -0400

    frontend: Demangle ScreenshotObj and OBSBasic
    Co-Authored-By: Clayton Groeneveld <19962531+cg2121@users.noreply.github.com>

--- frontend/widgets/OBSBasic.hpp
+++ frontend/widgets/OBSBasic.hpp
@@ -40,6 +40,7 @@
 #include <util/util.hpp>
 
 #include <QAccessible>
+#include <QPointer>
 #include <QSystemTrayIcon>
 
 #include <deque>
@@ -57,6 +58,7 @@ class OBSBasicTransform;
 class OBSLogViewer;
 class OBSMissingFiles;
 class OBSProjector;
+class ScreenshotObj;
 class VolumeControl;
 #ifdef YOUTUBE_ENABLED
 class YouTubeAppDock;
@@ -215,7 +217,6 @@ class OBSBasic : public OBSMainWindow {
 	friend class OBSYoutubeActions;
 	friend struct BasicOutputHandler;
 	friend struct OBSStudioAPI;
-	friend class ScreenshotObj;
 
 	enum class MoveDir { Up, Down, Left, Right };
 
@@ -1336,7 +1337,7 @@ public:
 	 * -------------------------------------
 	 */
 private:
-	QPointer<QObject> screenshotData;
+	QPointer<ScreenshotObj> screenshotData;
 	std::string lastScreenshot;
 
 private slots:
