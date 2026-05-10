21b6e78a84cd8d049d513d98bb91489d3243a5ce
Ashwin Gundarapu <linuxuser509@zohomail.in>
2026-05-10 14:16:53 +0530

    win-capture: Fix clang-format for WebView2 WGC fix

--- plugins/win-capture/window-capture.c
+++ plugins/win-capture/window-capture.c
@@ -111,10 +111,10 @@ struct window_capture {
 };
 
 static const char *wgc_partial_match_classes[] = {
-        "Chrome",
-        "Edge",
-        "Mozilla",
-        NULL,
+	"Chrome",
+	"Edge",
+	"Mozilla",
+	NULL,
 };
 
 static const char *wgc_whole_match_classes[] = {
