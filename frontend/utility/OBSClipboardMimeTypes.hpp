45400c92ead2dde7c440a536683e2151d92fa652
Kunal Dubey <xakep8@protonmail.com>
2026-05-31 11:36:41 +0530

    frontend: added clipboard mime types

---
+++ frontend/utility/OBSClipboardMimeTypes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace OBSClipboard {
+inline constexpr auto SceneItems = "application/x-obs-studio-scene-items+json";
+inline constexpr auto SourceFilters = "application/x-obs-studio-source-filters+json";
+inline constexpr auto SceneItemTransform = "application/x-obs-studio-scene-item-transform+json";
+inline constexpr auto SceneItemTransition = "application/x-obs-studio-scene-item-transition+json";
+
+inline constexpr int PayloadVersion = 1;
+} // namespace OBSClipboard
--- frontend/utility/OBSClipboardSerializer.hpp
+++ frontend/utility/OBSClipboardSerializer.hpp
@@ -18,7 +18,6 @@
 #pragma once
 
 #include <obs.hpp>
-#include <QMimeData>
 
 class OBSClipboardSerializer {
 public:
