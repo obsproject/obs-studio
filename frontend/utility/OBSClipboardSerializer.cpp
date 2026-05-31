9cf24f44cf47a35903cf6dcf358f5064c762f3ce
Kunal Dubey <xakep8@protonmail.com>
2026-05-31 10:07:02 +0530

    frontend: scaffold for clipboard service and serializer

---
+++ frontend/utility/OBSClipboardSerializer.cpp
@@ -0,0 +1,21 @@
+#include "OBSClipboardSerializer.hpp"
+
+OBSData OBSClipboardSerializer::SerializeSceneItem(OBSSceneItem item)
+{
+	return OBSData();
+}
+
+OBSData OBSClipboardSerializer::SerializeFilters(OBSSource source)
+{
+	return OBSData();
+}
+
+OBSData OBSClipboardSerializer::SerializeTransform(OBSSceneItem item)
+{
+	return OBSData();
+}
+
+OBSData OBSClipboardSerializer::SerializeTransition(OBSSceneItem item, bool show)
+{
+	return OBSData();
+}
