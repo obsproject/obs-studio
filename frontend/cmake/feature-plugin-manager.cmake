873eea74e3a208b8a4b0f73fb242c2884659e6a7
tytan652 <tytan652@tytanium.xyz>
2026-06-11 10:26:55 +0200

    frontend: Explicitly set plugin manager linkage to nlohmann_json

--- frontend/cmake/feature-plugin-manager.cmake
+++ frontend/cmake/feature-plugin-manager.cmake
@@ -1,3 +1,5 @@
+find_package(nlohmann_json 3.11 REQUIRED)
+
 target_sources(
   obs-studio
   PRIVATE
@@ -6,3 +8,5 @@ target_sources(
     plugin-manager/PluginManagerWindow.cpp
     plugin-manager/PluginManagerWindow.hpp
 )
+
+target_link_libraries(obs-studio PRIVATE nlohmann_json::nlohmann_json)
