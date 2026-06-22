8222e663269dbcfc3e37ad4274aa9b9e6c6ebd11
PatTheMav <PatTheMav@users.noreply.github.com>
2026-06-23 01:41:25 +0200

    build-aux: Update formatter script
    The updated script works as a formatter and linter launcher and is now
    available for Zsh, Bash, and Powershell.
    
    Symbolic links serve as easy entry points for the linter and use
    the same universal launcher script (which is compatible with Csh and
    Powershell) and identifies the desired linter by the used file name.
    
    Because Powershell only executes script files that carry the ".ps1"
    suffix, separate variants for Windows need to be provided, but still
    all point to the same universal launcher under the hood.
    
    New features:
    
    * Bash version for linting and formatting
    * Powershell version for linting and formatting
    * Zizmor linting for GitHub Actions workflows
    * Xmllint linting
    * Linting only checks files but does not format them
    * Linting mode has special "GitHub" syntax to annotate failed files

--- build-aux/.run-format.zsh
+++ build-aux/.run-format.zsh
@@ -1,295 +1,306 @@
 #!/usr/bin/env zsh
 
 builtin emulate -L zsh
-setopt EXTENDED_GLOB
-setopt PUSHD_SILENT
 setopt ERR_EXIT
 setopt ERR_RETURN
-setopt NO_UNSET
-setopt PIPE_FAIL
+setopt EXTENDED_GLOB
+setopt FUNCTION_ARGZERO
 setopt NO_AUTO_PUSHD
 setopt NO_PUSHD_IGNORE_DUPS
-setopt FUNCTION_ARGZERO
+setopt NO_UNSET
+setopt PIPE_FAIL
+setopt PUSHD_SILENT
+setopt WARN_CREATE_GLOBAL
+setopt WARN_NESTED_VAR
 
 ## Enable for script debugging
-# setopt WARN_CREATE_GLOBAL
-# setopt WARN_NESTED_VAR
 # setopt XTRACE
 
-autoload -Uz is-at-least && if ! is-at-least 5.2; then
-  print -u2 -PR "%F{1}${funcstack[1]##*/}:%f Running on Zsh version %B${ZSH_VERSION}%b, but Zsh %B5.2%b is the minimum supported version. Upgrade zsh to fix this issue."
-  exit 1
-fi
+check_linter() {
+  local -i found=0
+  local linter=${1}
+  local min_version
+  local version_number
 
-invoke_formatter() {
-  if (( # < 1 )) {
-    log_error "Usage invoke_formatter [formatter_name]"
-    exit 2
-  }
-
-  local formatter="${1}"
-  shift
-  local -a source_files=(${@})
-
-  case ${formatter} {
-    clang)
+  case ${linter} {
+    clang-format)
       if (( ${+commands[clang-format-22]} )) {
-        local formatter=clang-format-22
+        linter='clang-format-22'
+        found=1
       } elif (( ${+commands[clang-format]} )) {
-        local formatter=clang-format
-      } else {
-        log_error "No viable clang-format version found (required 22.1.3)"
-        exit 2
+        linter='clang-format'
+        found=1
       }
 
-      local -a formatter_version=($(${formatter} --version))
+      if (( found )) {
+        min_version='22.1.3'
 
-      if ! is-at-least 22.1.3 ${formatter_version[-1]}; then
-        log_error "clang-format is not version 22.1.3 or above (found ${formatter_version[-1]}."
-        exit 2
-      fi
-
-      if ! is-at-least ${formatter_version[-1]} 22.1.3; then
-        log_warning "clang-format is more recent than version 22.1.3 (found ${formatter_version[-1]})."
-      fi
-
-      if (( ! #source_files )) source_files=((libobs|libobs-*|frontend|plugins|deps|shared|test)/**/*.(c|cpp|h|hpp|m|mm)(.N))
-
-      source_files=(${source_files:#*/(obs-websocket/deps|decklink/*/decklink-sdk|mac-syphon/syphon-framework|libdshowcapture)/*})
-
-      local -a format_args=(-style=file -fallback-style=none)
-      if (( _loglevel > 2 )) format_args+=(--verbose)
-
-      check_files() {
-        local -i num_failures=0
-        local -a source_files=($@)
-        local file
-        local -a format_args=(-style=file -fallback-style=none)
-        if (( _loglevel > 2 )) format_args+=(--verbose)
-
-        local -a command=(${formatter} ${format_args})
-
-        for file (${source_files}) {
-          if ! ${command} "${file}" | diff -q "${file}" - &> /dev/null; then
-            log_error "${file} requires formatting changes."
-            if (( fail_on_error == 2 )) return 2;
-            num_failures=$(( num_failures + 1 ))
-          fi
-        }
-        if (( num_failures && fail_on_error == 1 )) return 2
+        local -a clang_format_version
+        read -r -A clang_format_version <<< "$(${linter} --version 2>/dev/null || true)"
+        version_number=${clang_format_version[-1]}
+      }
+      ;;
+    swift-format)
+      if (( ${+commands[swift-format]} )) {
+        linter='swift-format'
+        found=1
       }
 
-      format_files() {
-        local -a source_files=($@)
-
-        if (( ${#source_files} )) {
-          local -a format_args=(-style=file -fallback-style=none -i)
-          if (( _loglevel > 2 )) format_args+=(--verbose)
-
-          "${formatter}" ${format_args} ${source_files}
-        }
+      if (( found )) {
+        min_version='602.0.0'
+        version_number="$(swift-format --version 2>/dev/null || true)"
       }
       ;;
     gersemi)
-
-      if (( ${+commands[gersemi-0.25]} )) {
-        local formatter=gersemi-0.25
-      } elif (( ${+commands[gersemi]} )) {
-        local formatter=gersemi
-      } else {
-        log_error "No viable gersemi version found (required 0.25.0)"
-        exit 2
+      if (( ${+commands[gersemi]} )) {
+        linter='gersemi'
+        found=1
       }
 
-      local gersemi_version=($(${formatter} --version))
-
-      if ! is-at-least 0.25.0 ${gersemi_version[2]}; then
-        log_error "gersemi is not version 0.25.0 or above (found ${gersemi_version[2]}."
-        exit 2
-      fi
-
-      if (( ! #source_files )) source_files=(CMakeLists.txt (libobs|libobs-*|frontend|plugins|deps|shared|cmake|test)/**/(CMakeLists.txt|*.cmake)(.N))
-
-      source_files=(${source_files:#*/(jansson|decklink/*/decklink-sdk|obs-websocket|obs-browser|libdshowcapture)/*})
-      source_files=(${source_files:#(cmake/Modules/*|*/legacy.cmake)})
-
-      check_files() {
-        local -i num_failures=0
-        local -a source_files=($@)
-        local file
-        local -a command=(${formatter} -c --no-cache ${source_files})
-        local -i in_error=0
-
-        if (( ${#source_files} )) {
-          while read -r line; do
-            local -a line_tokens=(${(z)line})
-            if (( #line_tokens )) {
-              file=${line_tokens[1]}
-
-              if [[ -r ${file} ]] {
-                in_error=0
-                file=${file//*${project_root}\//}
-
-                log_error "${file} requires formatting changes."
-              } else {
-                if (( in_error )) {
-                  log_output "${line}"
-                } else {
-                  log_error "${line}"
-                }
-                in_error=1
-              }
-            } else {
-              log_output "${line}"
-            }
-
-            if (( fail_on_error == 2 )) return 2
-            num_failures=$(( num_failures + 1 ))
-          done < <(${command} 2>&1)
-
-          if (( num_failures && fail_on_error == 1 )) return 2
-        }
-      }
-
-      format_files() {
-        local -a source_files=($@)
-
-        if (( ${#source_files} )) {
-          "${formatter}" -i ${source_files}
-        }
+      if (( found )) {
+        min_version='0.27.0'
+        local -a gersemi_version
+        read -r -A gersemi_version <<< "$(gersemi --version 2>/dev/null || true)"
+        version_number=${gersemi_version[2]}
       }
       ;;
-    swift)
-      local formatter=swift-format
-      if (( ${+commands[swift-format]} )) {
-        local swift_format_version=$(swift-format --version)
-
-        if ! is-at-least 508.0.0 ${swift_format_version}; then
-          log_error "swift-format is not version 508.0.0 or above (found ${swift_format_version})."
-          exit 2
-        fi
-      } else {
-        log_error "No viable swift-format version found (required 508.0.0)"
-        exit 2
+    zizmor)
+      if (( ${+commands[zizmor]} )) {
+        linter='zizmor'
+        found=1
       }
 
-      if (( ! #source_files )) source_files=((libobs|libobs-*|frontend|plugins)/**/*.swift(.N))
-
-      check_files() {
-        local -i num_failures=0
-        local -a source_files=($@)
-        local file
-        local -a format_args=()
-
-        local -a command=(${formatter} ${format_args})
-
-        for file (${source_files}) {
-          if ! "${command}" "${file}" | diff -q "${file}" - &> /dev/null; then
-            log_error "${file} requires formatting changes."
-            if (( fail_on_error == 2 )) return 2;
-            num_failures=$(( num_failures + 1 ))
-          fi
-        }
-        if (( num_failures && fail_on_error == 1 )) return 2
-      }
-
-      format_files() {
-        local -a source_files=($@)
-
-        if (( ${#source_files} )) {
-          local -a format_args=(-i)
-
-          "${formatter}" ${format_args} ${source_files}
-        }
+      if (( found )) {
+        min_version='1.25.0'
+        local -a zizmor_version
+        read -r -A zizmor_version <<< "$(zizmor --version 2>/dev/null || true)"
+        version_number=${zizmor_version[2]}
       }
       ;;
-    *) log_error "Invalid formatter specified: ${1}. Valid options are clang-format, gersemi, and swift-format."; exit 2 ;;
+    xmllint)
+      if (( ${+commands[xmllint]} )) {
+        linter='xmllint'
+        found=1
+      }
+
+      if (( found )) {
+        min_version='20900.0.0'
+        local -a xmllint_version
+        read -r -A xmllint_version <<< "$(xmllint --version 2>&1 || true)"
+        version_number="${xmllint_version[5]}.0.0"
+      }
+      ;;
+    *)
+      print -u2 -PR "%F{1}  ✖  %f Unsupported linter specified."
+      return 1
+      ;;
   }
 
-  local file
-  local -i num_failures=0
-  if (( check_only )) {
-    if (( ${+functions[check_files]} )) {
-      check_files ${source_files}
-    } else {
-      log_error "No format check function defined for formatter '${formatter}'"
-      exit 2
+  if (( ! found )) {
+    print -u2 -PR "%F{1}  ✖  %f Unable to find %B'${linter}'%b on system."
+    return 1
+  }
+
+  if ! is-at-least ${min_version} ${version_number}; then
+    print -u2 -PR "%F{1}  ✖  %f ${linter} ${version_number} found (Required: %B${min_version}%b)."
+    return 1
+  fi
+}
+
+generate_file_list() {
+  local linter=${1}
+  local -a found_files
+
+  if (( ! #source_files )) {
+    case ${linter} {
+      clang-format)
+        found_files=((libobs|libobs-*|frontend|plugins|deps|shared|test)/**/*.(c|cpp|h|hpp|m|mm)(.N))
+        found_files=(${found_files:#*/(decklink/*/decklink-sdk|obs-websocket|obs-browser|libdshowcapture)/*})
+        ;;
+      swift-format)
+        found_files=((libobs|libobs-*|frontend|plugins)/**/*.swift(.N))
+        ;;
+      gersemi)
+        found_files=(CMakeLists.txt (libobs|libobs-*|frontend|plugins|deps|shared|cmake|test)/**/(CMakeLists.txt|*.cmake)(.N))
+        found_files=(${found_files:#*/(jansson|decklink/*/decklink-sdk|obs-websocket|obs-browser|libdshowcapture)/*})
+        ;;
+      zizmor)
+        found_files=(.github/(workflows|actions)/**/*.(yaml|yml))
+        ;;
+      xmllint)
+        found_files=(frontend/forms/**/*ui)
+        ;;
+      *) return ;;
     }
+
+    typeset -ga source_files=(${found_files})
   } else {
-    if (( ${+functions[format_files]} )) {
-      format_files ${source_files}
-    } else {
-      log_error "No format function defined for formatter '${formatter}'"
-      exit 2
-    }
+    typeset -ga source_files=(${source_files//${project_root}\/})
   }
 }
 
-run_format() {
-  if (( ! ${+SCRIPT_HOME} )) typeset -g SCRIPT_HOME=${ZSH_ARGZERO:A:h}
-  if (( ! ${+FORMATTER_NAME} )) typeset -g FORMATTER_NAME=${${(s:-:)ZSH_ARGZERO:t:r}[2]}
+invoke_formatter() {
+  local formatter=${1}
+  shift
+  local -a source_files
+  read -r -A source_files <<< "${@}"
+  local -a format_arguments
+
+  generate_file_list ${formatter}
+
+  case ${formatter} {
+    clang-format)
+      format_arguments=(--style=file --fallback-style=none -i)
+      if (( verbose_output )) {
+        format_arguments+=(--verbose)
+      }
+      ;;
+    swift-format)
+      format_arguments=(format --parallel --color-diagnostics -i)
+      ;;
+    gersemi)
+      format_arguments=(--no-cache -i)
+      ;;
+    *)
+      return 1
+  }
+
+  ${formatter} ${format_arguments} ${source_files}
+}
+
+invoke_linter() {
+  local linter=${1}
+  shift
+  local -a source_files
+  read -r -A source_files <<< "${@}"
+
+  local regexp
+  local glob_expression
+  local -a indices
+  local -a lint_arguments
+
+  generate_file_list ${linter}
+
+  case ${linter} {
+    clang-format)
+      regexp='^([^:]+):([0-9]+):[0-9]+:[[:space:]](.+):[[:space:]](.+)\[-W(.+)\]$'
+      indices=(1 2 3 5 4)
+      lint_arguments=(--style=file --fallback-style=none -Werror --dry-run)
+      if (( verbose_output )) {
+        lint_arguments+=(--verbose)
+      }
+      ;;
+    swift-format)
+      regexp='^([^:]+):([0-9]+):[0-9]+:[[:space:]](.+):[[:space:]]\[(.+)\][[:space:]](.+)$'
+      indices=(1 2 3 4 5)
+      lint_arguments=(lint)
+      ;;
+    gersemi)
+      regexp="^${project_root}/([^[:space:]]+)[[:space:]](.+)"
+      indices=(1 'Entire File' 'error' 'gersemi' 2)
+      lint_arguments=(--check --no-cache --warnings-as-errors)
+      ;;
+    zizmor)
+      regexp='^::(.+)[[:space:]]file=(.+),line=([0-9]+),title=(.+)::.+:[0-9]+:[[:space:]](.+)$'
+      indices=(2 3 1 4 5)
+      lint_arguments=(--offline --persona=auditor --format=github --no-progress --quiet)
+      ;;
+    xmllint)
+      regexp='^([^:]+):([0-9]+):[[:space:]]+.+:[[:space:]](.+):[[:space:]](.+)$'
+      indices=(1 2 error 3 4)
+      lint_arguments=(--schema ${project_root}/frontend/forms/XML-Schema-Qt5.15.xsd --noout)
+      ;;
+    *)
+      return 1
+      ;;
+  }
+
+  local -i num_failures=0
+
+  if (( #source_files )) {
+    local file_path
+    local line_number
+    local error_level
+    local error_title
+    local error_message
+
+    local line
+    local ordered_output
+    local -a match
+    local mbegin
+    local mend
+    local MATCH
+    local MBEGIN
+    local MEND
+
+    while read -r line; do
+      if [[ ${linter} == 'zizmor' ]] && (( github_style )) {
+        print -- ${line}
+        num_failures+=1
+        continue
+      }
+
+      if [[ ${line} =~ ${regexp} ]] {
+        ordered_output="${(j:|:)indices//(#m)<0-9>##/${match[${MATCH}]}}"
+        { IFS='|' read -r file_path line_number error_level error_title error_message } <<< "${ordered_output}"
+
+        if (( github_style )) {
+          print "::${error_level} file=${file_path},line=${line_number},title=${error_title}::${file_path:t}:${line_number}: ${error_message}"
+        } else {
+          print -PR "  %F{1}✖%f  ${file_path}:${line_number} - ${error_title}: ${error_message}"
+        }
+
+        num_failures+=1
+      } else {
+        print -- "${line}"
+      }
+    done < <(${linter} ${lint_arguments} ${source_files} 2>&1 || true)
+  }
+
+  if (( num_failures )) {
+    return 1
+  }
+}
+
+main() {
+  autoload -Uz is-at-least && if ! is-at-least 5.8; then
+    print -u2 -PR "%F{1}  ✖  %f ${ZSH_ARGZERO} requires Zsh %B5.8%b or later (detected version: %B${ZSH_VERSION}%b)."
+    exit 1
+  fi
+
+  if (( ! ${+SCRIPT_HOME} )) typeset -g SCRIPT_HOME="${ZSH_ARGZERO:A:h}"
   local project_root=${SCRIPT_HOME:A:h}
 
-  typeset -g host_os=${${(L)$(uname -s)}//darwin/macos}
-  local -i fail_on_error=0
-  local -i check_only=0
-  local -i verbosity=1
-  local -r _version='1.0.0'
-
-  fpath=("${SCRIPT_HOME}/.functions" ${fpath})
-  autoload -Uz set_loglevel log_info log_error log_output log_status log_warning
-
-  local -r _usage="
-Usage: %B${functrace[1]%:*}%b <option>
-
-%BOptions%b:
-
-%F{yellow} Formatting options%f
- -----------------------------------------------------------------------------
-  %B-c | --check%b                      Check only, no actual formatting takes place
-
-%F{yellow} Output options%f
- -----------------------------------------------------------------------------
-  %B-v | --verbose%b                    Verbose (more detailed output)
-  %B--fail-[never|error]                Fail script never/on formatting change - default: %B%F{green}never%f%b
-  %B--debug%b                           Debug (very detailed and added output)
-
-%F{yellow} General options%f
- -----------------------------------------------------------------------------
-  %B-h | --help%b                       Print this usage help
-  %B-V | --version%b                    Print script version information"
+  local -i verbose_output=0
+  local -i lint_only=0
+  local -i github_style=0
 
   local -a args
   while (( # )) {
     case ${1} {
-      -c|--check) check_only=1; shift ;;
-      -v|--verbose) (( verbosity += 1 )); shift ;;
-      -h|--help) log_output ${_usage}; exit 0 ;;
-      -V|--version) print -Pr "${_version}"; exit 0 ;;
-      --debug) verbosity=3; shift ;;
-      --fail-never)
-        fail_on_error=0
-        shift
-        ;;
-      --fail-error)
-        fail_on_error=1
-        shift
-        ;;
-      --fail-fast)
-        fail_on_error=2
-        shift
+      -c|--check) lint_only=1; shift ;;
+      -v|--verbose) verbose_output=1; shift ;;
+      -gh|--github) github_style=1; shift ;;
+      -l|--linter)
+        if (( ! ${+LINTER_NAME} )) && [[ -n ${2} ]] {
+          typeset -g LINTER_NAME="${2}"
+          shift 2
+        }
         ;;
       *)
-        args+=($@)
-        break
+        args+=(${1})
+        shift
         ;;
     }
   }
 
-  set -- ${(@)args}
-  set_loglevel ${verbosity}
+  check_linter ${LINTER_NAME}
 
-  invoke_formatter ${FORMATTER_NAME} ${args}
+  if (( lint_only )) {
+    invoke_linter ${LINTER_NAME} ${args}
+  } else {
+    invoke_formatter "${LINTER_NAME}" ${args}
+  }
 }
 
-run_format ${@}
+main ${@}
