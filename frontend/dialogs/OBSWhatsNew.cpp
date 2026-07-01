02b0a796d55d1acbbcf77ad826d7808d441a93e6
Warchamp7 <warchamp2003@hotmail.com>
2026-06-30 23:23:14 -0400

    frontend: Remove What's New bottom button

--- frontend/dialogs/OBSWhatsNew.cpp
+++ frontend/dialogs/OBSWhatsNew.cpp
@@ -37,17 +37,8 @@ OBSWhatsNew::OBSWhatsNew(QWidget *parent, const std::string &url) : QDialog(pare
 
 	connect(cefWidget, &QCefWidget::titleChanged, this, &OBSWhatsNew::setWindowTitle);
 
-	QPushButton *close = new QPushButton(QTStr("Close"));
-	connect(close, &QAbstractButton::clicked, this, &QDialog::accept);
-
-	QHBoxLayout *bottomLayout = new QHBoxLayout();
-	bottomLayout->addStretch();
-	bottomLayout->addWidget(close);
-	bottomLayout->addStretch();
-
 	QVBoxLayout *topLayout = new QVBoxLayout(this);
 	topLayout->addWidget(cefWidget);
-	topLayout->addLayout(bottomLayout);
 
 	show();
 #else
