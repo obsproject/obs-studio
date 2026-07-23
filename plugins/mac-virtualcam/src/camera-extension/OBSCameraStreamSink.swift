bdec0bc5486ea8a85cd9b468a8d95b3d37f9f88a
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-23 23:28:25 +0200

    plugins: Fix swift-format issues in virtualcam

--- plugins/mac-virtualcam/src/camera-extension/OBSCameraStreamSink.swift
+++ plugins/mac-virtualcam/src/camera-extension/OBSCameraStreamSink.swift
@@ -67,7 +67,7 @@ class OBSCameraStreamSink: NSObject, CMIOExtensionStreamSource {
             streamProperties.activeFormatIndex = 0
         }
         if properties.contains(.streamFrameDuration) {
-            let frameDuration = CMTime(value: 1, timescale: Int32(OBSCameraFrameRate))
+            let frameDuration = CMTime(value: 1, timescale: Int32(obsCameraFrameRate))
             streamProperties.frameDuration = frameDuration
         }
         if properties.contains(.streamSinkBufferQueueSize) {
