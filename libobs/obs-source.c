a874bb0acbd1bab242f31ce9733de5558697d8bb
pkv <pkv@obsproject.com>
2026-08-10 22:42:05 +0200

    libobs/audio-monitoring: Add a monitoring mixer
    This adds a new raw audio callback for audio outputs where monitored
    sources are mixed. The audio ticks at 256 frames so about 5 ms at
    48 kHz sample rate.
    This allows a much smaller latency than what is provided by the regular
    audio_callback which can buffer up to 960 ms (at 48 kHz).
    The main advantage against the low latency option which decreases the
    output latency to about 2 audio ticks (40 ms) is that this does not
    affect the regular audio pipeline and preserves the buffering which is
    essential for streams.
    The mixer is platform agnostic and can be used by any audio output.
    This is useful for ASIO which has no mixing capability, but even for
    WASAPI, CoreAudio or PulseAudio, this may allow in the future the
    creation of monitoring buses.
    With ASIO tests of the monitoring latency can be smaller than a video
    frame at 120 fps (so smaller than 8.3 ms with buffer 128, Yamaha driver)
    while WASAPI is in the range 30-50 ms.
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -18,6 +18,7 @@
 #include <inttypes.h>
 #include <math.h>
 
+#include "audio-monitoring/monitoring-mix.h"
 #include "media-io/format-conversion.h"
 #include "media-io/video-frame.h"
 #include "media-io/audio-io.h"
@@ -756,6 +757,9 @@ void obs_source_destroy(struct obs_source *source)
 	}
 
 	if (is_audio_source(source)) {
+		if (source->monitoring_mix_source && obs->audio.monitoring_mix) {
+			monitoring_mix_remove_source(obs->audio.monitoring_mix, source->monitoring_mix_source);
+		}
 		pthread_mutex_lock(&source->audio_cb_mutex);
 		da_free(source->audio_cb_list);
 		pthread_mutex_unlock(&source->audio_cb_mutex);
@@ -5628,9 +5632,13 @@ void obs_source_set_monitoring_enabled(obs_source_t *source, bool enabled)
 	if (was_on != enabled) {
 		if (!was_on) {
 			source->monitor = audio_monitor_create(source);
+			monitoring_mix_add_source(source);
 		} else {
 			audio_monitor_destroy(source->monitor);
 			source->monitor = NULL;
+			if (source->monitoring_mix_source && obs->audio.monitoring_mix) {
+				monitoring_mix_remove_source(obs->audio.monitoring_mix, source->monitoring_mix_source);
+			}
 		}
 	}
 }
