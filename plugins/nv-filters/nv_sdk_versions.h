9a250f95d4194038fc20314dc18b3c1a996dccd9
pkv <pkv@obsproject.com>
2026-08-29 19:57:49 +0200

    nv-filters: Enable audio FX SDK 3.0.0 for ARM
    SDK 3.0.0 brings compatibility with arm64. The DLL loading sequence has
    been adjusted since the FX are now loaded in their own child DLL.
    The DLL loader has been also cleaned up by removing unused symbols. This
    allowed us to dodge a few ABI breaks from NVIDIA...
    
    Signed-off-by: pkv <pkv@obsproject.com>

--- plugins/nv-filters/nv_sdk_versions.h
+++ plugins/nv-filters/nv_sdk_versions.h
@@ -1,2 +1,5 @@
 #define MIN_VFX_SDK_VERSION (0 << 24 | 7 << 16 | 6 << 8 | 0 << 0)
+
 #define MIN_AFX_SDK_VERSION (1 << 24 | 6 << 16 | 1 << 8 | 2 << 0)
+#define MIN_AFX_LOGGER_VERSION (1 << 24 | 6 << 16 | 1 << 8 | 2 << 0) // also gates VAD feature
+#define MIN_ARM_AFX_SDK_VERSION (3 << 24 | 0 << 16 | 0 << 8 | 49 << 0)
