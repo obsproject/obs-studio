98f68c067c3163d24b0bfb4b9404af9e73cd7a71
brazosbuackaroo <loughrandt@gmail.com>
2026-09-08 17:21:34 -0500

    fixed leaked memory with QAction copyAction and pasteAction

--- frontend/dialogs/OBSBasicFilters.cpp
+++ frontend/dialogs/OBSBasicFilters.cpp
@@ -869,7 +869,7 @@ void OBSBasicFilters::CustomContextMenu(const QPoint &pos, bool async)
 
 	if (item) {
 		popup->addSeparator();
-		popup->addAction(QTStr("Duplicate"), this, [&]() {
+		popup->addAction(QTStr("Duplicate"), popup, [&]() {
 			DuplicateItem(isAsync ? ui->asyncFilters->currentItem() : ui->effectFilters->currentItem());
 		});
 		popup->addSeparator();
@@ -877,7 +877,7 @@ void OBSBasicFilters::CustomContextMenu(const QPoint &pos, bool async)
 		popup->addAction(ui->actionRemoveFilter);
 		popup->addSeparator();
 
-		QAction *copyAction = new QAction(QTStr("Copy"));
+		QAction *copyAction = new QAction(QTStr("Copy"), popup);
 		connect(copyAction, &QAction::triggered, this, &OBSBasicFilters::CopyFilter);
 		copyAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_C));
 		ui->effectWidget->addAction(copyAction);
@@ -885,7 +885,7 @@ void OBSBasicFilters::CustomContextMenu(const QPoint &pos, bool async)
 		popup->addAction(copyAction);
 	}
 
-	QAction *pasteAction = new QAction(QTStr("Paste"));
+	QAction *pasteAction = new QAction(QTStr("Paste"), popup);
 	pasteAction->setEnabled(main->copyFilter);
 	connect(pasteAction, &QAction::triggered, this, &OBSBasicFilters::PasteFilter);
 	pasteAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_V));
