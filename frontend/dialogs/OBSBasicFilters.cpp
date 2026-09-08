2e121890a9f4fdadf9cb23cf15e07f3a24c1a8e2
brazosbuackaroo <loughrandt@gmail.com>
2026-09-08 17:24:37 -0500

    reparented qaction so it can be cleaned up properly

--- frontend/dialogs/OBSBasicFilters.cpp
+++ frontend/dialogs/OBSBasicFilters.cpp
@@ -503,7 +503,7 @@ OBSMenu *OBSBasicFilters::CreateAddFilterPopupMenu(QWidget *parent, const bool &
 			continue;
 		}
 
-		QAction *popupItem = new QAction(QT_UTF8(type.name.c_str()), this);
+		QAction *popupItem = new QAction(QT_UTF8(type.name.c_str()), popup);
 		popupItem->setData(QT_UTF8(type.type.c_str()));
 		connect(popupItem, &QAction::triggered, this, [this, type]() { AddNewFilter(type.type.c_str()); });
 		popup->addAction(popupItem);
