b5a926c9305420934412556e36ad948c13679dfe
Ryan Peters <me@rpeters.dev>
2026-09-13 12:33:13 -0500

    win-dshow: Check both registry views for virtual camera
    On Windows ARM64, virtual camera does not display in the UI by default
    because the win-dshow plugin was only checking for the base 32bit module
    registration. It now has diagnostic log messages and will work in
    portable mode if only the 64bit vcam is available.

--- plugins/win-dshow/dshow-plugin.cpp
+++ plugins/win-dshow/dshow-plugin.cpp
@@ -1,4 +1,6 @@
 #include <obs-module.h>
+#include <util/platform.h>
+#include <util/util.hpp>
 #include <strsafe.h>
 #include <strmif.h>
 #ifdef VIRTUALCAM_AVAILABLE
@@ -22,7 +24,10 @@ static bool vcam_installed(bool b64)
 {
 	wchar_t cls_str[CHARS_IN_GUID];
 	wchar_t temp[MAX_PATH];
+	wchar_t path[MAX_PATH];
+	DWORD path_size = sizeof(path);
 	HKEY key = nullptr;
+	const char *bits = b64 ? "64" : "32";
 
 	StringFromGUID2(CLSID_OBS_VirtualVideo, cls_str, CHARS_IN_GUID);
 	StringCbPrintf(temp, sizeof(temp), L"CLSID\\%s", cls_str);
@@ -32,10 +37,21 @@ static bool vcam_installed(bool b64)
 
 	LSTATUS status = RegOpenKeyExW(HKEY_CLASSES_ROOT, temp, 0, flags, &key);
 	if (status != ERROR_SUCCESS) {
+		blog(LOG_INFO, "Virtual camera %s-bit filter is not registered", bits);
 		return false;
 	}
 
+	status = RegGetValueW(key, L"InprocServer32", nullptr, RRF_RT_REG_SZ, nullptr, path, &path_size);
 	RegCloseKey(key);
+
+	if (status == ERROR_SUCCESS) {
+		BPtr<char> path_utf8;
+		os_wcs_to_utf8_ptr(path, 0, &path_utf8);
+		blog(LOG_INFO, "Virtual camera %s-bit filter is registered: %s", bits, (const char *)path_utf8);
+	} else {
+		blog(LOG_INFO, "Virtual camera %s-bit filter is registered but its path could not be read", bits);
+	}
+
 	return true;
 }
 #endif
@@ -45,8 +61,13 @@ bool obs_module_load(void)
 	RegisterDShowSource();
 	RegisterDShowEncoders();
 #ifdef VIRTUALCAM_AVAILABLE
-	if (vcam_installed(false)) {
+	bool installed32 = vcam_installed(false);
+	bool installed64 = vcam_installed(true);
+
+	if (installed32 || installed64) {
 		obs_register_output(&virtualcam_info);
+	} else {
+		blog(LOG_WARNING, "Virtual camera output is unavailable, no virtual camera filter is registered");
 	}
 #endif
 
